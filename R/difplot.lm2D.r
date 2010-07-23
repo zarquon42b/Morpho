0c5a795d2fe57c5eb73f0fc809e40bcf6c1997f5
zarquon42b <zarquon42b@users.sourceforge.net>
2010-07-23 13:33:25 +0200

    added 2D support to (mc.)find.outliers

---
+++ R/difplot.lm2D.r
@@ -0,0 +1,22 @@
+difplot.lm2D<-function(refshape,targetshape,color=4,lwd=1,lcol=2,main=main)
+{   
+    A<-refshape
+    k<-dim(A)[1]
+    m<-dim(A)[2]
+    
+    sds<-0
+     #lim<-max(abs(refshape))
+    
+    
+      
+      #sz <- (centroid.size(refshape)/sqrt(k))*(1/80)
+	
+      #if (!spheres)
+      plot(refshape,  col = color,main=main,asp=1)
+      #else {spheres3d(refshape, radius = sz, col = color)}
+      #plot3d(refshape,box=F,axes=F,type=type,xlim=c(-lim,lim),ylim=c(-lim,lim),zlim=c(-lim,lim),col=color,xlab="",ylab="",zlab="",radius=sz)
+    for (j in 1:k)
+          {lines(rbind(refshape[j,],targetshape[j,]),col=lcol,lwd=lwd)}
+      text(refshape,labels=paste("",c(1:k),sep=""),col=lcol,pos=2) 
+    
+}
--- R/find.outliers.r
+++ R/find.outliers.r
@@ -3,23 +3,17 @@ find.outliers<-function(A,color=4,lwd=1,lcol=2)
 	raw<-A
 	n<-dim(A)[3]
 	k<-dim(A)[1]
+	m<-dim(A)[2]
 	A<-procGPA(A,distances=FALSE,pcaoutput=FALSE)
-	
+
+###from here on the same as mc.find.outliers ###	
 	rho<-0
 	for(i in 1:n)
 		{rho[i]<-angle.calc(A$rotated[,,i],A$mshape)$rho
 		}
 	A$rho<-rho
 	
-	if (dim (A$rotated)[2]==2)
-		{A0<-array(NA,dim=c(k,3,n))
-		for (i in 1:n)
-			
-			{A0[,,i]<-cbind(A$rotated[,,i],0)
-			}
-		A$mshape<-cbind(A$mshape,0)
-		A$rotated<-A0
-		}
+	
     	#rgl.clear()
     	#rgl.bg(color = "white")
    	disti<-data.frame(c(1:n),A$rho)
@@ -29,14 +23,80 @@ find.outliers<-function(A,color=4,lwd=1,lcol=2)
     	outlier<-NULL
     	t1 <- 1
     	lo<-1
-    	op<-open3d()	
+	if (m==3)
+		{op<-open3d()	
+		}  	
+	
 	while (t1 <= n )
     		{
-	difplot.lm(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,rgl.new=FALSE)
+	if (m==3)
+		{difplot.lm(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,rgl.new=FALSE)
+		}
+	else 
+		{difplot.lm2D(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,main=disti.sort[t1,1])
+		}
       
       	cat(paste("outlier #",t1,": ",disti.sort[t1,1],"     procrustes dist. to mean: ",disti.sort[t1,2],"\n",sep=""))
-      
-      	answer <- substr(readline(" add to outlierlist (y/N/s)?\ny=yes,n=no,s=switch landmarks: "), 1L,1L)
+      	
+	if (disti.sort[t1,1] %in% outlier)
+		{ answer<-substr(readline(" already added to outlierlist! remove fromlist (y/N/s)?"), 1L,1L)
+		while (!(answer %in% c("y","Y","s","S","N","n")))
+			{answer<-substr(readline(" already added to outlierlist! remove fromlist (y/N/s)?"), 1L,1L)
+			}
+		### keep in outlier list		
+		if (answer == "n" || answer == "N")
+         		{
+          		lo<-lo+1
+          		}
+		### switching landmark routine starting ###	 	
+		else if (answer == "s" || answer == "S")  
+			{loop0="y"
+			while(loop0=="y" || loop0 =="Y")
+				{answer1a<-as.integer(readline("select  two Landmarks to switch position\ninsert first: "))
+				while (is.na(answer1a)|| answer1a > k || answer1a < 1)
+					{answer1a<-as.integer(readline(paste("please enter integer <",k,"!\ninsert first: ")))
+					}
+				answer1b<-as.integer(readline("insert second: "))
+				while (is.na(answer1b) || answer1b > k || answer1b < 1)
+					{answer1b<-as.integer(readline(paste("please enter integer <",k,"!\ninsert second: ")))
+					}
+		
+		### switch rows of selected landmarks ##
+			raw[c(answer1a,answer1b),,disti.sort[t1,1]]<-raw[c(answer1b,answer1a),,disti.sort[t1,1]]
+			A$rotated[c(answer1a,answer1b),,disti.sort[t1,1]]<-A$rotated[c(answer1b,answer1a),,disti.sort[t1,1]]
+			rho.new<-angle.calc(A$rotated[,,disti.sort[t1,1]],A$mshape)$rho
+			
+			if (m==3)
+				{rgl.clear()
+				difplot.lm(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,rgl.new=FALSE)
+				}
+			else 
+				{difplot.lm2D(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,main=disti.sort[t1,1])
+				}
+			cat(paste("new distance to mean:",rho.new,"\n"))
+			loop0<-substr(readline("switch more (y/N)? "),1L,1L)
+				while(loop0 != "y" && loop0 != "Y" && loop0 != "n" && loop0 != "N" )
+					{loop0<-substr(readline("yes or no? "),1L,1L)
+					}
+			}
+			answer <- substr(readline("do you still want to add to outliers (y/N)? "), 1L,1L)
+       				while (!(answer %in% c("y","Y","N","n")))
+				{answer <- substr(readline("do you still want to add to outliers (y/N)? "), 1L,1L)
+				}
+       	
+		
+			}
+		### remove from outlier list ###
+		else if (answer == "y" || answer == "Y")
+         			{outlier<-outlier[-which(outlier==disti.sort[t1,1])]
+				cat(paste("outlier #",t1,": ",disti.sort[t1,1],"removed from outlierlist.\n"))
+          			lo<-lo+1
+          			}	
+		}
+      	
+	else
+	{	
+	answer <- substr(readline(" add to outlierlist (y/N/s)?\ny=yes,n=no,s=switch landmarks: "), 1L,1L)
        	while (!(answer %in% c("y","Y","s","S","N","n")))
       		{answer <- substr(readline(" add to outlierlist (y/N/s)?\ny=yes,n=no,s=switch landmarks: "), 1L,1L)
        		}
@@ -62,8 +122,13 @@ find.outliers<-function(A,color=4,lwd=1,lcol=2)
 			raw[c(answer1a,answer1b),,disti.sort[t1,1]]<-raw[c(answer1b,answer1a),,disti.sort[t1,1]]
 			A$rotated[c(answer1a,answer1b),,disti.sort[t1,1]]<-A$rotated[c(answer1b,answer1a),,disti.sort[t1,1]]
 			rho.new<-angle.calc(A$rotated[,,disti.sort[t1,1]],A$mshape)$rho
-			rgl.clear()
-			difplot.lm(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,rgl.new=FALSE)
+			if (m==3)
+				{rgl.clear()
+				difplot.lm(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,rgl.new=FALSE)
+				}
+			else 
+				{difplot.lm2D(A$mshape,A$rotated[,,disti.sort[t1,1]],color=color,lwd=1,lcol=lcol,main=disti.sort[t1,1])
+				}
 			cat(paste("new distance to mean:",rho.new,"\n"))
 			loop0<-substr(readline("switch more (y/N)? "),1L,1L)
 				while(loop0 != "y" && loop0 != "Y" && loop0 != "n" && loop0 != "N" )
@@ -71,14 +136,16 @@ find.outliers<-function(A,color=4,lwd=1,lcol=2)
 					}
 			}
 			answer <- substr(readline("do you still want to add to outliers (y/N)? "), 1L,1L)
-       	
+       				while (!(answer %in% c("y","Y","N","n")))
+				{answer <- substr(readline("do you still want to add to outliers (y/N)? "), 1L,1L)
+				}
 			if (answer == "y" || answer == "Y")
          			{outlier[lo]<-disti.sort[t1,1]
           			lo<-lo+1
           			}	
 			}
 		    
-		
+	}	
       ### go on or halt ###
       	answer1 <- substr(readline(" next/previous/stop (n/p/s)?  "), 1L,1L)
 	 	if (!(answer1 %in% c("P","p","S","s")))
@@ -88,8 +155,10 @@ find.outliers<-function(A,color=4,lwd=1,lcol=2)
             		t1<-t1+1          
          		}
 		else if (answer1 %in% c("P","p"))
-			{rgl.clear()
-           		rgl.bg(color = "white")
+			{if(m==3)
+				{rgl.clear()
+           			rgl.bg(color = "white")
+				}
 			if (t1 !=1)            		
 				{t1<-t1-1
 				}
