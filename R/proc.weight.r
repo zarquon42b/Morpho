b03fa792f7e300847e58f89b6267ecfe43221820
zarquon42b <zarquon42b@users.sourceforge.net>
2010-10-22 13:05:14 +0200

    added: proc.weight for weighting closest shapes

---
+++ R/proc.weight.r
@@ -0,0 +1,31 @@
+proc.weight<-function(data,number,ref)
+{	
+	col<-2
+	l<-dim(data)[3]
+	rho<-0
+	for(i in 1:l){rho[i]<-angle.calc(data[,,ref],data[,,i])$rho}
+	
+	if (is.null(dimnames(data)[[3]]))
+		{id<-as.character(c(1:l))
+		}
+	else
+		{id<-dimnames(data)[[3]]}
+	data<-data.frame(id,rho)
+	dat.sort.i<-data[order(data[,col]),]
+	dat.which<-dat.sort.i[2:(number+1),]
+		
+	all<-sum(dat.which[,col])
+	share<-0
+		
+	for (i in 1:length(dat.which[,col]))
+		{share[i]<-dat.which[i,col]/all
+		
+		}
+	weight<-sort(share,decreasing=T)
+	out<-data.frame(dat.which,weight)
+	cat(paste("  reference was",id[ref],"\n"))
+	return(list(data=out,reference=id[ref],rho.all=data))
+}
+	
+	
+
