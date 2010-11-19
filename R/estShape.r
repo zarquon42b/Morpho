ddf219ee0d5954d2f4a5796aa4382378026e0b35
zarquon42b <zarquon42b@users.sourceforge.net>
2010-11-19 13:18:45 +0100

    added some helper functions

--- R/estShape.r
+++ R/estShape.r
@@ -1,22 +1,31 @@
-estShape<-function(shapelist,procrot,n,ref)
+estShape<-function(shapelist,procrot,n,ref,softproc=NULL,size=NULL)
 {	
 	wcalc<-proc.weight(procrot,n,ref)
 	dimvb<-dim(shapelist[[1]]$vb)
-	
+	lm.est<-NULL
 	vb<-matrix(0,3,dimvb[2])
-	
+	meandev<-NULL
 	for (i in 1:n)
 		{vb<-vb+shapelist[[wcalc$data$nr[i]]]$vb[1:3,]*wcalc$data$weight[i]
 		}
 	
-	lms<-procrot[,,wcalc$data$nr]
-	lm.est<-matrix(0,dim(procrot)[1],3)
+	
+	if (! is.null(softproc))
+	{
+	if (is.null(size))
+	stop("please enter size vector")
+	
+	lms<-softproc[,,wcalc$data$nr]
+	lm.est<-matrix(0,dim(softproc)[1],3)
 	for (i in 1:n)
 		{lm.est<-lm.est+lms[,,i]*wcalc$data$weight[i]
 		}
+	meandev<-mean(sqrt(diag(tcrossprod((lm.est-softproc[,,ref])*size[ref]))))
+	
+	}
 	mesh<-shapelist[[1]]
 	mesh$vb[1:3,]<-vb	
-	return(list(mesh=mesh,lm.est=lm.est))
+	return(list(mesh=mesh,lm.est=lm.est,weights=wcalc$data,meandev=meandev))
 	#weights<-wcalc$data[,4]
 	
 }
---
+++ R/mc.unify.list.r
@@ -0,0 +1,30 @@
+mc.unify.list<-function(meshlist,proc,ref)
+{	
+	n<-dim(proc)[[3]]
+	uni.list<-list()
+	nam<-names(meshlist)
+	namref<-dimnames(proc)[[3]][ref]
+	jref<-grep(namref,nam)
+	njref<-names(meshlist)[[jref]]
+	
+	
+	for (i in 1:n)
+	{	nm0<-dimnames(proc)[[3]][i]
+		j<-grep(nm0,nam)
+		nj<-names(meshlist)[[j]]
+		if (i == ref)
+			{uni.list[[i]]<-meshlist[[j]]
+			
+			}
+		else
+			{uni.list[[i]]<-mc.unify.mesh(meshlist[[jref]],meshlist[[j]],proc[,,ref],proc[,,i])$unimesh
+			}
+		
+		
+		names(uni.list)[i]<-nj
+		#print(j)
+	}
+		
+		return(uni.list)
+}
+
