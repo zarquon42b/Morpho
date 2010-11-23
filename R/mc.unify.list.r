7cffe0aaa057012ca7ae373dded95af02846cc75
zarquon42b <zarquon42b@users.sourceforge.net>
2010-11-23 18:08:08 +0100

    mc.unify.list now unifys from files on HDD.

--- R/mc.unify.list.r
+++ R/mc.unify.list.r
@@ -1,30 +1,42 @@
-mc.unify.list<-function(meshlist,proc,ref)
+mc.unify.list<-function(surpath="sur",lm.data,ref,ext=".ply")
 {	
-	n<-dim(proc)[[3]]
+	n<-dim(lm.data)[[3]]
+	lmnames<-dimnames(lm.data)[[3]]
 	uni.list<-list()
-	nam<-names(meshlist)
-	namref<-dimnames(proc)[[3]][ref]
-	jref<-grep(namref,nam)
-	njref<-names(meshlist)[[jref]]
+	proclist<-list()
+	meshlist<-list()
+	proc<-mc.procGPA(lm.data,CSinit=TRUE)
+	#nam<-names(meshlist)
+	#namref<-dimnames(proc)[[3]][ref]
+	#jref<-grep(namref,nam)
+	#njref<-names(meshlist)[[jref]]
 	
+	### read meshes from file ###	
+	for (i in 1:n)
+		{proclist[[i]]<-file2mesh(paste("sur/",lmnames[i],ext,sep=""))
+		
+	### create superimposed meshes ### 
+		meshlist[[i]]<-rotmesh.onto(proclist[[i]],lm.data[,,i],proc$rotated[,,i],scale=TRUE)$mesh
+		}
 	
 	for (i in 1:n)
-	{	nm0<-dimnames(proc)[[3]][i]
-		j<-grep(nm0,nam)
-		nj<-names(meshlist)[[j]]
+	{	#nm0<-dimnames(proc)[[3]][i]
+		#j<-grep(nm0,nam)
+		#nj<-names(meshlist)[[j]]
 		if (i == ref)
-			{uni.list[[i]]<-meshlist[[j]]
+			{uni.list[[i]]<-meshlist[[i]]
 			
 			}
 		else
-			{uni.list[[i]]<-mc.unify.mesh(meshlist[[jref]],meshlist[[j]],proc[,,ref],proc[,,i])$unimesh
+			{uni.list[[i]]<-mc.unify.mesh(meshlist[[ref]],meshlist[[i]],proc$rotated[,,ref],proc$rotated[,,i])$unimesh
 			}
 		
 		
-		names(uni.list)[i]<-nj
-		#print(j)
-	}
 		
-		return(uni.list)
+		
+	}
+		names(uni.list)<-lmnames
+		names(proclist)<-lmnames
+		return(list(uni.list=uni.list,proclist=proclist))
 }
 
