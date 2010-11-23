4ba1ff7854303fe3f0d4871cf4d2760c47d4f1eb
zarquon42b <zarquon42b@users.sourceforge.net>
2010-11-23 17:31:42 +0100

    added updatenormals

--- R/estShape.r
+++ R/estShape.r
@@ -7,6 +7,7 @@ estShape<-function(shapelist,procrot,n,ref,softproc=NULL,size=NULL)
 	meandev<-NULL
 	for (i in 1:n)
 		{vb<-vb+shapelist[[wcalc$data$nr[i]]]$vb[1:3,]*wcalc$data$weight[i]
+		print(names(shapelist)[wcalc$data$nr[i]])
 		}
 	
 	
@@ -17,14 +18,21 @@ estShape<-function(shapelist,procrot,n,ref,softproc=NULL,size=NULL)
 	
 	lms<-softproc[,,wcalc$data$nr]
 	lm.est<-matrix(0,dim(softproc)[1],3)
+	if (i > 1)
+	{	
 	for (i in 1:n)
 		{lm.est<-lm.est+lms[,,i]*wcalc$data$weight[i]
 		}
+	}
+	else 
+	{lm.est<-lms
+	}
 	meandev<-mean(sqrt(diag(tcrossprod((lm.est-softproc[,,ref])*size[ref]))))
 	
 	}
 	mesh<-shapelist[[1]]
-	mesh$vb[1:3,]<-vb	
+	mesh$vb[1:3,]<-vb
+	mesh<-adnormals(mesh)	
 	return(list(mesh=mesh,lm.est=lm.est,weights=wcalc$data,meandev=meandev))
 	#weights<-wcalc$data[,4]
 	
