92710fad9eb3c0a3248d2da82e28c280b3c9c18a
schlager <schlager@anthro-hp-1.(none)>
2010-12-02 12:08:52 +0100

    added (mc.)patch2surf

---
+++ R/patch2surf.r
@@ -0,0 +1,29 @@
+patch2surf<-function(patch,lm.ref,lm.tar,tarmesh,ray=FALSE,refmesh=NULL,tol=NULL,clean=TRUE)
+	
+{ 	if (is.null(tol))
+		{tol<-1e3
+		}
+	est0<-tps3d(patch,lm.ref,lm.tar)
+	if (ray)	
+		{	if (is.null(refmesh))
+				{stop("please enter reference mesh to estimate normals")
+				}
+			if (is.character(refmesh))
+				{	refmesh_in<-file2mesh(refmesh)
+				}
+			est.mesh<-warp.mesh(refmesh_in,lm.ref,lm.tar,updateNormals=FALSE)
+			mesh2ply(est.mesh,"est.mesh")
+			patch_upd<-proj.read(est0,"est.mesh.ply",clean=FALSE)
+			system(paste("triray_project out_cloud.ply",tarmesh,tol,sep=" "))
+			patchpro<-ply2mesh("out_ray.ply")
+		
+		}
+	else
+	{
+	patchpro<-proj.read(est0,tarmesh,readnormals=F)
+	}
+	if (clean)
+		{unlink(c("est.mesh.ply","out_cloud.ply","out_ray.ply"))
+		}
+	return(patchpro)
+}
