c40f2d6a42104c03c9a1735755dc23d43e072f10
schlager <schlager@anthro-hp-1.(none)>
2010-12-03 15:21:04 +0100

    fixed bug in rotonmat.

--- R/rotonmat.r
+++ R/rotonmat.r
@@ -1,10 +1,12 @@
 rotonmat<-function(X,refmat,tarmat,scale=TRUE)
 {	
 	ro<-rotonto(tarmat,refmat,scaling=scale,signref=F)
+	
 	Xrot<-t(apply(X,1,function(x){x-ro$transy}))%*%ro$gamm
 	if (scale)
 		{sf<-c.size(refmat)/c.size(ro$yrot)
 		Xrot<-Xrot/sf
 		}
+	Xrot<-t(apply(Xrot,1,function(x){x+ro$trans}))
 	return(Xrot)
 }
