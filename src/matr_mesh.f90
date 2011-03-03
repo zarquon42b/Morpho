4829947e388949653720181708e588cec5d3013f
schlager <schlager@anthro-hp-1.(none)>
2011-03-03 10:14:21 +0100

    slight changes and removed obsolete r-code

--- src/matr_mesh.f90
+++ src/matr_mesh.f90
@@ -3,14 +3,15 @@ SUBROUTINE matr_mesh(matr,nmat,VB,nvb,IT,nit,dif,fptr,outmatr,regionv)
 IMPLICIT NONE
 integer ::nit, IT(3,nit),ittmp(3),i,nvb,nmat,fptr(nmat),ptrtmp,regionv(nmat)
 integer:: region
-real*8 :: point(3),clost(3),VB(3,nvb),normals(3,3),dif(nmat)
-real*8 :: DAT(nit,12),matr(nmat,3),diff,outmatr(nmat,3)
- 
+real*8 :: clost(3),VB(3,nvb),normals(3,3),dif(nmat)
+real*8 :: DAT(nit,12),diff,outmatr(nmat,3)
+real*8 ::  matr(nmat,3)
+real*8 :: point(3)
  call updateSearch(VB,nvb,IT,nit,DAT)
  
  do i = 1,nmat
 
-    point(:) = matr(i,:)
+    point(:) = matr(i,1:3)
     
     call pt_upmesh(point,DAT,nit,clost,diff,ptrtmp,region)
     outmatr(i,:) = clost(:)
