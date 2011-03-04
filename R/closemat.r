4b8d2b52c4ebd4e03310185efb6ddd998d5500e6
schlager <schlager@anthro-hp-1.(none)>
2011-03-04 15:38:37 +0100

    added mc.mesh_mesh

--- R/closemat.r
+++ R/closemat.r
@@ -79,7 +79,59 @@ mc.closemat <- function(matr,mesh,cores=getOption("cores"))
     return(list(clost=outmat,dist=dist))
   }
 
+mc.mesh_mesh <- function(refmesh,mesh,cores=getOption("cores"))
+  {
+    if (is.null(cores))
+    {cores=3
+   }
+    mclist <- list()
+    matr <- t(refmesh$vb[1:3,])
+    vb <- (mesh$vb[1:3,])
+    it <- (mesh$it)
+    nvb <- dim(vb)[2]
+    nit <- dim(it)[2]
+    nmat<-dim(matr)[1]
+    dif<-rep(0,nmat)
+    fptr <- dif
+    clost <- c(0,0,0)
 
+    iter <- as.integer(nmat/cores)
+    for (i in 1:(cores-1))
+      {mclist[[i]] <- matr[(1:iter)+((i-1)*iter),]
+     }
+    mclist[[cores]] <-  matr[-c(1:((cores-1)*iter)),]
+   
+
+    # set variable storage mode
+    storage.mode(fptr) <- "integer"
+    storage.mode(it) <- "integer"
+    storage.mode(nvb) <- "integer" 
+    storage.mode(nit) <- "integer"
+    storage.mode(nmat) <- "integer"
+    storage.mode(matr) <- "double"
+    storage.mode(point) <- "double"
+    storage.mode(vb) <- "double"
+    storage.mode(dif) <- "double"
+    storage.mode(clost) <- "double"
+
+    outmatr <- matr
+    region <- fptr
+    mcfun1 <-function(x)
+      {out <- closemat(x,mesh)
+       return(out)
+     }
+    mclist <- mclapply(mclist,mcfun1)
+    gc()
+    outmat <- NULL
+    dist <- NULL
+    for (i in 1:cores)
+      {outmat <- rbind(outmat,mclist[[i]][[9]])
+       dist <- c(dist,mclist[[i]][[7]])
+     }
+    outmesh <- refmesh
+    outmesh$vb[1:3,] <- t(outmat)
+    return(list(clost=outmat,dist=dist,mesh=outmesh))
+  }
 mesh_mat <- function(mesh1,mesh2,toldist=1,rhotol=0.5)
 {
    if (is.null(mesh1$normals))
