7839681e81b7b3523a8e5594ea826cc5d4ba56c6
schlager <schlager@anthro-hp-1.(none)>
2011-03-18 10:17:38 +0100

    added option inbound and strict to ray2mesh added inflate option to place.patch to evade thin walled mesh problems

--- R/place.patch.r
+++ R/place.patch.r
@@ -1,27 +1,53 @@
-place.patch <- function(dat.array,path,atlas.mesh,atlas.lm,lm,which.fix,prefix="skull_",tol=tol,split=1000,ray=T,outlines=NULL,SMvector=NULL,deselect=TRUE)
+place.patch <- function(dat.array,path,atlas.mesh,atlas.lm,patch,which.fix,prefix="skull_",tol=5,ray=T,outlines=NULL,SMvector=NULL,deselect=TRUE,inflate=NULL)
   {
     n <- dim(dat.array)[3]
-    lm.dim <- dim(lm)
-    out <- array(NA,dim=c(lm.dim,n))
+    k <- dim(dat.array)[1]
+    patch.dim <- dim(patch)[1]
+    out <- array(NA,dim=c((patch.dim+k),3,n))
     dimnames(out) <- dimnames(dat.array)
     
     L <- CreateL(atlas.lm)
-        
+    L1 <- CreateL(rbind(atlas.lm,patch))
     name <- dimnames(dat.array)[[3]]
     for(i in 1:n){
-
       
       tmp.name <- paste(path,prefix,name[i],".ply",sep="")
       tmp.data <- proj.read(dat.array[,,i],tmp.name,readnormals=TRUE)
+
+      ### relax existing curves against atlas ###
+      if (!is.null(SMvector))
+        {
+          sm <- SMvector
+          U<-calcTang_U_s(t(tmp.data$vb[1:3,]),t(tmp.data$normals[1:3,]),SMvector=SMvector,outlines=outlines,surface=NULL,deselect=deselect)
+          slide <- calcGamma(U$Gamma0,L$Lsubk3,U$U,dims=3)$Gamatrix
+          tps.lm <- tps3d(patch,atlas.lm,slide)
+        }
+      else
+        {
+          sm <- 1:k
+        }
+      if (!is.null(inflate)) ### use for mullitlayer meshes to avoid projection inside
+        {
+              atlas.warp <- warp.mesh(atlas.mesh,atlas.lm,slide)
+              tps.lm <- proj.read(tps.lm,atlas.warp,readnormals=TRUE)
+              tps.lm$vb[1:3,] <- tps.lm$vb[1:3,]+inflate*tps.lm$normals[1:3,]
+              tps.lm <- ray2mesh(tps.lm,tmp.name,inbound=TRUE,tol=tol)
+              relax <- rbind(slide,t(tps.lm$vb[1:3,]))
+              normals <- rbind(slide,t(tps.lm$normals[1:3,]))
+              
+              U1 <-calcTang_U_s(relax,normals,SMvector=sm,outlines=outlines,surface=c((k+1):(patch.dim+k)),deselect=deselect)                       
+              tps.lm <- calcGamma(U1$Gamma0,L1$Lsubk3,U1$U,dims=3)$Gamatrix[c((k+1):(patch.dim+k)),]
+              tps.lm <- proj.read(tps.lm,tmp.name,readnormals=FALSE)
+          
+            }
       
-      U<-calcTang_U_s(t(tmp.data$vb[1:3,]),t(tmp.data$normals[1:3,]),SMvector=SMvector,outlines=outlines,surface=NULL,deselect=deselect)
-      slide <- calcGamma(U$Gamma0,L$Lsubk3,U$U,dims=3)$Gamatrix
-      tps.lm <- tps3d(lm,atlas.lm,slide)
-      tps.lm <- proj.read(tps.lm,tmp.name,readnormals=FALSE)
+      else ### just project warped patch on surface (suitable for singlelayer meshes)
+        {
+          tps.lm <- proj.read(tps.lm,tmp.name,readnormals=FALSE)
+        }
       
-      dat <- tps.lm
-     
-      out[,,i] <- dat
+          
+      out[,,i] <-rbind(dat.array[,,i],tps.lm)
 
     }
     return(out)
--- R/ray2mesh.r
+++ R/ray2mesh.r
@@ -1,6 +1,13 @@
-ray2mesh<-function(mesh1,tarmesh,tol=1,clean=TRUE,outname=NULL,readback=TRUE)
+ray2mesh<-function(mesh1,tarmesh,tol=1,clean=TRUE,outname=NULL,readback=TRUE,inbound=FALSE,strict=FALSE)
 {	
-	
+	options <- NULL
+        if (inbound == TRUE)
+          {
+            options <- "--inbound" # set option to search along negative nromals first
+          }
+        if (strict == TRUE)
+          {options <- paste(options,"--strict") #mark vertices that are not hit along rays
+         }
 	if (is.null(outname))
 		{outname<-"project.mesh.ply"
 		}
@@ -8,11 +15,11 @@ ray2mesh<-function(mesh1,tarmesh,tol=1,clean=TRUE,outname=NULL,readback=TRUE)
 	mesh2ply(mesh1,"reference")
         if (is.null(mesh1$it))
           {
-            system(paste("rayproject reference.ply target.ply -cloud -t ",tol,"-o ",outname,sep=""))
+            system(paste("rayproject reference.ply target.ply -cloud ",options,"-t ",tol,"-o ",outname,sep=""))
           }
         else
           {
-            system(paste("rayproject reference.ply target.ply -t ",tol,"-o ",outname,sep=""))
+            system(paste("rayproject reference.ply target.ply ",options," -t ",tol,"-o ",outname,sep=""))
           }
           
 	outmesh<-ply2mesh(outname,readnormals=TRUE)
