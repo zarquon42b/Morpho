443be3b71e68d2c4f3d87644f813e468e0a96a81
schlager <schlager@anthro-hp-1.(none)>
2011-03-18 15:32:19 +0100

    added place.patch to check visually, if automatic patch application was succesful

--- R/place.patch.r
+++ R/place.patch.r
@@ -9,47 +9,46 @@ place.patch <- function(dat.array,path,atlas.mesh,atlas.lm,patch,which.fix,prefi
     L <- CreateL(atlas.lm)
     L1 <- CreateL(rbind(atlas.lm,patch))
     name <- dimnames(dat.array)[[3]]
-    for(i in 1:n){
-      
-      tmp.name <- paste(path,prefix,name[i],".ply",sep="")
-      tmp.data <- proj.read(dat.array[,,i],tmp.name,readnormals=TRUE)
 
-      ### relax existing curves against atlas ###
-      if (!is.null(SMvector))
-        {
-          sm <- SMvector
-          U<-calcTang_U_s(t(tmp.data$vb[1:3,]),t(tmp.data$normals[1:3,]),SMvector=SMvector,outlines=outlines,surface=NULL,deselect=deselect)
-          slide <- calcGamma(U$Gamma0,L$Lsubk3,U$U,dims=3)$Gamatrix
-          tps.lm <- tps3d(patch,atlas.lm,slide)
-        }
-      else
-        {
-          sm <- 1:k
-        }
-      if (!is.null(inflate)) ### use for mullitlayer meshes to avoid projection inside
-        {
-              atlas.warp <- warp.mesh(atlas.mesh,atlas.lm,slide)
-              tps.lm <- proj.read(tps.lm,atlas.warp,readnormals=TRUE)
-              tps.lm$vb[1:3,] <- tps.lm$vb[1:3,]+inflate*tps.lm$normals[1:3,]
-              tps.lm <- ray2mesh(tps.lm,tmp.name,inbound=TRUE,tol=tol)
-              relax <- rbind(slide,t(tps.lm$vb[1:3,]))
-              normals <- rbind(slide,t(tps.lm$normals[1:3,]))
-              
-              U1 <-calcTang_U_s(relax,normals,SMvector=sm,outlines=outlines,surface=c((k+1):(patch.dim+k)),deselect=deselect)                       
-              tps.lm <- calcGamma(U1$Gamma0,L1$Lsubk3,U1$U,dims=3)$Gamatrix[c((k+1):(patch.dim+k)),]
-              tps.lm <- proj.read(tps.lm,tmp.name,readnormals=FALSE)
+    for(i in 1:n)
+      {
+        tmp.name <- paste(path,prefix,name[i],".ply",sep="")
+        tmp.data <- proj.read(dat.array[,,i],tmp.name,readnormals=TRUE)
+
+### relax existing curves against atlas ###
+        if (!is.null(SMvector))
+          {
+            sm <- SMvector
+            U<-calcTang_U_s(t(tmp.data$vb[1:3,]),t(tmp.data$normals[1:3,]),SMvector=SMvector,outlines=outlines,surface=NULL,deselect=deselect)
+            slide <- calcGamma(U$Gamma0,L$Lsubk3,U$U,dims=3)$Gamatrix
+            tps.lm <- tps3d(patch,atlas.lm,slide)
+          }
+        else
+          {
+            sm <- 1:k
+          }
+### use for mullitlayer meshes to avoid projection inside
+        if (!is.null(inflate))
+          {
+          atlas.warp <- warp.mesh(atlas.mesh,atlas.lm,slide)
+          tps.lm <- proj.read(tps.lm,atlas.warp,readnormals=TRUE)
           
-            }
-      
-      else ### just project warped patch on surface (suitable for singlelayer meshes)
-        {
+          tps.lm$vb[1:3,] <- tps.lm$vb[1:3,]+inflate*tps.lm$normals[1:3,] ###inflate outward along normals
+          tps.lm <- ray2mesh(tps.lm,tmp.name,inbound=TRUE,tol=tol) ### deflate in opposite direction
+          relax <- rbind(slide,t(tps.lm$vb[1:3,]))
+          normals <- rbind(slide,t(tps.lm$normals[1:3,]))
+              
+          U1 <-calcTang_U_s(relax,normals,SMvector=sm,outlines=outlines,surface=c((k+1):(patch.dim+k)),deselect=deselect)                       
+          tps.lm <- calcGamma(U1$Gamma0,L1$Lsubk3,U1$U,dims=3)$Gamatrix[c((k+1):(patch.dim+k)),]
           tps.lm <- proj.read(tps.lm,tmp.name,readnormals=FALSE)
         }
-      
-          
-      out[,,i] <-rbind(dat.array[,,i],tps.lm)
-
-    }
+### just project warped patch on surface (suitable for singlelayer meshes)
+        else
+          {
+            tps.lm <- proj.read(tps.lm,tmp.name,readnormals=FALSE)
+          }
+        out[,,i] <-rbind(dat.array[,,i],tps.lm)
+      }
     return(out)
   }
-      
+
