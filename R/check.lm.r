173b8a8e225c6d435683d4118850beae3acc000d
schlager <schlager@anthro-hp-1.(none)>
2011-05-20 15:53:08 +0200

    Revert "some improvements in check.lm"
    This reverts commit 896b58f77a20f53e5fb378c5996df7285447533b.

--- R/check.lm.r
+++ R/check.lm.r
@@ -1,8 +1,8 @@
-check.lm <- function(dat.array,path,prefix="",suffix=".ply",col=3,radius=1,alpha=0.7,begin=1)
+check.lm <- function(dat.array,path,prefix="",suffix=".ply",col=3,radius=1,alpha=0.7)
   {
     n <- dim(dat.array)[3]
     name <- dimnames(dat.array)[[3]]
-    i <- begin
+    i <- 1
 open3d()
     while (i <= n)
       {
@@ -10,7 +10,7 @@ open3d()
         tmp.name <- paste(path,prefix,name[i],suffix,sep="")
         spheres3d(dat.array[,,i],radius=radius)
         shade3d(file2mesh(tmp.name),col=col,alpha=alpha)
-        answer <- readline(paste("viewing #",i,"next"))
+        answer <- readline("next")
         i <- i+1
 
         rgl.clear()
