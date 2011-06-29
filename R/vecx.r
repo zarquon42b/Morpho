e17aa9654498156b41176a441bdb83a28197fe21
schlager <zarquon42@gmail.com>
2011-06-29 22:14:53 +0200

    dimnames[[3]] of input is added as rownames to output of vecx

--- R/vecx.r
+++ R/vecx.r
@@ -3,10 +3,16 @@ vecx<-function(x)
 	n<-dims[3]
 	k<-dims[1]
 	m<-dims[2]
+   names <- dimnames(x)[[3]]
+   
 	vecs<-matrix(0,n,k*m)
 	for(i in 1:n)
 		{vecs[i,]<-as.vector(x[,,i])
 		}
+   if (!is.null(names))
+     {
+       rownames(vecs) <- names
+     }
 	#vecs<-apply(vecs,2,scale,scale=F)
 	return(vecs)
 }
