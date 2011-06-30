07f591b67c5870bc0eca5b3ba8fe29347ec88067
schlager <zarquon42@gmail.com>
2011-07-01 00:22:34 +0200

    added ask option to fq

--- R/fq.r
+++ R/fq.r
@@ -1,5 +1,25 @@
-fq <- function(image=TRUE,history=FALSE)
+fq <- function(ask=TRUE,image=TRUE,history=FALSE)
   {
+    if (ask)
+      ans <- 3
+      { while (! ans%in% c(0:2))
+        {
+          ans <- readline(" save nothing: 0 \n save workspace: 1\n save workspace and history:2\n")
+        }
+        if (ans == 0)
+          {image <- FALSE
+           history <- FALSE
+         }
+        else if (ans == 1)
+          { image <- TRUE
+           history <- FALSE
+          }
+         else if (ans == 2)
+           { image <- TRUE
+           history <- TRUE
+          }
+      }
+        
     if (image)
       {
       save.image()
