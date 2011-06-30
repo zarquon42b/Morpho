d4269715d160d3ae8840ee4f7a997b6168e72980
schlager <zarquon42@gmail.com>
2011-06-30 23:51:12 +0200

    added qf - a custom exit command forcing quit R on Linux

---
+++ R/qf.r
@@ -0,0 +1,12 @@
+qf <- function(image=TRUE,history=FALSE)
+  {
+    if (image)
+      {
+      save.image()
+    }
+    if (history)
+      {  savehistory()
+       }
+    pid <- Sys.getpid()
+    system(paste("kill -9 ",pid))
+  }
