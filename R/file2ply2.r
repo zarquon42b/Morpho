6710d4a73833d97173d1897c4dbec9842f234223
schlager <schlager@anthro-hp-1.(none)>
2011-07-18 09:45:11 +0200

    moved all functions related to crestlines to file "crestlines.r"

--- R/file2ply2.r
+++
@@ -1,5 +0,0 @@
-file2ply2 <- function(filein,fileout,curvature=TRUE,neighbour=2)
-  {
-    tmpmesh <- file2mesh(filein)
-    mesh2ply2(tmpmesh,filename=fileout,curvature=curvature,neighbour=neighbour)
-  }
