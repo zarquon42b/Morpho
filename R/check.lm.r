cc410e374fa9944dba7004b6c068c79f69fce4ef
schlager <schlager@anthro-hp-1.(none)>
2011-08-02 15:47:28 +0200

    added possibility not to adress surfaces in check.lm

--- R/check.lm.r
+++ R/check.lm.r
@@ -1,4 +1,4 @@
-check.lm <- function(dat.array,path,prefix="",suffix=".ply",col=3,radius=1,alpha=0.7,begin=1)
+check.lm <- function(dat.array,path=NULL,prefix="",suffix=".ply",col=3,radius=1,alpha=0.7,begin=1)
   {
     n <- dim(dat.array)[3]
     name <- dimnames(dat.array)[[3]]
@@ -9,7 +9,10 @@ open3d()
         
         tmp.name <- paste(path,prefix,name[i],suffix,sep="")
         spheres3d(dat.array[,,i],radius=radius)
-        shade3d(file2mesh(tmp.name),col=col,alpha=alpha)
+        if (!is.null(path))
+          {
+            shade3d(file2mesh(tmp.name),col=col,alpha=alpha)
+          }
         answer <- readline(paste("viewing #",i,"next"))
         i <- i+1
 
