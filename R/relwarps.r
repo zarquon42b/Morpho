a0df41eb5fa1f15439ca20ca67c46b1fa1d99c7f
Stefan <zarquon42@gmail.com>
2012-02-22 10:36:09 +0100

    added option "silent" to sc.procGPA to suppress output of time elapsed. Finished relWarps, wrote documentation and crosschecked results with output from othe software.

--- R/relwarps.r
+++ R/relwarps.r
@@ -1,151 +1,107 @@
 relWarps<-function(data,scale=TRUE,CSinit=TRUE,alpha=1,tol=1e-10,orp=TRUE)
-{	n<-dim(data)[3]
-	m<-dim(data)[2]
-	k<-dim(data)[1]
-
-	### superimpose data ###
-	proc<-sc.procGPA(data,scale=scale,CSinit=CSinit)
-	if (orp)
-          {
-            proc$rotated<-orp(proc$rotated)
-          }
-        proc$mshape <- apply(proc$rotated,1:2,mean)
-	### create bending energy matrix ###
-	if (m==2)
-		{BE<-CreateL2D(proc$mshape)$Lsubk
-		}
-	else
-		{BE<-(CreateL(proc$mshape)$Lsubk)
-		}
-	### vectorize and scale superimposed data ###
-	vecs<-matrix(0,n,k*m)
-	for(i in 1:n)
-		{vecs[i,]<-as.vector(proc$rotated[,,i])
-		}
-	vecs<-apply(vecs,2,scale,scale=F)
-	
-	### generate covariance matrix of superimposed data ###
-	Sc<-cov(vecs)
-	
-	### explore eigenstructure of BE ###	
-	eigBE<-eigen(BE,symmetric=TRUE)
-	
-	eigBE$values<-Re(eigBE$values)
-	eigBE$vectors<-Re(eigBE$vectors)
-	
-	zero<-which(eigBE$values<tol)
-	diaginv<-diagBE<-eigBE$values*0
-	diagBE[-zero]<-eigBE$values[-zero]^(-alpha/2)
-	diaginv[-zero]<-eigBE$values[-zero]^(alpha/2)
-	IM<-diag(rep(1,m))
-	
-	if (alpha !=0)
-		{	
-		BE2<-IM%x%(eigBE$vectors%*%diag(diagBE)%*%t(eigBE$vectors))
-	
-		invBE2<-IM%x%(eigBE$vectors%*%diag(diaginv)%*%t(eigBE$vectors))
-		}
-	else
-		{BE2<-diag(rep(1,k*m))
-		invBE2<-BE2
-		}
-
-	### generate covariance structure of scaled space ###
-		covcom<-BE2%*%Sc%*%BE2	
-		eigCOVCOM<-eigen(covcom,symmetric=TRUE)
-		nonz<-which(eigCOVCOM$values>tol)
-		bescores<-t(t(eigCOVCOM$vectors[,nonz])%*%BE2%*%t(vecs))
-
-	
-	### calculate uniform component scores ###
-		U<-NULL
-		uniscores<-NULL
-        
-	if (m==0)
-		{
-                  rotms<-eigen(crossprod(proc$mshape))$vectors
-                  if (det(rotms) < 0)
-                    {rotms[,1]<-rotms[,1]*-1
-                                        #rotms<-rotms*c(1,-1)#%*%matrix(c(0,-1,1,0),2,2)
-                   }
-		print(dim(rotms))
-		msrot<-(proc$mshape%*%rotms)/cSize(proc$mshape)
-		al<-sum(msrot[,1]^2)
-		be<-sum(msrot[,2]^2)
-		ga<-sqrt(al*be)
-		u1<-c(al*msrot[,2],be*msrot[,1])/ga
-		u2<-c(-be*msrot[,1],al*msrot[,2])/ga
-                  U<-cbind(u1,u2)
-                 
-                  uniscores<-vecs%*%U
-		}
-        else
-          { met="r1"
-            vecData <- vecx(proc$rotated)
-            if (met=="r1")
-              {
+{
+  n<-dim(data)[3]
+  m<-dim(data)[2]
+  k<-dim(data)[1]
+  datanames <- dimnames(data)[[3]]
+### superimpose data ###
+  proc<-sc.procGPA(data,scale=scale,CSinit=CSinit,silent=TRUE)
+  if (orp)
+    {
+      proc$rotated<-orp(proc$rotated)
+      proc$mshape <- apply(proc$rotated,1:2,mean)
+    }
+  
+  
+### create bending energy matrix ###
+  if (m==2)
+    {
+      BE<-CreateL2D(proc$mshape)$Lsubk
+    }
+  else
+    {
+      BE<-(CreateL(proc$mshape)$Lsubk)
+    }
+### vectorize and scale superimposed data ###
+  vecs <- vecx(proc$rotated)
+  vecs<-apply(vecs,2,scale,scale=F)
+  rownames(vecs) <- datanames
+  
+### generate covariance matrix of superimposed data ###
+  Sc<-cov(vecs)
+  
+### explore eigenstructure of BE ###	
+  eigBE<-eigen(BE,symmetric=TRUE)
+  eigBE$values<-Re(eigBE$values)
+  eigBE$vectors<-Re(eigBE$vectors)
+  
+  zero<-which(eigBE$values<tol)
+  diaginv<-diagBE<-eigBE$values*0
+  diagBE[-zero]<-eigBE$values[-zero]^(-alpha/2)
+  diaginv[-zero]<-eigBE$values[-zero]^(alpha/2)
+  IM<-diag(rep(1,m))
+  
+  if (alpha !=0)
+    {	
+      BE2<-IM%x%(eigBE$vectors%*%diag(diagBE)%*%t(eigBE$vectors))
+      invBE2<-IM%x%(eigBE$vectors%*%diag(diaginv)%*%t(eigBE$vectors))
+    }
+  else
+    {
+      BE2<-diag(rep(1,k*m))
+      invBE2<-BE2
+    }
+  
+### generate covariance structure of scaled space ###
+  covcom<-BE2%*%Sc%*%BE2	
+  eigCOVCOM<-eigen(covcom,symmetric=TRUE)
+  nonz<-which(eigCOVCOM$values>tol)
+  bescores<-t(t(eigCOVCOM$vectors[,nonz])%*%BE2%*%t(vecs))
+  rownames(bescores) <- rownames(vecs)
+  bePCs <- IM %x% eigBE$vectors
+  bePCs <-bePCs %*% diag(rep(diaginv,3)) %*% t(bePCs) %*%  eigCOVCOM$vectors
+  
+### calculate uniform component scores ###
+  U<-NULL
+  uniscores<-NULL
+  
 ### Rohlf first method ###
-                
-                E <- eigBE$vectors[,-zero]
-                N <- diag(rep(1,k))-E%*%solve(crossprod(E))%*%t(E)
-                V <-vecs
-                NIk <- matrix(0,k*m,k*m)
-               # NIk <- kronecker(E,diag(rep(1,m)))
-                
-                for (i in 1:m)
-                 {
-                    
-                    NIk[((i-1)*k+1):(i*k),((i-1)*k+1):(i*k)] <- N
-                 }
-                 
-                svdBend <- svd(V%*%NIk)
-                 #print(dim(svdBend$v))
-                LS <- svdBend$u%*%diag(svdBend$d)
-              
-                uniscores <- LS[,1:(m+0.5*m*(m-1)-1)]
-              }
-            else
-              {
-####Rohlf second method
-          
-                Bxyz <- NULL
-                tXcInv <- solve(crossprod(proc$mshape))
-                for(i in 1:m)
-                  {  
-                    Bxyz <- cbind(Bxyz,t(tXcInv%*%t(proc$mshape)%*%t(vecData[,((i-1)*k+1):(i*k)])))
-                    print(((i-1)*k+1):(i*k))
-                  }
-                Bend <- cbind(t(Bxyz[[1]]),t(Bxyz[[2]]),t(Bxyz[[3]]))
-                                        # print(dim(Bxyz))
-            svdBend <- svd(Bxyz)
-                LS <- svdBend$u%*%diag(svdBend$d)
-                print(dim(LS))
-                uniscores <- LS[,1:(m+0.5*m*(m-1)-1)]
-              }
-          }
-        
-
+  
+  E <- eigBE$vectors[,-zero]
+  N <- diag(rep(1,k))-E%*%solve(crossprod(E))%*%t(E)
+  V <-vecs
+  NIk <- IM %x% N
+   
+  svdBend <- svd(V%*%NIk)
+  LS <- svdBend$u%*%diag(svdBend$d)
+  uniscores <- LS[,1:(m+0.5*m*(m-1)-1)]
+  rownames(uniscores) <- datanames   
+  
 ### create Variance table according to eigenvalues ###
-		values<-eigCOVCOM$values[nonz]
-		if (length(values)==1)
-          	{Var<-values}
-        else
-        	{
-          	Var<-matrix(NA,length(values),3)
-          	Var[,1]<-values
-        
-          	for (i in 1:length(values))
-            		{
-              		Var[i,2]<-(values[i]/sum(values))*100
-            		}
-          	Var[1,3]<- Var[1,2]
-          	for (i in 2:length(values))
-           		{         
-             		Var[i,3]<-Var[i,2]+ Var[i-1,3]
-            		}
-          	colnames(Var)<-c("eigenvalues","% Variance","Cumulative %")
-        	}
+  values<-eigCOVCOM$values[nonz]
+  if (length(values)==1)
+    {
+      Var<-values
+    }
+  else
+    {
+      Var<-matrix(NA,length(values),3)
+      Var[,1]<-values
       
-	
-	return(list(bescores=bescores,uniscores=uniscores,BE2=BE2,vecs=vecs,U=U,covSc=eigCOVCOM,invBE2=invBE2,proc=proc,Var=Var,mshape=proc$mshape,rotated=proc$rotated,uniPCs=svdBend$v[,1:(m+0.5*m*(m-1)-1)]))
+      for (i in 1:length(values))
+        {
+          Var[i,2]<-(values[i]/sum(values))*100
+        }
+      Var[1,3]<- Var[1,2]
+      for (i in 2:length(values))
+        {         
+          Var[i,3]<-Var[i,2]+ Var[i-1,3]
+        }
+      colnames(Var)<-c("eigenvalues","% Variance","Cumulative %")
+    }
+  
+  
+  return(list(bescores=bescores,uniscores=uniscores,Var=Var,mshape=proc$mshape,rotated=proc$rotated,bePCs=bePCs,uniPCs=svdBend$v[,1:(m+0.5*m*(m-1)-1)]))
+  
 }
+
