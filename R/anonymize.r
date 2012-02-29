330ef5cfa47abb6af8830ff51faf21ccd5528c3b
schlager <zarquon42@gmail.com>
2012-02-29 14:51:35 +0100

    optimized anonymize for complete replacement of strings and adding suffixes and prefixes

--- R/anonymize.r
+++ R/anonymize.r
@@ -1,4 +1,4 @@
-anonymize <- function(data,path=NULL,dest.path=NULL,suffix=".ply",split="_",remove=NULL,levels=TRUE)
+anonymize <- function(data,path=NULL,dest.path=NULL,ext=".ply",split="_",remove=NULL,levels=TRUE,prefix=NULL,suffix=NULL,sample=TRUE)
 {
   if (length(dim(data)) == 3)
     {
@@ -24,14 +24,27 @@ anonymize <- function(data,path=NULL,dest.path=NULL,suffix=".ply",split="_",remo
     }
   
   alias.frame <- data.frame(matrix(NA,length(orig),faclen))
+  
   for (i in 1:length(orig)){alias.frame[i,] <- alias.tmp[[i]]}
   
   orig.frame <- alias.frame
-  facs <- apply(orig.frame[,remove],1,paste,collapse="_")
+  if (length(remove) > 1)
+    {
+      facs <- apply(orig.frame[,remove],1,paste,collapse="_")
+    }
+  else
+    { facs <- orig.frame[,remove]
+    }
   if (levels)
     {
       levorig <- levels(as.factor(facs))
-      alias.lev <- sample(sprintf("%04d",1:length(levorig)))
+     
+        
+      alias.lev <- paste(prefix,sprintf("%04d",1:length(levorig)),suffix,sep="")
+       if (sample)
+         {
+           alias.lev <- sample(alias.lev)
+         }
       alias <- rep(0,length(orig))
       for (i in 1:length(levorig))
         {
@@ -48,15 +61,27 @@ anonymize <- function(data,path=NULL,dest.path=NULL,suffix=".ply",split="_",remo
     {
       alias.frame <- alias.frame[,-(remove[-1])]
     }
-  alias <- NULL; for (i in 1:length(orig)){alias[i] <- paste(alias.frame[i,],collapse="_")}
+
+  alias <- NULL
+  if (!is.null(dim(alias.frame)))
+    {
+      for (i in 1:length(orig)){alias[i] <- paste(alias.frame[i,],collapse="_")}
+    }
+  else
+    {
+      for (i in 1:length(orig))
+        {
+          alias[i] <-alias.frame[i]
+        }
+    }
   anonymkey <- data.frame(orig,alias)
   if (!is.null(path))
-  {
-    for ( i in 1:length(orig))
-      {
-        system(paste("cp ",path,"/",orig[i],suffix," ", dest.path,"/",alias[i],suffix,sep=""))
-      }
-  }
+    {
+      for ( i in 1:length(orig))
+        {
+          system(paste("cp ",path,"/",orig[i],ext," ", dest.path,"/",alias[i],ext,sep=""))
+        }
+    }
   if (length(dim(data)) == 3)
     {
       dimnames(data)[[3]] <- alias
