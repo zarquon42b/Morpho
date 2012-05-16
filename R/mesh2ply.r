ae4d6c8b0b8e3dee4d13fa7c698e7008dce2aa94
schlager <zarquon42@gmail.com>
2012-05-16 17:44:50 +0200

    added coloring support for mesh2ply

--- R/mesh2ply.r
+++ R/mesh2ply.r
@@ -1,38 +1,39 @@
-mesh2ply<-function(x,filename="default")
+mesh2ply<-function(x,filename="default",col=NULL)
 {	
-	if (is.matrix(x))
-		{x<-list(vb=x)}
-	filename<-paste(filename,".ply",sep="")
-	vert<-x$vb[1:3,]
-	vert<-round(vert,digits=6)
-	if (!is.null(x$it))
-		{face<-x$it-1
-		fd<-3
-		fn<-dim(face)[2]
-		}
-	else 
-		{fn<-0
-		}	
-	vert.all<-vert
-	vn<-dim(vert)[2]
-	vn.all<-3
-	texfile<-x$TextureFile
-		
-	
+
+  if (is.matrix(x))
+    {x<-list(vb=x)}
+  filename<-paste(filename,".ply",sep="")
+  vert<-x$vb[1:3,]
+  vert<-round(vert,digits=6)
+  if (!is.null(x$it))
+    {face<-x$it-1
+     fd<-3
+     fn<-dim(face)[2]
+   }
+  else 
+    {fn<-0
+   }	
+  vert.all<-vert
+  vn<-dim(vert)[2]
+  vn.all<-3
+  texfile<-x$TextureFile
+  
 ### start writing to file ###
-
+  
 ### write header ###
-	cat("ply\nformat ascii 1.0\ncomment MORPHO generated\n",file=filename)
-
-	### check for Texture information and write to header ###	
-	
-	if (is.character(texfile))
-		{cat(paste("comment TextureFile ",texfile,"\n",sep=""),file=filename,append=TRUE) 
-		}
-
-	### write vertex infos to header ###
-	
-	v.info<-paste("element vertex ",vn,"\n","property float x\nproperty float y\nproperty float z\n",sep="")	
+  cat("ply\nformat ascii 1.0\ncomment MORPHO generated\n",file=filename)
+  
+### check for Texture information and write to header ###	
+  
+  if (is.character(texfile))
+    {cat(paste("comment TextureFile ",texfile,"\n",sep=""),file=filename,append=TRUE) 
+   }
+  
+### write vertex infos to header ###
+  
+	v.info<-paste("element vertex ",vn,"\n","property float x\nproperty float y\nproperty float z\n",sep="")
+ 
 	cat(v.info,file=filename,append=TRUE)	
 		if (!is.null(x$normals))
 			{cat("property float nx\nproperty float ny\nproperty float nz\n",file=filename,append=TRUE)
@@ -40,6 +41,11 @@ mesh2ply<-function(x,filename="default")
 			vert.all<-rbind(vert,norma)	
 			vn.all<-6		
 			}
+   if (!is.null(col))    
+     {
+      
+       v.info <- cat("property uchar red\nproperty uchar green\nproperty uchar blue\n",file=filename,append=T)
+     }
 	### write face infos and texture infos to header and finish ###	
 	
 	cat(paste("element face ",fn,"\n",sep=""),file=filename,append=TRUE)	
@@ -51,7 +57,21 @@ mesh2ply<-function(x,filename="default")
 		}
 	
 	### write vertices and vertex normals ###
-	write.table(format(t(vert.all),scientific=F,trim=T),file=filename,sep=" ",append=TRUE, quote = FALSE, row.names = FALSE, col.names = FALSE, na = "")	
+  vert.all <- data.frame(t(vert.all))
+  if (!is.null(col))
+    {
+      if (is.vector(col))
+        {
+          colout <- t(col2rgb(col))
+        }
+      else
+        {
+          colout <- matrix(col2rgb(col),vn,3,byrow=T)
+        }
+      vert.all <- cbind(vert.all,colout)
+    }
+  
+      write.table(format(vert.all,scientific=F,trim=T),file=filename,sep=" ",append=TRUE, quote = FALSE, row.names = FALSE, col.names = FALSE, na = "")	
 
 	
 	### write face and Texture information ###	
--- R/ply2mesh.r
+++ R/ply2mesh.r
@@ -1,74 +1,74 @@
 ply2mesh<-function (filename, adnormals = TRUE,readnormals=FALSE) 
 {
-    x <- filename
-    A <- readLines(x, n = 100)
-    end <- which(A == "end_header")
-    infos <- A[1:end]
-    vertinfo <- strsplit(A[grep("element vertex", infos)], " ")
-    faceinfo <- strsplit(A[grep("element face", infos)], " ")
-	texinfo<-NULL	
-	#if (length(grep("property list uchar float texcoord",A))==1) 
-	qualinfo<-grep("property float quality", infos)
-	if (length(qualinfo)==1)
-	{qualine<-qualinfo-grep("element vertex",infos)
-	qual<-TRUE
-	}
-	else 
-	{qual <- FALSE}
-	fn <- as.numeric(faceinfo[[1]][3])
-    	vn <- as.numeric(vertinfo[[1]][3])
-    	vert.all <- read.table(x, skip = end, sep = " ", nrows = vn)
-    	vert <- apply(vert.all[, 1:3], 2, as.numeric)
-    	vert.n <- NULL
-	quality<-NULL
-    	if (length(grep("property float nx", A)) == 1) {
-        vert.n <- t(vert.all[, 4:6])
-	if (qual)
-		{quality<-as.vector(vert.all[,qualine])}
-    }
-    	
-	if (fn !=0)
-		{
-		face.all <- read.table(x, skip = end + vn, nrows = fn)
-    		face <- face.all[, 2:4]+1
-		mesh <- list(vb = rbind(t(vert), 1), it = t(face), primitivetype = "triangle", 
-        	material = NULL,normals = vert.n)
-		class(mesh) <- c("mesh3d", "shape3d")
-		}
-	else
-		{if (is.null(vert.n) || readnormals==FALSE)
-			{cat(paste("mesh contains no faces. Vertices will be stored in a",vn,"x 3 matrix\n"))
-			mesh<-vert}	
-		else if (readnormals)
-			{cat(paste("mesh contains no faces. vertices and vertex normals are stored in a list\n"))
-			mesh<-list(vb=t(vert),normals=vert.n)
-			}	
-			
-		}
-### generate object of class mesh3d ###	
-	
-	
 
-### add TexCoords ###
-	if (length(grep("property list uchar float texcoord",A))==1 && length(grep("comment TextureFile",A))==1)
-		{texn<-face.all[1,5]
-		tex<-face.all[,c(6:(6+(texn-1)))]
-		mesh$tex<-t(tex)
-		mesh$TextureFile<-strsplit(A[grep("comment TextureFile", infos)], " ")[[1]][3]
-		} 
-    
-	
-   
-### check for normals and update if required ###
-	if (fn !=0)	
-		{if (adnormals && is.null(mesh$normals) ) {
-        	cat("calculating normals...\n")
-        	mesh <- adnormals(mesh)	
-		}
+  x <- filename
+  A <- readLines(x, n = 100)
+  end <- which(A == "end_header")
+  infos <- A[1:end]
+  vertinfo <- strsplit(A[grep("element vertex", infos)], " ")
+  faceinfo <- strsplit(A[grep("element face", infos)], " ")
+  texinfo<-NULL	
+                                        #if (length(grep("property list uchar float texcoord",A))==1) 
+  qualinfo<-grep("property float quality", infos)
+  if (length(qualinfo)==1)
+    {qualine<-qualinfo-grep("element vertex",infos)
+     qual<-TRUE
+   }
+  else 
+    {qual <- FALSE}
+  fn <- as.numeric(faceinfo[[1]][3])
+  vn <- as.numeric(vertinfo[[1]][3])
+  vert.all <- read.table(x, skip = end, sep = " ", nrows = vn)
+  vert <- apply(vert.all[, 1:3], 2, as.numeric)
+  vert.n <- NULL
+  quality<-NULL
+  if (length(grep("property float nx", A)) == 1) {
+    vert.n <- t(vert.all[, 4:6])
+    if (qual)
+      {quality<-as.vector(vert.all[,qualine])}
+  }
+  
+  if (fn !=0)
+    {
+      face.all <- read.table(x, skip = end + vn, nrows = fn)
+      face <- face.all[, 2:4]+1
+      mesh <- list(vb = rbind(t(vert), 1), it = t(face), primitivetype = "triangle", material = NULL,normals = vert.n)
+      class(mesh) <- c("mesh3d", "shape3d")
     }
-	if (qual && readnormals)
-		{mesh$quality<-quality
-		}
-    return(mesh)
+  else
+    {if (is.null(vert.n) || readnormals==FALSE)
+       {cat(paste("mesh contains no faces. Vertices will be stored in a",vn,"x 3 matrix\n"))
+        mesh<-vert}	
+    else if (readnormals)
+      {cat(paste("mesh contains no faces. vertices and vertex normals are stored in a list\n"))
+       mesh<-list(vb=t(vert),normals=vert.n)
+     }	
+     
+   }
+### generate object of class mesh3d ###	
+  
+  
+  
+### add TexCoords ###
+  if (length(grep("property list uchar float texcoord",A))==1 && length(grep("comment TextureFile",A))==1)
+    {texn<-face.all[1,5]
+     tex<-face.all[,c(6:(6+(texn-1)))]
+     mesh$tex<-t(tex)
+     mesh$TextureFile<-strsplit(A[grep("comment TextureFile", infos)], " ")[[1]][3]
+   } 
+  
+  
+  
+### check for normals and update if required ###
+  if (fn !=0)	
+    {if (adnormals && is.null(mesh$normals) ) {
+      cat("calculating normals...\n")
+      mesh <- adnormals(mesh)	
+    }
+   }
+  if (qual && readnormals)
+    {mesh$quality<-quality
+   }
+  return(mesh)
 }
 
