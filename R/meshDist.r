654d34180202b0c923ba73fc8fc893365d84699f
schlager <zarquon42@gmail.com>
2012-05-18 18:27:22 +0200

    added rgl rendering option in meshDist

--- R/meshDist.r
+++ R/meshDist.r
@@ -1,4 +1,4 @@
-meshDist <- function(mesh1,mesh2,from=NULL,to=NULL,steps=20,ceiling=FALSE,file="default",imagedim="100x800",uprange=1,ray=FALSE,raytol=50)
+meshDist <- function(mesh1,mesh2,from=NULL,to=NULL,steps=20,ceiling=FALSE,file="default",imagedim="100x800",uprange=1,ray=FALSE,raytol=50,save=FALSE)
   {
     if(!ray)
       {
@@ -21,10 +21,18 @@ meshDist <- function(mesh1,mesh2,from=NULL,to=NULL,steps=20,ceiling=FALSE,file="
     colseq <- seq(from=from,to=to,length.out=steps)
     coldif <- colseq[2]-colseq[1]
     distqual <- ceiling((dists/coldif)+1e-14)
-    mesh2ply(mesh1,col=ramp[distqual],filename=file)
-    widxheight <- as.integer(strsplit(imagedim,split="x")[[1]])
-    png(filename=paste(file,".png",sep=""),width=widxheight[1],height=widxheight[2])
+    if (save)
+      {
+        mesh2ply(mesh1,col=ramp[distqual],filename=file)
+        widxheight <- as.integer(strsplit(imagedim,split="x")[[1]])
+        png(filename=paste(file,".png",sep=""),width=widxheight[1],height=widxheight[2])
+        image(1,colseq, matrix(data=colseq, ncol=length(colseq),nrow=1),col=ramp,useRaster=T,ylab="Distance in mm",xlab="",xaxt="n")
+        dev.off()
+      }
+    colorall <- ramp[distqual]
+    mesh1$material$color <- apply(mesh1$it,1:2,function(x){x <- colorall[x];return(x)})
+    shade3d(mesh1)
     image(1,colseq, matrix(data=colseq, ncol=length(colseq),nrow=1),col=ramp,useRaster=T,ylab="Distance in mm",xlab="",xaxt="n")
-    dev.off()
-    return(list(dists=dists,cols=ramp[distqual]))
+      
+    return(list(colMesh=mesh1,dists=dists,cols=ramp[distqual]))
   }
