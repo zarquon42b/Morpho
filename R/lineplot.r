24fbc422cac73104487d5d23141db660dc759900
schlager <zarquon42@gmail.com>
2012-06-16 16:30:37 +0200

    added lineplot for 2D and 3D wireframe plotting.

---
+++ R/lineplot.r
@@ -0,0 +1,36 @@
+lineplot <-function(x,point,col=1,lwd=1,line_antialias = FALSE,add=TRUE)
+{
+  
+  if (dim(x)[2] == 3)
+    {
+      if (is.list(point)==TRUE)
+        {
+          for (i in 1:length(point))
+            {
+              lines3d(x[point[[i]],1],x[point[[i]],2],x[point[[i]],3],col=col,lwd=lwd,line_antialias = line_antialias)
+           }
+        }
+      else                                                
+        {
+          lines3d(x[point,1],x[point,2],x[point,3],col=col,lwd=lwd,line_antialias = line_antialias)
+        }
+    }
+  else
+    {
+      if (!add)
+        {
+          plot(x,asp=1,cex=0,xlab="x-coordinate",ylab="y-coordinate")
+        }
+      if (is.list(point)==TRUE)
+        {
+          for (i in 1:length(point))
+            {
+              lines(x[point[[i]],1],x[point[[i]],2],col=col,lwd=lwd)
+            }
+        }
+      else                                                
+        {
+          lines(x[point,1],x[point,2],col=col,lwd=lwd)
+      }
+    }
+}
