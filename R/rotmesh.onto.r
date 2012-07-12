0bb9caf8cc2d6ef7d07a8205fbfcb423e4edb3a3
schlager <zarquon42@gmail.com>
2012-07-12 13:37:13 +0200

    moved gaussDisplace and icp to package mesheR and speed up rotmesh.onto

--- R/rotmesh.onto.r
+++ R/rotmesh.onto.r
@@ -1,26 +1,18 @@
 rotmesh.onto<-function(mesh,refmat,tarmat,adnormals=TRUE,scale=FALSE,reflection=FALSE)
-{ 	rot<-rotonto(tarmat,refmat,scale=scale,reflection=reflection)
-	
-	transymat<-diag(c(rep(1,4)))
-	transymat[4,1:3]<--rot$transy
-	transmat<-diag(c(rep(1,4)))
-	transmat[4,1:3]<-rot$trans
-	#print(transmat)
-	mesh$vb<-(apply(t(mesh$vb),1,function(x){x%*%transymat}))
-	#print(mesh$vb[,1:2])	
-	mesh$vb[1:3,]<-t(t(mesh$vb[1:3,])%*%rot$gamm)
-	if (scale)
-		{mesh$vb[1:3,]<-mesh$vb[1:3,]*rot$bet
-		}
-  	#mesh$vb[1:3,]<-t(centmeshr)
-	mesh$vb<-(apply(t(mesh$vb),1,function(x){x%*%transmat}))
-  	
-	if (sign(det(rot$gamm)<0 && reflection))
-  		{mesh<-conv2backf(mesh)
-		}
-	if (adnormals) 
-		{mesh<-adnormals(mesh)
-		}
-
-  	return(list(mesh=mesh,yrot=rot$yrot))
+{
+  rot<-rotonto(tarmat,refmat,scale=scale,reflection=reflection)
+  mesh$vb[1:3,] <- mesh$vb[1:3,]-rot$transy
+  mesh$vb[1:3,]<-t(t(mesh$vb[1:3,])%*%rot$gamm)
+  if (scale)
+    {
+      mesh$vb[1:3,]<-mesh$vb[1:3,]*rot$bet
+    }
+  mesh$vb[1:3,] <- mesh$vb[1:3,]+rot$trans
+  if (sign(det(rot$gamm)<0 && reflection))
+    {mesh<-conv2backf(mesh)
+   }
+  if (adnormals) 
+    {mesh<-adnormals(mesh)
+   }
+  return(list(mesh=mesh,yrot=rot$yrot))
 }
