fe3fae00b3bd596c9df41f93a615bed212ab3ad5
schlager <zarquon42@gmail.com>
2012-07-13 14:13:59 +0200

    added typprob and typprobClass to perform calculation of typicality probabilities.

---
+++ R/typprob.r
@@ -0,0 +1,59 @@
+typprob <- function(x,data,small=FALSE,method=c("chisquare","wilson"))
+  {
+    method <- substr(method[1],1L,1L)
+    if (is.matrix(x))
+      nx <- dim(x)[2]
+    else
+      nx <- length(x)
+
+    ndata <- dim(data)[1]
+    dists <- mahalanobis(x,apply(data,2,mean),cov(data))
+    if (method == "w")
+      {
+        if (small)
+          {
+            dists <- ndata*log(1+(dists*ndata/(ndata^2-1)))
+          }
+        t2 <- ndata*dists/(ndata+1)
+        f <- t2*(ndata-nx)/(nx*(ndata-1))
+        alpha <- pf(f,nx,(ndata-nx),lower.tail=T)
+        alpha <- 1-alpha
+      }
+
+    else if (method == "c")
+      {
+        alpha <- pchisq(dists,nx,lower.tail=F)
+      }
+    else
+      stop("please chose valid method")
+
+    
+    return(alpha)
+  }
+typprobClass <- function(x,data,groups,small=FALSE,method=c("chisquare","wilson"),outlier=0.01)
+  {
+    if (!is.factor(groups))
+      {
+        groups <- as.factor(groups)
+        warning("groups coerced to factors")
+      }
+    probs <- NULL
+    glev <- levels(groups)
+    nlev <- length(glev)
+    for( i in 1:nlev)
+      {
+        tmp <- typprob(x,data[groups==glev[i],],small=small,method=method)
+        probs <- cbind(probs,tmp)
+      }
+    colnames(probs) <- as.character(glev)
+    classify <- apply(probs,1,function(x){out <- which(x==max(x));return(out)})
+    glev <- c(glev,"none")
+    outsider <- which(apply(probs,1,max) < outlier)
+    classify[outsider] <- nlev+1
+    groupaffin <- as.factor(glev[classify])
+    
+    out <- (list(probs=probs,groupaffin=groupaffin))
+    class(out) <- "typprob"
+    return(out)
+  }
+        
