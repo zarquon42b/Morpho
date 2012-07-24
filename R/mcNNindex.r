ecc25c6edb524100cc3f6580f73ecd339701bc18
schlager <zarquon42@gmail.com>
2012-07-24 14:09:53 +0200

    added fix in mcNNindex for single core

--- R/mcNNindex.r
+++ R/mcNNindex.r
@@ -4,12 +4,17 @@ mcNNindex <- function(target,query,cores=detectCores(),k=k,...)
    out <- NULL
     mclist <- list()
     nx <- dim(query)[1]
-    iter <-floor(nx/cores)    
-    for (i in 1:(cores-1))
-      {
-        mclist[[i]] <- query[(1:iter)+((i-1)*iter),]
-      }
-    mclist[[cores]] <-  query[-c(1:((cores-1)*iter)),]
+    iter <-floor(nx/cores)
+   if (cores > 1)
+     {
+       for (i in 1:(cores-1))
+         {
+           mclist[[i]] <- query[(1:iter)+((i-1)*iter),]
+         }
+       mclist[[cores]] <-  query[-c(1:((cores-1)*iter)),]
+     }
+   else
+     mclist[[1]] <- query
     tmpfun <- function(x,...)
       {
         tmp0 <- nn2(target,x,k=k,searchtype="priority",...)$nn.idx
