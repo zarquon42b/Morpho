cf2c323593ee8274cf061039761c47bb2be49614
schlager <zarquon42@gmail.com>
2012-11-20 10:46:50 +0100

    changed weighting in proc.weight to weighting by inverse distance and updated docu

--- R/proc.weight.r
+++ R/proc.weight.r
@@ -1,66 +1,73 @@
 proc.weight<-function(data,number,ref,report=TRUE,reg=0,log=FALSE,mahalanobis=FALSE)
-{	
-	col<-3
-        rho<-0
-        if (length(dim(data))==3)
-          {
-            l<-dim(data)[3]
-            for(i in 1:l){rho[i]<-angle.calc(data[,,ref],data[,,i])$rho}
-            if (is.null(dimnames(data)[[3]]))
-              {id<-as.character(c(1:l))
-             }
-            else
-              {id<-dimnames(data)[[3]]}
-          }
-        else
-          {
-            l<-dim(data)[1]
-            if (mahalanobis)
+{
+  lmdat <- FALSE
+  col <- 3
+  rho <- 0
+  if (length(dim(data))==3)
+    {
+      data <- vecx(data)
+      lmdat <- TRUE
+    }
+  l<-dim(data)[1]
+  obsnames <- dimnames(data)[[1]]
+  if (lmdat && !mahalanobis)
+    {
+      for(i in 1:l){rho[i]<-angle.calc(data[ref,],data[i,])$rho}
+      if (is.null(obsnames))
+        {id <- as.character(c(1:l))
+       }
+      else
+        {id <- obsnames}
+    }
+  else
+    {
+      if (mahalanobis)
+        {
+          covtmp <- cov(data)
+          if (reg != 0)
+            {
               {
-                covtmp <- cov(data)
-                if (reg != 0)
-                  {
-                    {
-                      eig <- eigen(covtmp,symmetric=TRUE)
-                      covtmp <- t(eig$vectors)%*%diag(eig$values+reg)%*%eig$vectors
-                    }
-                  }
+                eig <- eigen(covtmp,symmetric=TRUE)
+                covtmp <- t(eig$vectors)%*%diag(eig$values+reg)%*%eig$vectors
               }
-            else
-              {
-                covtmp <- diag(ncol(data))
-              }
-            rho <- mahalanobis(data,cov=covtmp,center=data[ref,])
-            
+            }
+          checksing <- try(covtmp <- solve(covtmp),silent = TRUE)
+          if (class(checksing)=="try-error")
+            {
+              covtmp <- ginv(covtmp)
+              cat("singular covariance matrix: using general inverse\n")
+            }
+        }
+      else
+        {
+          covtmp <- diag(ncol(data))
+        }
+      rho <- sqrt(mahalanobis(data,cov=covtmp,center=data[ref,],inverted = TRUE))
+    }
+  
+  if (is.null(obsnames))
+    {id <- as.character(c(1:l))
+   }
+  else
+    {id <- obsnames}
+  if (log)
+    {rho <- log(rho)
+   }
+  nr <- c(1:l)
+  data <- data.frame(nr,id,rho)
+  dat.sort.i <- data[order(data[,col]),]
+  dat.which <- dat.sort.i[2:(number+1),]
+  weight <- dat.which[,col]
+  if (0 %in% weight)
+    weight[which(weight == 0)] <- 1/1e12
+  
+  weight <- 1/weight
+  weight <- weight/sum(weight)
+  out <- data.frame(dat.which,weight)
+  if (report)
+    {
+      cat(paste("  reference was",id[ref],"\n"))
+    }
+  return(list(data=out,reference=id[ref],rho.all=data))
 
-            if (is.null(dimnames(data)[[1]]))
-              {id<-as.character(c(1:l))
-             }
-            else
-              {id<-dimnames(data)[[1]]}
-          }
-        if (log)
-          {
-            rho <- log(rho)
-          }
-        nr<-c(1:l)
-        data<-data.frame(nr,id,rho)
-        dat.sort.i<-data[order(data[,col]),]
-        dat.which<-dat.sort.i[2:(number+1),]
-        
-        all<-sum(dat.which[,col])
-        share<-0
-        
-        for (i in 1:length(dat.which[,col]))
-          {share[i]<-dat.which[i,col]/all
-           
-         }
-        weight<-sort(share,decreasing=T)
-        out<-data.frame(dat.which,weight)
-        if (report)
-          {
-            cat(paste("  reference was",id[ref],"\n"))
-          }
-        return(list(data=out,reference=id[ref],rho.all=data))
-        
 }
