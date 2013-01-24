6b7e2d754d84491fbb03414efc8d4011dfe25608
schlager <zarquon42@gmail.com>
2013-01-24 17:10:20 +0100

    added dependency Matrix to gain from sparse matrix support for lots of sliding landmarks

--- R/CreateL.r
+++ R/CreateL.r
@@ -1,26 +1,31 @@
 CreateL<-function(matrix,lambda=0)
 {  
-	 k<-dim(matrix)[1]
-    q1<-matrix(c(rep(1,k)),k,1)
-    K<-matrix(0,k,k)
-    Q<-cbind(q1,matrix)
-    O<-matrix(c(rep(0,16)),4,4)
+  k<-dim(matrix)[1]
+  q1<-matrix(c(rep(1,k)),k,1)
+  K<-matrix(0,k,k)
+  
+  Q<-cbind(q1,matrix)
+  O<-matrix(c(rep(0,16)),4,4)
 
-storage.mode(K)<-"double"
-storage.mode(matrix)<-"double"
-    K<-.Fortran("createL",K,nrow(K),matrix,ncol(matrix))[[1]]
-	
-	diag(K)<-lambda
-    	L<-rbind(cbind(K,Q),cbind(t(Q),O))
-	
-	L1<-try(solve(L),silent=TRUE)
-    	if (class(L1)=="try-error")
-		{cat("CreateL: singular matrix: general inverse will be used.\n")
-		L1<-ginv(L)		
-		}
+  storage.mode(K)<-"double"
+  storage.mode(matrix)<-"double"
+  K<-.Fortran("createL",K,nrow(K),matrix,ncol(matrix))[[1]]
+  
+  diag(K)<-lambda
+  L<-rbind(cbind(K,Q),cbind(t(Q),O))
+  L1<-try(solve(L),silent=TRUE)
+  if (class(L1)=="try-error")
+    {cat("CreateL: singular matrix: general inverse will be used.\n")
+     L1<-ginv(L)		
+   }
 
-	Lsubk<-L1[1:k,1:k]
-
-    Lsubk3<-rbind(cbind(Lsubk,matrix(0,k,2*k)),cbind(matrix(0,k,k),Lsubk,matrix(0,k,k)),cbind(matrix(0,k,2*k),Lsubk))
-    return(list(L=L,Linv=L1,Lsubk=Lsubk,Lsubk3=Lsubk3))
+  Lsubk<-L1[1:k,1:k]
+ 
+###print(system.time(Lsubk3<-rbind(cbind(Lsubk,matrix(0,k,2*k)),cbind(matrix(0,k,k),Lsubk,matrix(0,k,k)),cbind(matrix(0,k,2*k),Lsubk))))
+  Lsubk3 <- matrix(0,3*k,3*k)
+  Lsubk3[1:k,1:k] <- Lsubk
+  Lsubk3[(k+1):(2*k),(k+1):(2*k)] <- Lsubk
+  Lsubk3[(2*k+1):(3*k),(2*k+1):(3*k)] <- Lsubk;
+                  
+  return(list(L=L,Linv=L1,Lsubk=Lsubk,Lsubk3=Lsubk3))
 }
