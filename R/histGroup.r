e54535a6b84d51e597db728587ac7f5b54fd64c3
schlager <zarquon42@gmail.com>
2013-02-12 11:02:26 +0100

    added option 'col' ins histGroup

--- R/histGroup.r
+++ R/histGroup.r
@@ -1,4 +1,4 @@
-histGroup <- function(data,groups, main=paste("Histogram of" , dataname),xlab=dataname,ylab,alpha=0.5,breaks="Sturges")
+histGroup <- function(data,groups, main=paste("Histogram of" , dataname),xlab=dataname,ylab,col=NULL, alpha=0.5,breaks="Sturges")
   {
    
     dataname <- paste(deparse(substitute(data), 500), collapse="\n")
@@ -12,7 +12,26 @@ histGroup <- function(data,groups, main=paste("Histogram of" , dataname),xlab=da
       }
     lev <- levels(groups)
     nlev <- length(lev)
-    colo <- rainbow(nlev,alpha=alpha)
+    if (is.null(col))
+      colo <- rainbow(nlev,alpha=alpha)
+    else
+      {
+        if (length(col) != nlev)
+          stop("length of 'col' must match number of groups")
+        else if (is.character(col))
+          colo <- col
+        else
+          {
+            rgbfun <- function(x)
+              {
+                alpha <- alpha*255
+                x <- rgb(x[1],x[2],x[3],maxColorValue = 255,alpha=alpha)
+                return(x)
+              }
+            colo <- apply(col2rgb(col),2,rgbfun)
+            
+          }
+      }
     testrun <- 0
     for( i in 1:nlev)
      { testrun[i] <-  max(hist(data[groups==lev[i]],breaks=histo$breaks,plot=F)$counts)
