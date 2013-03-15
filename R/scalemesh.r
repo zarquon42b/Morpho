bb7fae178cf5bdae4b36b5b82d487d19d69d246d
schlager <zarquon42@gmail.com>
2013-03-15 15:36:41 +0100

    fixed docu of procSym (thanks to Paolo Piras) and option to center mesh in scalemesh according to bbox or vertex mean

--- R/scalemesh.r
+++ R/scalemesh.r
@@ -1,6 +1,16 @@
-scalemesh<-function(mesh,size)
+scalemesh<-function(mesh,size,center=c("bbox","mean"))
 {	
-	mesh$vb[1:3,]<-mesh$vb[1:3,]*size
-	return(mesh)
+
+  if (substr(center[1],1L,1L) =="b")
+    meshmean <- apply(meshcube(mesh),2,mean)
+  else if (substr(center[1],1L,1L) =="m")
+    meshmean <- apply(vert2points(mesh),2,mean)
+  else
+    stop("Please provide valid centering method\n")
+  mesh <- translate3d(mesh,-meshmean[1],-meshmean[2],-meshmean[3])
+    mesh$vb[1:3,]<-mesh$vb[1:3,]*size
+  
+  mesh <- translate3d(mesh,meshmean[1],meshmean[2],meshmean[3])
+  return(mesh)
 }
 	
