d587d5380ec9ebfa236034aacc1b3561cdece18e
schlager <zarquon42@gmail.com>
2013-03-20 13:27:48 +0100

    more docu work and added option to project meshes in the workspace with ray2mesh

--- R/ray2mesh.r
+++ R/ray2mesh.r
@@ -4,15 +4,16 @@ ray2mesh<-function(mesh1,tarmesh,tol=1,angmax=NULL,clean=TRUE,outname=NULL,readb
   options <- NULL
   opt <- FALSE
   target <- "target.ply"
+  reference <- "reference.ply"
   if (inbound == TRUE)
     { opt <- TRUE
-      options <- "--inbound" # set option to search along negative nromals first
+      options <- "--inbound" # set option to search along negative normals first
     }
   if (strict == TRUE)
     {
       opt <- TRUE
       options <- paste(options,"--strict") #mark vertices that are not hit along rays
-   }
+    }
   if (mindist == TRUE)
     {
       opt <- TRUE
@@ -36,13 +37,19 @@ ray2mesh<-function(mesh1,tarmesh,tol=1,angmax=NULL,clean=TRUE,outname=NULL,readb
     {
       mesh2ply(tarmesh,"target")
     }
-
+  
+  if (is.character(mesh1))
+    reference <- mesh1
+  
+  else
+    mesh2ply(mesh1,"reference")
+  
   mesh2ply(mesh1,"reference")
   
   if (is.null(mesh1$it))
     {
-      cmd <- paste("rayproject reference.ply ",target," -cloud",options," -t ",tol," -o ",outname,sep="")
-      system(paste("rayproject reference.ply ",target," -cloud",options," -t ",tol," -o ",outname,sep=""),ignore.stdout=ignore.stdout)
+      cmd <- paste("rayproject ",reference," ",target," -cloud",options," -t ",tol," -o ",outname,sep="")
+      system(paste("rayproject ",reference," ",target," -cloud",options," -t ",tol," -o ",outname,sep=""),ignore.stdout=ignore.stdout)
     }
   else
     { cmd <- paste("rayproject reference.ply ",target,options," -t ",tol," -o ",outname,sep="")
@@ -50,11 +57,11 @@ ray2mesh<-function(mesh1,tarmesh,tol=1,angmax=NULL,clean=TRUE,outname=NULL,readb
     }
   
 
- # print (cmd)
+                                        # print (cmd)
 
   outmesh<-ply2mesh(outname,readnormals=TRUE)
   if (clean)
-    {unlink(c("reference.ply","target.ply","project.mesh.ply"))
+    {unlink(c("reference.ply","target.ply",outname))
    }
   if (readback)
     {return(outmesh)
