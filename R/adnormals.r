4c20e834f9a9b82f8301db731785437f3fa3ca08
schlager <zarquon42@gmail.com>
2013-05-29 16:26:06 +0200

    added function facenormals to calculate per-face normals (including fortran90 routine)

--- R/adnormals.r
+++ R/adnormals.r
@@ -26,5 +26,29 @@ adnormals<- function(x,angle=TRUE)
   x$normals <- normals
   return(x)
 }
+facenormals<- function(x) 
+{
+    barymesh <- list()
+    barymesh$vb <- rbind(t(barycenter(x)),1)
+    
+  v <- x$vb
+  # Make sure v is homogeneous with unit w
+  if (nrow(v) == 3) v <- rbind(v, 1)
+  else v <- t( t(v)/v[4,] )
+  v <- v[1:3,]
+  it <- x$it
+  normals <- it*0
+  storage.mode(v)<-"double"
+  storage.mode(normals)<-"double"
+  storage.mode(it)<-"integer"
+  out<-.Fortran("facenormals",v,ncol(v),it,ncol(it),nrow(it),normals=normals)$normals
+	
+
+  normals <- out#/out[4,]
+    class(barymesh) <- "mesh3d"
+  barymesh$normals <- normals
+  
+  return(barymesh)
+}
 
 
