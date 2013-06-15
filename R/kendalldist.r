98df380968a63e6142e18650f3dfedcd789716bc
schlager <zarquon42@gmail.com>
2013-06-15 21:58:33 +0200

    cosmetic changes in kendalldist

--- R/kendalldist.r
+++ R/kendalldist.r
@@ -1,15 +1,15 @@
 kendalldist <- function(x,y)
     {
         m <- ncol(x)
-        x <- apply(x,2,scale,scale=F)
-        y <- apply(y,2,scale,scale=F)
+        x <- apply(x,2,scale,scale=FALSE)
+        y <- apply(y,2,scale,scale=FALSE)
         x <- x/cSize(x)
         y <- y/cSize(y)
         if (max(abs(x - y) > 0))
             {
                 if (m == 3)
                     {
-                        eigxy <- eigen(t(x)%*%tcrossprod(y)%*%(x))$values
+                        eigxy <- eigen(t(x)%*%tcrossprod(y)%*%(x),symmetric = TRUE)$values
                         signchk <- det(crossprod(y,x))
                         good <- which(eigxy > 0)
                         eigxy <- sqrt(eigxy[1:m])
@@ -19,8 +19,8 @@ kendalldist <- function(x,y)
                 else
                     {
                         ## this is copied from 'riemdist' in shapes package
-                        x <- realtocomplex(x)
-                        y <- realtocomplex(y)
+                        x <- x[,1]+(0+1i)*x[,2]
+                        y <- y[,1]+(0+1i)*y[,2]
                         rho <- acos(min(1, (Mod(t(Conj(x)) %*% y))))
                     }
             }
