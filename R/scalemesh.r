9c027ad6c5695bc95587fdfac3ba2e064481490f
schlager <zarquon42@gmail.com>
2013-06-25 11:50:19 +0200

    removed shapes as dependency and upscaled nose data, as well as adding option center="none" to scalemesh.

--- R/scalemesh.r
+++ R/scalemesh.r
@@ -1,16 +1,22 @@
-scalemesh<-function(mesh,size,center=c("bbox","mean"))
+scalemesh<-function(mesh,size,center=c("bbox","mean", "none"))
 {	
-
+    getmean <- TRUE
   if (substr(center[1],1L,1L) =="b")
     meshmean <- apply(meshcube(mesh),2,mean)
   else if (substr(center[1],1L,1L) =="m")
     meshmean <- apply(vert2points(mesh),2,mean)
+  else if (substr(center[1],1L,1L) =="n")
+     getmean <- FALSE
   else
     stop("Please provide valid centering method\n")
-  mesh <- translate3d(mesh,-meshmean[1],-meshmean[2],-meshmean[3])
-    mesh$vb[1:3,]<-mesh$vb[1:3,]*size
-  
-  mesh <- translate3d(mesh,meshmean[1],meshmean[2],meshmean[3])
+    if (getmean)
+        {
+            mesh <- translate3d(mesh,-meshmean[1],-meshmean[2],-meshmean[3])
+            mesh$vb[1:3,]<-mesh$vb[1:3,]*size
+            mesh <- translate3d(mesh,meshmean[1],meshmean[2],meshmean[3])
+        }
+   else
+       mesh$vb[1:3,]<-mesh$vb[1:3,]*size
   return(mesh)
 }
 	
