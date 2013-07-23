250e9f1e06f8b201f41660001778f55f3b300ec4
schlager <zarquon42@gmail.com>
2013-07-23 10:00:43 +0200

    reverted to RANN and fixed extensive usage section length in anonymize an rotonmat

--- R/mcNNindex.r
+++ R/mcNNindex.r
@@ -1,35 +1,32 @@
 mcNNindex <- function(target,query,cores=detectCores(),k=k,...)
-  
-  {
-    if(.Platform$OS.type == "windows")
-        cores <- 1
-    if (nrow (query) < 1000)
-        cores <- 1
-    out <- NULL
-    mclist <- list()
-    nx <- dim(query)[1]
-    iter <-floor(nx/cores)
-    if (cores > 1)
-      {
-        for (i in 1:(cores-1))
-          {
-            mclist[[i]] <- query[(1:iter)+((i-1)*iter),]
-          }
-        mclist[[cores]] <-  query[-c(1:((cores-1)*iter)),]
-      }
-    else
-      mclist[[1]] <- query
-    tmpfun <- function(x,...)
-      {
-        #tmp0 <- nn2(target,x,k=k,searchtype="priority",...)$nn.idx
-          tmp0 <- ann(ref=target, target=x, k=k, search.type="priority",verbose=FALSE)$knnIndexDist[,1:k]
-        return(tmp0)
-      }
-    tmp <- mclapply(mclist,tmpfun,mc.cores=cores)
-    for (i in 1:cores)
-      {
-        out <- rbind(out,tmp[[i]])
-      }
-    return(out)
+    {
+        if(.Platform$OS.type == "windows")
+            cores <- 1
+        if (nrow (query) < 1000)
+            cores <- 1
+        out <- NULL
+        mclist <- list()
+        nx <- dim(query)[1]
+        iter <-floor(nx/cores)
+        if (cores > 1)
+            {
+                for (i in 1:(cores-1))
+                    mclist[[i]] <- query[(1:iter)+((i-1)*iter),]
+                
+                mclist[[cores]] <-  query[-c(1:((cores-1)*iter)),]
+            }
+        else
+            mclist[[1]] <- query
+        tmpfun <- function(x,...)
+            {
+                tmp0 <- nn2(target,x,k=k,searchtype="priority",...)$nn.idx
+                ##  tmp0 <- ann(ref=target, target=x, k=k, search.type="priority",verbose=FALSE)$knnIndexDist[,1:k] ## ann function from package yaImpute
+                return(tmp0)
+            }
+        tmp <- mclapply(mclist,tmpfun,mc.cores=cores)
+        for (i in 1:cores)
+            out <- rbind(out,tmp[[i]])
+            
+        return(out)
 
-  }
+    }
