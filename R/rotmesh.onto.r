fea9eaaf875f563a6289424b4726655698b29fb4
schlager <zarquon42@gmail.com>
2013-08-06 13:25:42 +0200

    changed default adnormals=FALSE in rotmesh.onto and added rotation for existing normals in that case. added examples in warp.mesh and rotmesh.onto

--- R/rotmesh.onto.r
+++ R/rotmesh.onto.r
@@ -1,18 +1,18 @@
-rotmesh.onto <- function(mesh,refmat,tarmat,adnormals=TRUE,scale=FALSE,reflection=FALSE)
+rotmesh.onto <- function(mesh, refmat, tarmat, adnormals=FALSE, scale=FALSE, reflection=FALSE)
 {
   rot <- rotonto(tarmat,refmat,scale=scale,reflection=reflection)
   mesh$vb[1:3,] <- mesh$vb[1:3,]-rot$transy
   mesh$vb[1:3,] <- t(t(mesh$vb[1:3,])%*%rot$gamm)
   if (scale)
-    {
       mesh$vb[1:3,] <- mesh$vb[1:3,]*rot$bet
-    }
+    
   mesh$vb[1:3,] <- mesh$vb[1:3,]+rot$trans
-  if (sign(det(rot$gamm)<0 && reflection))
-    {mesh <- conv2backf(mesh)
-   }
+  if (sign(det(rot$gamm) < 0 && reflection))
+      mesh <- conv2backf(mesh)
   if (adnormals) 
-    {mesh <- adnormals(mesh)
-   }
+      mesh <- adnormals(mesh)
+  if (!is.null(mesh$normals) && !adnormals)
+      mesh$normals[1:3,] <- t(t(mesh$normals[1:3,]) %*% rot$gamm)
+   
   return(list(mesh=mesh,yrot=rot$yrot))
 }
