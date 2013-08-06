90dd964cb6baa32dcc6651f35f506826121e2a89
schlager <zarquon42@gmail.com>
2013-08-06 14:08:58 +0200

    coerced PCscores to matrix in covPCA to allow rownames for two-group case.

--- R/covDist.r
+++ R/covDist.r
@@ -44,8 +44,8 @@ covPCA <- function(data,groups,scores=TRUE,rounds=0, mc.cores=detectCores())
                 eigenD <- eigen(D,symmetric = TRUE)
                 eigenD$values <- eigenD$values[1:(nlev-1)]
                 eigenD$vectors <- eigenD$vectors[,1:(nlev-1)]
-                PCscores <- t(t(eigenD$vectors)*sqrt(eigenD$values))
-                rownames(PCscores) <- rownames(eigenD$vectors) <- lev
+                PCscores <- as.matrix(t(t(eigenD$vectors)*sqrt(eigenD$values)))
+                rownames(PCscores) <- lev
                 
                 out$PCscores <- PCscores
                 out$Var <- eigenD$values/sum(eigenD$values)
