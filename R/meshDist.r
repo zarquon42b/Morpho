6eaea52e108fa332cbb3a5309196758df013710d
schlager <zarquon42@gmail.com>
2013-09-13 11:57:03 +0200

    added createAtlas, plotAtlas and wrote an end user wrapper for place.patch, that now will not be exported anymore

--- R/meshDist.r
+++ R/meshDist.r
@@ -5,76 +5,54 @@ meshDist.mesh3d <- function(x, mesh2=NULL, distvec=NULL, from=NULL, to=NULL, ste
     method=substring(method[1],1L,1L)
     neg=FALSE
     ramp <- blue2green2red(steps-1)
-    if (is.null(distvec))
-      {
-        if(!ray)
-          {
-            if (method == "v")
-              {
+    if (is.null(distvec)) {
+        if(!ray) {
+            if (method == "v") {
                 promesh <- projRead(t(x$vb[1:3,]),mesh2,readnormals=T,sign=T)
-              }
-            else
-              {
+            } else {
                 promesh <- closemeshKD(x,mesh2,sign=T)
-              }
+            }
             clost <- promesh$vb[1:3,]
             dists <- promesh$quality
             distsOrig <- dists
             if (!sign)
-              {dists <- abs(dists)
-             }
-          }
-        else
-          {
+                dists <- abs(dists)
+        } else {
             promesh <- ray2mesh(x,mesh2,tol=raytol,mindist=TRUE)
             clost <- promesh$vb[1:3,]
             dists <- promesh$quality
             distsOrig <- dists
             if (!sign)
-              {dists <- abs(dists)
-             }
-          }
-      }
-    else
-      {
+              dists <- abs(dists)
+        }
+    } else {
         clost <- NULL
         dists <- distvec
         distsOrig <- dists
         if (!sign)
-          {
             dists <- abs(dists)
-          }
-      }  
+    }  
     
-    if (is.null(from))
-      {
+    if (is.null(from)) {
         mindist <- min(dists)
-        if (sign && mindist < 0 )
-          {
+        if (sign && mindist < 0 ) {
             from <- quantile(dists,probs=(1-uprange)) 
-                                        #from <- mindist
             neg <- TRUE            
-          }
-        else
-          {
+        } else {
             from <- 0
-          }
-      }
+        }
+    }
     if (from < 0)
-      {
         neg <- TRUE
-      }
     if (is.null(to))
-      {to <- quantile(dists,probs=uprange)    
-     }
+        to <- quantile(dists,probs=uprange)    
     if(ceiling)
-      {to <- ceiling(to)
-     }
+        to <- ceiling(to)
+    
     to <- to+1e-10
     colseq <- seq(from=from,to=to,length.out=steps)
     coldif <- colseq[2]-colseq[1]
-    if (neg && sign)
-      {
+    if (neg && sign) {
         negseq <- length(which(colseq<0))
         poseq <- steps-negseq
         maxseq <- max(c(negseq,poseq))
@@ -82,26 +60,20 @@ meshDist.mesh3d <- function(x, mesh2=NULL, distvec=NULL, from=NULL, to=NULL, ste
         ramp <- ramp[c(maxseq-negseq+1):(maxseq+poseq)]
         distqual <- ceiling(((dists+abs(from))/coldif)+1e-14)
         distqual[which(distqual < 1)] <- steps+10
-      }
-    else
-      {
+    } else {
         distqual <- ceiling((dists/coldif)+1e-14)
-      }
+    }
     colorall <- ramp[distqual]
     
-    if (!is.null(tol))
-      {
-        if (sign)
-          {
+    if (!is.null(tol)) {
+        if (sign) {
             tol <- c(-tol,tol)
-          }
-        else
-          {
+        } else {
             tol <- c(0,tol)
-          }
+        }
         good <- which(abs(dists) < tol[2])
         colorall[good] <- "#00FF00"
-      }   
+    }   
     
     colfun <- function(x){x <- colorall[x];return(x)}
     x$material$color <- matrix(colfun(x$it),dim(x$it))
@@ -111,15 +83,11 @@ meshDist.mesh3d <- function(x, mesh2=NULL, distvec=NULL, from=NULL, to=NULL, ste
     class(out) <- "meshDist"
 
     if (plot)
-      {
         render(out,output=FALSE,displace=displace,shade=shade,...)
-      }
     if (save)
-      {
         export(out,file=file,imagedim=imagedim)
-      }
     invisible(out)
-  }
+}
 render <- function(x,...) UseMethod("render")
 render.meshDist <- function(x,from=NULL,to=NULL,steps=NULL,ceiling=NULL,uprange=NULL,tol=NULL,displace=FALSE,shade=TRUE,sign=NULL,...)
   {
@@ -131,61 +99,45 @@ render.meshDist <- function(x,from=NULL,to=NULL,steps=NULL,ceiling=NULL,uprange=
     params <- x$params
     distqual <- x$distqual
     if (rgl.cur() !=0)
-      {
         rgl.clear()
-      }
-    if (!is.null(from) || !is.null(to) || !is.null(to) || !is.null(uprange) ||  !is.null(tol)  ||  !is.null(sign))
-      {
+    
+    if (!is.null(from) || !is.null(to) || !is.null(to) || !is.null(uprange) ||  !is.null(tol)  ||  !is.null(sign)) {
         neg=FALSE
         colMesh <- x$colMesh
         if(is.null(steps))
-          {steps <- x$params$steps
-         }
+            steps <- x$params$steps
         if(is.null(sign))
-          {sign <- x$params$sign
-         }
-        if (!sign)
-          {
+          sign <- x$params$sign
+        if (!sign) {
             distsOrig <- dists
             dists <- abs(dists)
-          }
+        }
         if(is.null(ceiling))
-          {ceiling <- x$params$ceiling
-         }
+            ceiling <- x$params$ceiling
         if(is.null(uprange))
-          {uprange <- x$params$uprange
-         }
-        
-        if (is.null(from))
-          {
+          uprange <- x$params$uprange
+                 
+        if (is.null(from)) {
             mindist <- min(dists)
-            if (sign && mindist < 0 )
-              {
+            if (sign && mindist < 0 ) {
                 from <- quantile(dists,probs=(1-uprange)) 
-                                        #from <- mindist
                 neg <- TRUE            
-              }
-            else
-              {
+            } else {
                 from <- 0
-              }             
-          }
+            }             
+        }
         if (from < 0)
-          {
             neg <- TRUE
-          }         
         if (is.null(to))
-          {to <- quantile(dists,probs=uprange)    
-         }
+            to <- quantile(dists,probs=uprange)    
         if(ceiling)
-          {to <- ceiling(to)
-         }
+            to <- ceiling(to)
+        
         to <- to+1e-10
         ramp <- blue2green2red(steps-1)
         colseq <- seq(from=from,to=to,length.out=steps)
         coldif <- colseq[2]-colseq[1]
-        if (neg && sign)
-          {
+        if (neg && sign) {
             negseq <- length(which(colseq<0))
             poseq <- steps-negseq
             maxseq <- max(c(negseq,poseq))
@@ -193,83 +145,66 @@ render.meshDist <- function(x,from=NULL,to=NULL,steps=NULL,ceiling=NULL,uprange=
             ramp <- ramp[c(maxseq-negseq+1):(maxseq+poseq)]
             distqual <- ceiling(((dists+abs(from))/coldif)+1e-14)
             distqual[which(distqual < 1)] <- steps+10
-          }
-        else
-          {
+        } else {
             distqual <- ceiling((dists/coldif)+1e-14)
-          }
+        }
         colorall <- ramp[distqual]
-        if (!is.null(tol))
-          {
-            if (sign)
-              {
+        if (!is.null(tol)) {
+            if (sign) {
                 tol <- c(-tol,tol)
-              }
-            else
-              {
+            } else {
                 tol <- c(0,tol)
-              }
+            }
             good <- which(abs(dists) < tol[2])
             colorall[good] <- "#00FF00"
-          }
+        }
         colfun <- function(x){x <- colorall[x];return(x)}
         colMesh$material$color <- matrix(colfun(colMesh$it),dim(colMesh$it))
         colramp <- list(1,colseq, matrix(data=colseq, ncol=length(colseq),nrow=1),col=ramp,useRaster=T,ylab="Distance in mm",xlab="",xaxt="n")
-      }
-    else
-      {
+    } else {
         if (is.null(tol))
-          {
             tol <- x$params$tol
-          }
+        
         colramp <- x$colramp
         colMesh <- x$colMesh
-      }
+    }
     if (shade)
-      {
         shade3d(colMesh,specular="black",...)
-      }
-    if (displace)
-      {
+    if (displace) {
         dismesh <- colMesh
         vl <- dim(colMesh$vb)[2]
         dismesh$vb <- cbind(colMesh$vb,rbind(clost,1))
         dismesh$it <- rbind(1:vl,1:vl,(1:vl)+vl)
         dismesh$material$color <- rbind(colorall,colorall,colorall)
         wire3d(dismesh,lit=FALSE)
-      }
+    }
     diffo <- ((colramp[[2]][2]-colramp[[2]][1])/2)
     image(colramp[[1]],colramp[[2]][-1]-diffo,t(colramp[[3]][1,-1])-diffo,col=colramp[[4]],useRaster=TRUE,ylab="Distance in mm",xlab="",xaxt="n")
-    if (!is.null(tol))
-      {
+    if (!is.null(tol)) {
         if (sum(abs(tol)) != 0)
-          {
             image(colramp[[1]],c(tol[1],tol[2]),matrix(c(tol[1],tol[2]),1,1),col="green",useRaster=TRUE,add=TRUE)
-          }
-      }
+    }
     params <- list(steps=steps,from=from,to=to,uprange=uprange,ceiling=ceiling,sign=sign,tol=tol)
     out <- list(colMesh=colMesh,dists=distsOrig,cols=colorall,colramp=colramp,params=params,distqual=distqual,clost=clost)
                                         #out <- list(colMesh=colMesh,colramp=colramp)
     class(out) <- "meshDist"
     invisible(out)
-  }
+}
 
 export <- function(x,...)UseMethod("export")
 export.meshDist <- function(x,file="default",imagedim="100x800",...)
 {
-  tol <- x$params$tol
-  colramp <- x$colramp
-  widxheight <- as.integer(strsplit(imagedim,split="x")[[1]])
-  mesh2ply(x$colMesh,col=x$cols,filename=file)
-  png(filename=paste(file,".png",sep=""),width=widxheight[1],height=widxheight[2])
-  diffo <- ((colramp[[2]][2]-colramp[[2]][1])/2)
-  image(colramp[[1]],colramp[[2]][-1]-diffo,t(colramp[[3]][1,-1])-diffo,col=colramp[[4]],useRaster=TRUE,ylab="Distance in mm",xlab="",xaxt="n")
-  if (!is.null(tol))
-    {
-      if (sum(abs(tol)) != 0)
-        {
-          image(colramp[[1]],c(tol[1],tol[2]),matrix(c(tol[1],tol[2]),1,1),col="green",useRaster=TRUE,add=TRUE)
+    tol <- x$params$tol
+    colramp <- x$colramp
+    widxheight <- as.integer(strsplit(imagedim,split="x")[[1]])
+    mesh2ply(x$colMesh,col=x$cols,filename=file)
+    png(filename=paste(file,".png",sep=""),width=widxheight[1],height=widxheight[2])
+    diffo <- ((colramp[[2]][2]-colramp[[2]][1])/2)
+    image(colramp[[1]],colramp[[2]][-1]-diffo,t(colramp[[3]][1,-1])-diffo,col=colramp[[4]],useRaster=TRUE,ylab="Distance in mm",xlab="",xaxt="n")
+    if (!is.null(tol)) {
+        if (sum(abs(tol)) != 0) {
+            image(colramp[[1]],c(tol[1],tol[2]),matrix(c(tol[1],tol[2]),1,1),col="green",useRaster=TRUE,add=TRUE)
         }
     }
-  dev.off()
+    dev.off()
 }
--- R/place.patch.r
+++ R/place.patch.r
@@ -1,129 +1,137 @@
-place.patch <- function(dat.array,path,atlas.mesh,atlas.lm,patch,curves=NULL,prefix=NULL,tol=5,ray=T,outlines=NULL,SMvector=NULL,deselect=TRUE,inflate=NULL,relax.patch=TRUE,rhotol=NULL,fileext=".ply")
-  {
-     k <- dim(dat.array)[1]
-    patch.dim <- dim(patch)[1]
-    usematrix <- FALSE
-    if (! is.matrix(dat.array))
-      {
-        n <- dim(dat.array)[3]
-        out <- array(NA,dim=c((patch.dim+k),3,n))
-        dimnames(out)[[3]] <- dimnames(dat.array)[[3]]
-        name <- dimnames(dat.array)[[3]]
-      }
-    else
-      {
-        usematrix <- TRUE
-        n <- 1
-        out <- matrix(NA,(patch.dim+k),3)
-        name <- NULL
-      }
+createAtlas <- function(mesh, landmarks, patch, corrCuves=NULL, patchCurves=NULL)
+    {
+        atlas <- list()
+        class(atlas) <- "atlas"
+        atlas$mesh <- mesh
+        atlas$landmarks <- landmarks
+        atlas$patch <- patch
+        atlas$corrCurves <- corrCuves
+        atlas$patchCurves<- patchCurves
+        return(atlas)
+    }
+plotAtlas <- function(atlas, radius=1, meshcol="white", add=TRUE)
+    {
+        if (!inherits(atlas, "atlas"))
+            stop("please provide object of class atlas")
+        if (add)
+            open3d()
+        wire3d(atlas$mesh, col=meshcol)
+        spheres3d(atlas$landmarks,col=2)
+        spheres3d(atlas$patch,col=3,radius=radius/2)
+        if (!is.null(atlas$corrCurves))
+            spheres3d(atlas$landmarks[unlist(atlas$corrCurves),],col=4,radius=radius+0.001)
+        if (!is.null(atlas$patchCurves))
+            spheres3d(atlas$patch[unlist(atlas$patchOutlines),],col=5,radius=radius/2+0.001)
+        plot(0,0, xlab="", ylab="", axes =F, cex=0,xlim=c(-1,1), ylim=c(-1,1))
+        legend(-1,1, pch=20, cex=2, col=2:5, legend=c("landmarks", "patch", "curves on all specimen", "curves only on atlas"))
+    }
 
-   
-    L <- CreateL(atlas.lm)
-    L1 <- CreateL(rbind(atlas.lm,patch))
-    
-    for(i in 1:n)
-      {
-        tmp.name <- paste(path,prefix,name[i],fileext,sep="")
-        if (!usematrix)
-          tmp.data <- projRead(dat.array[,,i],tmp.name,readnormals=TRUE)
-        else
-          tmp.data <- projRead(dat.array,tmp.name,readnormals=TRUE)
-        
-### relax existing curves against atlas ###
-        if (!is.null(outlines))
-          {
-            sm <- SMvector
-            U <- .calcTang_U_s(t(tmp.data$vb[1:3,]),t(tmp.data$normals[1:3,]),SMvector=SMvector,outlines=outlines,surface=NULL,deselect=deselect)
-            slide <- calcGamma(U$Gamma0,L$Lsubk3,U$U,dims=3)$Gamatrix
-            slide <- projRead(slide,tmp.name,readnormals=FALSE)
-            tps.lm <- tps3d(patch,atlas.lm,slide)
-          }
-        else if (!is.null(SMvector) && is.null(outlines) )
-          {
-            sm <- SMvector
-            slide <- t(tmp.data$vb[1:3,])           
-            tps.lm <- tps3d(patch,atlas.lm,t(tmp.data$vb[1:3,]))
-          }
-        else
-          
-          {
-            sm <- 1:k
-            slide <- t(tmp.data$vb[1:3,])           
-            tps.lm <- tps3d(patch,atlas.lm,t(tmp.data$vb[1:3,]))
-          }
-### use for mullitlayer meshes to avoid projection inside
-        if (!is.null(inflate))
-          {
-            atlas.warp <- warp.mesh(atlas.mesh,atlas.lm,slide)
-            tps.lm <- projRead(tps.lm,atlas.warp,readnormals=TRUE,smooth=TRUE)
-            warp.norm <- tps.lm$normals[1:3,]### keep projected normals
-
-            tps.lm$vb[1:3,] <- tps.lm$vb[1:3,]+inflate*tps.lm$normals[1:3,] ###inflate outward along normals
-            tps.lm <- ray2mesh(tps.lm,tmp.name,inbound=TRUE,tol=tol,angmax=rhotol) ### deflate in opposite direction
-            relax <- rbind(slide,t(tps.lm$vb[1:3,]))
-            normals <- rbind(slide,t(tps.lm$normals[1:3,]))
-
-            surface <- c((k+1):(patch.dim+k))  ## define surface as appended to preset landmarks
-            free <- NULL
-### compare normals of projection and original points
-            if (!is.null(rhotol))
-              {
-                
-                rho <- NULL
-                for (j in 1:patch.dim)
-                  {
-                    rho[j] <- angle.calc(tps.lm$normals[1:3,j],warp.norm[1:3,j])
-                  }
-                rhoex <- which(rho > rhotol) 
-                
-                if (length(rhoex) > 0)
-                  {
-                    free <- surface[rhoex]
-                    surface <- surface[-rhoex]
-                  }
-              }
-            gc()
-### end compare normals #### 
-
-### relax patch against reference ###
-            if (relax.patch) ### relax against reference
-              
-              {
-                
-                outltmp <- append(outlines,curves) ## add curves from patch to predefined curves
-                remout <- which(surface %in% curves)
-
-                if (length(remout) > 0)
-                  {
-                    surface <- surface[-remout] ### remove patch curves from surface 
-                  }
-                if (length(surface)==0)
-                  {surface <- NULL
-                 }
-                
-                U1 <- .calcTang_U_s(relax,normals,SMvector=sm,outlines=outltmp,surface=surface,free=free,deselect=deselect)
-                
-                tps.lm <- calcGamma(U1$Gamma0,L1$Lsubk3,U1$U,dims=3)$Gamatrix[c((k+1):(patch.dim+k)),]
-                tps.lm <- projRead(tps.lm,tmp.name,readnormals=FALSE)
-              }
-### end relaxation ########################
+placePatch <- function(atlas, dat.array, path, prefix=NULL, fileext=".ply", tol=5, ray=TRUE, inflate=NULL, relax.patch=TRUE, keep.fix=NULL, rhotol=NULL)
+    {
+        if (!inherits(atlas, "atlas"))
+            stop("please provide object of class atlas")
+        if (is.null(keep.fix))
+            keep.fix <- 1:dim(atlas$landmarks)[1]
             
+        patched <- place.patch(dat.array, path, atlas.mesh =atlas$mesh, atlas.lm = atlas$landmarks, patch =atlas$patch, curves=atlas$patchCurves, prefix=prefix, tol=tol, ray=ray, outlines=atlas$corrCurves, inflate=inflate, relax.patch=relax.patch, rhotol=rhotol, fileext=fileext,SMvector = keep.fix)
+        return(patched)
+    }
+
+place.patch <- function(dat.array,path,atlas.mesh,atlas.lm,patch,curves=NULL,prefix=NULL,tol=5,ray=T,outlines=NULL,SMvector=NULL,deselect=TRUE,inflate=NULL,relax.patch=TRUE,rhotol=NULL,fileext=".ply")
+    {
+        k <- dim(dat.array)[1]
+        patch.dim <- dim(patch)[1]
+        usematrix <- FALSE
+        if (! is.matrix(dat.array)) {
+            n <- dim(dat.array)[3]
+            out <- array(NA,dim=c((patch.dim+k),3,n))
+            dimnames(out)[[3]] <- dimnames(dat.array)[[3]]
+            name <- dimnames(dat.array)[[3]]
+        } else {
+            usematrix <- TRUE
+            n <- 1
+            out <- matrix(NA,(patch.dim+k),3)
+            name <- NULL
+        }
+        
+        L <- CreateL(atlas.lm)
+        L1 <- CreateL(rbind(atlas.lm,patch))
+        
+        for(i in 1:n) {
+            tmp.name <- paste(path,prefix,name[i],fileext,sep="")
+            if (!usematrix)
+                tmp.data <- projRead(dat.array[,,i],tmp.name,readnormals=TRUE)
             else
-              {
-                tps.lm <- t(tps.lm$vb[1:3,])
-              }
-          }
-### just project warped patch on surface (suitable for singlelayer meshes)
-        else
-          {
-            tps.lm <- projRead(tps.lm,tmp.name,readnormals=FALSE)
-          }
-        if (!usematrix)
-          out[,,i] <- rbind(dat.array[,,i],tps.lm)
-        else
-          out <- rbind(dat.array,tps.lm)
-      }
-    return(out)
-  }
+                tmp.data <- projRead(dat.array,tmp.name,readnormals=TRUE)
+            
+### relax existing curves against atlas ###
+            if (!is.null(outlines)) {
+                sm <- SMvector
+                U <- .calcTang_U_s(t(tmp.data$vb[1:3,]),t(tmp.data$normals[1:3,]),SMvector=SMvector,outlines=outlines,surface=NULL,deselect=deselect)
+                slide <- calcGamma(U$Gamma0,L$Lsubk3,U$U,dims=3)$Gamatrix
+                slide <- projRead(slide,tmp.name,readnormals=FALSE)
+                tps.lm <- tps3d(patch,atlas.lm,slide)
+            } else if (!is.null(SMvector) && is.null(outlines)) {
+                sm <- SMvector
+                slide <- t(tmp.data$vb[1:3,])           
+                tps.lm <- tps3d(patch,atlas.lm,t(tmp.data$vb[1:3,]))
+            } else {
+                sm <- 1:k
+                slide <- t(tmp.data$vb[1:3,])           
+                tps.lm <- tps3d(patch,atlas.lm,t(tmp.data$vb[1:3,]))
+            }
+### use for mullitlayer meshes to avoid projection inside
+            if (!is.null(inflate)) {
+                atlas.warp <- warp.mesh(atlas.mesh,atlas.lm,slide)
+                tps.lm <- projRead(tps.lm,atlas.warp,readnormals=TRUE,smooth=TRUE)
+                warp.norm <- tps.lm$normals[1:3,]### keep projected normals
+                
+                tps.lm$vb[1:3,] <- tps.lm$vb[1:3,]+inflate*tps.lm$normals[1:3,] ###inflate outward along normals
+                tps.lm <- ray2mesh(tps.lm,tmp.name,inbound=TRUE,tol=tol,angmax=rhotol) ### deflate in opposite direction
+                relax <- rbind(slide,t(tps.lm$vb[1:3,]))
+                normals <- rbind(slide,t(tps.lm$normals[1:3,]))
+                
+                surface <- c((k+1):(patch.dim+k))  ## define surface as appended to preset landmarks
+                free <- NULL
+### compare normals of projection and original points
+                if (!is.null(rhotol)) {
+                    rho <- NULL
+                    for (j in 1:patch.dim)
+                        rho[j] <- angle.calc(tps.lm$normals[1:3,j],warp.norm[1:3,j])
+                    
+                    rhoex <- which(rho > rhotol) 
+                    if (length(rhoex) > 0) {
+                        free <- surface[rhoex]
+                        surface <- surface[-rhoex]
+                    }
+                }
+                gc()
+### end compare normals #### 
+                
+### relax patch against reference ###
+                if (relax.patch){ ### relax against reference
+                    outltmp <- append(outlines,curves) ## add curves from patch to predefined curves
+                    remout <- which(surface %in% curves)
+                    
+                    if (length(remout) > 0)
+                        surface <- surface[-remout] ### remove patch curves from surface 
+                    if (length(surface)==0)
+                        surface <- NULL
+                    
+                    U1 <- .calcTang_U_s(relax,normals,SMvector=sm,outlines=outltmp,surface=surface,free=free,deselect=deselect)
+                    tps.lm <- calcGamma(U1$Gamma0,L1$Lsubk3,U1$U,dims=3)$Gamatrix[c((k+1):(patch.dim+k)),]
+                    tps.lm <- projRead(tps.lm,tmp.name,readnormals=FALSE)
+                } else {# end relaxation ########################
+                    tps.lm <- t(tps.lm$vb[1:3,])
+                }
+            } else {## just project warped patch on surface (suitable for singlelayer meshes)
+                tps.lm <- projRead(tps.lm,tmp.name,readnormals=FALSE)
+            }
+            if (!usematrix)
+                out[,,i] <- rbind(dat.array[,,i],tps.lm)
+            else
+                out <- rbind(dat.array,tps.lm)
+        }
+        return(out)
+    }
 
