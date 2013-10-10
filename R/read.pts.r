f34fd1d7de4858e322bab3651148d57c12ee5583
schlager <zarquon42@gmail.com>
2013-10-10 16:16:14 +0200

    added option "na" in 'read.pts' and 'read.lmdta' to specify values indicating missing data.

--- R/read.pts.r
+++ R/read.pts.r
@@ -1,7 +1,10 @@
-read.pts <- function(file="x")
+read.pts <- function(file="x", na=9999)
 {	
 	pts <- read.table(file,skip=2)
 	rownames(pts) <- pts[,1]
 	pts <- as.matrix(pts[,2:4])
+        nas <- which(pts == na)
+        if (length(nas) > 0)
+            pts[nas] <- NA
 	return(pts)
 }
