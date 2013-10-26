f1f520ab6e95e07e0106f45dab7674d182af1f51
schlager <zarquon42@gmail.com>
2013-10-26 15:18:47 +0200

    fixed issue with Matrix import in calcGamma

--- R/calcGamma.r
+++ R/calcGamma.r
@@ -6,10 +6,10 @@ calcGamma <- function(Gamma0,Lsubk3,U,dims,weights=NULL)
       diag(ULU) <- diag(ULU)+1e-8
       if (is.null(weights))
             weights <- 1
-      Gamma1 <- try(Gamma0 - weights*(U%*%solve(ULU,ULU2)))
+      Gamma1 <- try(Gamma0 - weights*(U%*%Matrix::solve(ULU,ULU2)))
       if (class(Gamma1)=="try-error") {
           cat("calcGamma: singular matrix: general inverse will be used.\n")
-          B <- ginv(ULU)		
+          B <- ginv(as.matrix(ULU))		
           Gamma1 <- Gamma0-weights*(U%*%B%*%Matrix::crossprod(U,(Lsubk3%*%Gamma0)))
     }
       Gamatrix <- matrix(Gamma1,length(Gamma1)/dims,dims)
