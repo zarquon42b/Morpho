cc3be6e7d1e25f58097f04d70a1dd6131f1c692b
schlager <zarquon42@gmail.com>
2013-10-27 10:40:25 +0100

    added option "blockdiag" in 'CreateL' to supress returning blockdiagonal matrix containin Lsubk and added imported function of Matrix in NAMESPACE

--- R/CreateL.r
+++ R/CreateL.r
@@ -1,4 +1,4 @@
-CreateL <- function(matrix,lambda=0)
+CreateL <- function(matrix,lambda=0, blockdiag=TRUE)
 {
     if (dim(matrix)[2] == 3) {
         k <- dim(matrix)[1]
@@ -18,21 +18,24 @@ CreateL <- function(matrix,lambda=0)
             L1 <- ginv(L)		
         }
         Lsubk <- L1[1:k,1:k]
-        Lsubk3 <- matrix(0,3*k,3*k)
-        Lsubk3[1:k,1:k] <- Lsubk
-        Lsubk3[(k+1):(2*k),(k+1):(2*k)] <- Lsubk
-        Lsubk3[(2*k+1):(3*k),(2*k+1):(3*k)] <- Lsubk;
+        Lsubk3 <- NULL
+        if (blockdiag) {
+            Lsubk3 <- matrix(0,3*k,3*k)
+            Lsubk3[1:k,1:k] <- Lsubk
+            Lsubk3[(k+1):(2*k),(k+1):(2*k)] <- Lsubk
+            Lsubk3[(2*k+1):(3*k),(2*k+1):(3*k)] <- Lsubk
+        }
         
         return(list(L=L,Linv=L1,Lsubk=Lsubk,Lsubk3=Lsubk3))
     }
     else if (dim(matrix)[2] == 2) {
         
-        out <- CreateL2D(matrix, lambda)
+        out <- CreateL2D(matrix, lambda, blockdiag=blockdiag)
         return(out)
     } else
         stop("only works for matrices with 2 or 3 columns")
 }
-CreateL2D <- function(matrix, lambda=0)
+CreateL2D <- function(matrix, lambda=0, blockdiag=TRUE)
 {
     k <- dim(matrix)[1]
     K <- matrix(0,k,k)
@@ -55,6 +58,8 @@ CreateL2D <- function(matrix, lambda=0)
             L1 <- ginv(L)		
         }
     Lsubk <- L1[1:k,1:k]
-    Lsubk3 <- rbind(cbind(Lsubk,matrix(0,k,k)),cbind(matrix(0,k,k),Lsubk))
+    Lsubk3 <- NULL
+    if (blockdiag)
+        Lsubk3 <- rbind(cbind(Lsubk,matrix(0,k,k)),cbind(matrix(0,k,k),Lsubk))
     return(list(L=L,Linv=L1,Lsubk=Lsubk,Lsubk3=Lsubk3))
 }
--- R/calcGamma.r
+++ R/calcGamma.r
@@ -1,16 +1,16 @@
 calcGamma <- function(Gamma0,Lsubk3,U,dims,weights=NULL)
   {
       U <- as( U,"sparseMatrix")
-      ULU <- Matrix::crossprod(U,(Lsubk3%*%U))
-      ULU2 <-  Matrix::crossprod(U,(Lsubk3%*%Gamma0))
+      ULU <- crossprod(U,(Lsubk3%*%U))
+      ULU2 <-  crossprod(U,(Lsubk3%*%Gamma0))
       diag(ULU) <- diag(ULU)+1e-8
       if (is.null(weights))
             weights <- 1
-      Gamma1 <- try(Gamma0 - weights*(U%*%Matrix::solve(ULU,ULU2)))
+      Gamma1 <- try(Gamma0 - weights*(U%*%solve(ULU,ULU2)))
       if (class(Gamma1)=="try-error") {
           cat("calcGamma: singular matrix: general inverse will be used.\n")
           B <- ginv(as.matrix(ULU))		
-          Gamma1 <- Gamma0-weights*(U%*%B%*%Matrix::crossprod(U,(Lsubk3%*%Gamma0)))
+          Gamma1 <- Gamma0-weights*(U%*%B%*%crossprod(U,(Lsubk3%*%Gamma0)))
     }
       Gamatrix <- matrix(Gamma1,length(Gamma1)/dims,dims)
       return(list(Gamma1=Gamma1,Gamatrix=Gamatrix))
