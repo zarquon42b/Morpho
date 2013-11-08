d8837891c3c444e429bd1788becb84fd20813700
schlager <zarquon42@gmail.com>
2013-11-08 09:25:56 +0100

    commented out the answers in find.outliers to be able to run run_examples(...,run=FALSE) without errors

--- R/Morpho-package.R
+++ R/Morpho-package.R
@@ -11,8 +11,8 @@
 #' \tabular{ll}{
 #' Package: \tab Morpho\cr
 #' Type: \tab Package\cr
-#' Version: \tab 1.0-2.131107\cr
-#' Date: \tab 2013-11-07\cr
+#' Version: \tab 1.0-2.131108\cr
+#' Date: \tab 2013-11-08\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr
 #' }
