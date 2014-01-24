2aafe5f2be74a0f17c382104803e8aee483d6c7a
schlager <zarquon42@gmail.com>
2014-01-24 15:26:48 +0100

    removed Matrix as dependency

--- R/Morpho-package.R
+++ R/Morpho-package.R
@@ -37,11 +37,11 @@
 #' @import doParallel
 #' @importFrom colorRamps blue2green2red
 #' @importFrom foreach foreach '%dopar%' '%do%'
-#' @importFrom Matrix sparseMatrix diag crossprod solve
+## @importFrom Matrix sparseMatrix diag crossprod solve
 #' @importFrom parallel mclapply detectCores
 #' @importFrom rgl lines3d open3d points3d rgl.bg rgl.bringtotop rgl.clear rgl.close  rgl.cur rgl.pop rgl.snapshot shade3d spheres3d text3d translate3d wire3d
 #' @importFrom yaImpute ann
-#' @importClassesFrom Matrix dgCMatrix dgeMatrix dsCMatrix dtCMatrix sparseMatrix
+## @importClassesFrom Matrix dgCMatrix dgeMatrix dsCMatrix dtCMatrix sparseMatrix
 NULL
 
 
