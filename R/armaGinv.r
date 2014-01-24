d9c65a8795c377cdd4ef9821e94f892495918313
schlager <zarquon42@gmail.com>
2014-01-24 14:57:07 +0100

    some more changes in armaGinv(.r/.cpp)

--- R/armaGinv.r
+++ R/armaGinv.r
@@ -1,6 +1,6 @@
-armaGinv <- function(x, tol= sqrt(.Machine$double.eps))
+armaGinv <- function(x, tol=NULL)
     {
-        if (!is.matrix(x))
+        if (!is.matrix(x) || !is.numeric(x))
             stop("input must be a matrix")
         out <- .Call("armaGinv", x ,tol)
         return(out)
--- src/armaGinv.cpp
+++ src/armaGinv.cpp
@@ -1,15 +1,25 @@
 #include <RcppArmadillo.h>
+#include <Rcpp.h>
 
 using namespace Rcpp;
 using namespace std;
 using namespace arma;
 
 
-RcppExport SEXP armaGinv(SEXP matIn_, double *tol) {
+RcppExport SEXP armaGinv(SEXP matIn_, SEXP tol_) {
+  if (!Rf_isMatrix(matIn_)){
+    return wrap(1);
+  } else {
   NumericMatrix matIn(matIn_);
   mat matA(matIn.begin(), matIn.nrow(), matIn.ncol());
-  mat invA = pinv(matA, *tol);
+  mat invA;
+  if (Rf_isNumeric(tol_)) {
+    double tol = as<double>(tol_);
+    invA = pinv(matA,tol);
+  } else {
+    invA = pinv(matA);
+  }
   return wrap(invA);
-  
+  }
 
 }
