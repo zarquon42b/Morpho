4d5a66f299fbf6e50db8d535770ba9da3aed9871
schlager <zarquon42@gmail.com>
2014-01-27 15:25:54 +0100

    pushed version to daily

--- R/Morpho-package.R
+++ R/Morpho-package.R
@@ -11,8 +11,8 @@
 #' \tabular{ll}{
 #' Package: \tab Morpho\cr
 #' Type: \tab Package\cr
-#' Version: \tab 1.99.140124\cr
-#' Date: \tab 2014-01-24\cr
+#' Version: \tab 1.99.140127\cr
+#' Date: \tab 2014-01-27\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr
 #' }
