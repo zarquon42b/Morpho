89fa4b50405b850226a060736dfc637aa784d2ba
schlager <zarquon42@gmail.com>
2014-01-27 15:43:13 +0100

    added line break in example of warpmovie3d to avoid complaint during CRAN check

--- R/warpmovie.matrix.r
+++ R/warpmovie.matrix.r
@@ -57,7 +57,8 @@
 #' warpmovie3d(shortnose.mesh,warpnose.long,n=15)## create 15 images.
 #' 
 #' ### ad some landmarks
-#' warpmovie3d(shortnose.mesh,warpnose.long,n=15,xland=shortnose.lm, yland=longnose.lm)## create 15 images.
+#' warpmovie3d(shortnose.mesh,warpnose.long,n=15,xland=shortnose.lm,
+#'             yland=longnose.lm)## create 15 images.
 #'
 #' 
 #' ### restrict to landmarks
