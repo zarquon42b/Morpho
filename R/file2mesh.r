3af37286a469274eeb25a8b08537f8eae37bf177
schlager <zarquon42@gmail.com>
2014-02-03 16:02:15 +0100

    addes missing character in file2mesh

--- R/file2mesh.r
+++ R/file2mesh.r
@@ -2,7 +2,7 @@
 #' @importFrom Rvcg vcgImport
 #' @export
 file2mesh <- function(filename,clean=TRUE,readcol=FALSE) {
-    mesh <- vcgImport(filename, clean=clean, readcol=readcol)
+    mesh <- vcgImport(filename, clean=clean, readcolor=readcol)
     return(mesh)
 }
 	
