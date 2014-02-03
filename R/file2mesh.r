242baa0b2321583496a049ed923b350f87e845b6
schlager <zarquon42@gmail.com>
2014-02-03 15:31:16 +0100

    made file2mesh a wrapper for vcgImport

--- R/file2mesh.r
+++ R/file2mesh.r
@@ -1,18 +1,8 @@
 #' @rdname ply2mesh
+#' @importFrom Rvcg vcgImport
 #' @export
-file2mesh <- function(filename,clean=TRUE,readcol=FALSE)
- 
-{
-  options=NULL
-  if (!clean)
-    {options <- paste(options,"--noclean")
-   }
-  if (readcol)
-    {options <- paste(options,"--color")
-   }
-  system(paste("ply2ascii ",filename," dump.ascii.ply",options,sep=""))
-  mesh <- ply2mesh("dump.ascii.ply",readcol=readcol)
-  unlink("dump.ascii.ply")
-  return(mesh)
+file2mesh <- function(filename,clean=TRUE,readcol=FALSE) {
+    mesh <- vcgImport(filename, clean=clean, readcol=readcol)
+    return(mesh)
 }
 	
