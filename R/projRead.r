e9c02cbdb1f34b100a73baea1f7e1f643d59fdb2
schlager <zarquon42@gmail.com>
2014-02-04 15:08:37 +0100

    removed now obsolete projBack and speed up projRead by removing redunand operations.

--- R/projRead.r
+++ R/projRead.r
@@ -1,28 +1,3 @@
-projBack <- function(data,surface,dataname=NULL,outname=NULL,smooth=TRUE,ignore.stdout=FALSE,sign=FALSE)
-{
-  checkCLI <- try(system("trimesh_project",intern=TRUE),silent=TRUE)
-   if(class(checkCLI) == "try-error")
-      stop("please install trimesh-tools")
-  options <- NULL
-  
-  if (!smooth)
-      options <- paste(options,"--nosmooth")
-  if (sign)
-      options <- paste(options,"--sign")
-  if (is.null(dataname))
-      dataname <- "out"
-  write.obj(cbind("v",data),filename=dataname)
-  if (is.null(outname))
-      command <- paste("trimesh_project"," ",dataname,".obj"," ",surface,options,sep="")  
-  else
-      command <- paste("trimesh_project"," ",dataname,".obj"," ",surface," -o ",outname,options,sep="")
-  
-  system(command,ignore.stdout=ignore.stdout)
-  unlink(paste(dataname,".obj",sep="")) #clean up
-}
-
-
-
 #' Project points onto the closest point on a mesh
 #' 
 #' project points onto a given surface and return projected points and normals.
@@ -33,11 +8,11 @@ projBack <- function(data,surface,dataname=NULL,outname=NULL,smooth=TRUE,ignore.
 #' @param readnormals logical: return normals of projected points.
 #' @param smooth logical: rerturn smoothed normals.
 #' @param sign logical: request signed distances.
-#' @param \dots additional arguments not used at the moment.
+#' @param \dots additional arguments currently not used.
 #' @return if readnormals = FALSE, a m x 3 matrix containing projected points
 #' is returned, otherwise a list, where
 #' \item{vb }{3 x m matrix containing projected points}
-#' \item{normals }{3 x m matrix containing normals}
+#' \item{normals }{3 x m matrix containing normals} 
 #' \item{quality }{vector containing distances }
 #' @author Stefan Schlager
 #' @seealso \code{\link{closemeshKD}}
@@ -59,7 +34,7 @@ projBack <- function(data,surface,dataname=NULL,outname=NULL,smooth=TRUE,ignore.
 projRead <- function(lm, mesh,readnormals=TRUE, smooth=TRUE, sign=TRUE,...)
 {
     if (is.character(mesh))
-        mesh <- vcgImport(mesh)
+        mesh <- vcgImport(mesh,updateNormals=FALSE,clean=FALSE)
     
     data <- vcgClost(lm, mesh, smoothNormals=smooth,sign=sign,borderchk=FALSE)
     if (!readnormals)
