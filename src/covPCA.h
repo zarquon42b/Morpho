beb784a1c60e27c56b84b7b4db47604fd106f0ad
schlager <zarquon42@gmail.com>
2014-02-05 14:38:15 +0100

    added header file for covPCA and commented C++ code

--- src/covPCA.h
+++ src/covPCA.h
@@ -1,7 +1,9 @@
 #ifndef _covPCA_H
 #define _covPCA_H
 #include <RcppArmadillo.h>
-
+using namespace Rcpp;
+using namespace std;
+using namespace arma;
 
 double covDist(mat &s1, mat &s2);
 
@@ -15,13 +17,5 @@ List covMDS(mat &dists);
 
 RcppExport SEXP covPCAwrap(SEXP data_, SEXP groups_, SEXP scramble_, SEXP rounds_);
 
-RcppExport SEXP covWrap(SEXP s1_, SEXP s2_) {
-  NumericMatrix s1(s1_);
-  NumericMatrix s2(s2_);
-  mat S1(s1.begin(), s1.nrow(), s1.ncol());
-  mat S2(s2.begin(), s2.nrow(), s2.ncol());
-  double cdist = covDist(S1, S2);
-  return wrap(cdist);
-
-}  
+RcppExport SEXP covWrap(SEXP s1_, SEXP s2_);   
 #endif
