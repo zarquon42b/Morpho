2af571648baaab2feca128f2300ccb1f812d296a
schlager <zarquon42@gmail.com>
2014-02-24 10:34:28 +0100

    made arrMean3 public function

--- R/addo.r
+++ R/addo.r
@@ -4,6 +4,18 @@ addo <- function(arr) {
     out <- .Call("addo",arr)
     return(out)
 }
+#' calculate mean of an array
+#'
+#' calculate mean of a 3D-array (e.g. containing landmarks) (fast) using the Armadillo C++ Backend
+#'
+#' @param arr \code{k x m x n} dimensional numeric array
+#' @return matrix of dimensions \code{k x m}.
+#' @note this is the same as \code{apply(arr, 1:2, mean)}, only faster for large configurations.
+#' @examples
+#' data(boneData)
+#' proc <- ProcGPA(boneLM, silent = TRUE)
+#' mshape <- arrMean3(proc$rotated)
+#' @export
 arrMean3 <- function(arr) {
     if (!is.numeric(arr) || length(dim(arr)) != 3)
         stop("please provide 3D numeric array")
