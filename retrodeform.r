45e49529773cbc9236d3ad79ed3267081d89983f
schlager <zarquon42@gmail.com>
2014-07-30 11:25:13 +0200

    finished retrodeform

--- retrodeform.r
+++ retrodeform.r
@@ -1,44 +1,28 @@
 #' @importFrom Rvcg vcgKDtree
-wNeighbour <- function(mat,pairedLM,hmult=5) {
+getLocalStretchNoArticulate <- function(mat,pairedLM,hmult=5) {
     npair <- nrow(pairedLM)
     P <- mat[pairedLM[,1],]
     Q <- mat[pairedLM[,2],]
-    nnpd <- vcgKDtree(mat[pairedLM[,1],],mat[pairedLM[,1],],2)$distance[,-1]
-   
-    nnqd <- vcgKDtree(mat[pairedLM[,2],],mat[pairedLM[,2],],2)$distance[,-1]
-    h <- hmult*mean(c(nnpd,nnqd))
-    h2 <- h^2
-    dp <- exp(-as.matrix(dist(mat[pairedLM[,1]])^2)/h2)
-    dq <- exp(-as.matrix(dist(mat[pairedLM[,2]]))^2/h2)
+    PhiIJ <- GetPhi(P,Q,hmult)
     
-    arr <- bindArr(dp,dq,along=3)
-    PhiIJ <- apply(arr,1:2,min)
-    diag(PhiIJ) <- 1
     getSi <- function(i) {
-        Ptmp <- P*PhiIJ[,i]
-        Qtmp <- Q*PhiIJ[,i]
-        #Ptmp <- P
-        #Qtmp <- Q
-        trans <- colMeans(rbind(Qtmp,Ptmp))
+        trans <- apply(rbind(Q,P),2,weighted.mean,w=rep(PhiIJ[,i],2))
         Psweep <- sweep(P,2,trans)
         Qsweep <- sweep(Q,2,trans)
-        C <- 0
-        for (j in 1:nrow(Ptmp))
-            C <- PhiIJ[j,i]*(C+tcrossprod(Psweep[j,])+tcrossprod(Qsweep[j,]))
-        C <- C/(2*npair-1)
+        ##calc weighted covariance matrix
+        C <- cov.wt(rbind(Psweep,Qsweep),wt=rep(PhiIJ[,i],2),center=rep(0,3))$cov
         svdC <- eigen(C)
         Tinv <- svdC$vectors%*%diag(sqrt(svdC$values))%*%t(svdC$vectors)
         T <- svdC$vectors%*%diag(1/sqrt(svdC$values))%*%t(svdC$vectors)
         TP <- Psweep%*%T
         TQ <-Qsweep%*%T
-        
+        ## get orthogonal plane
         CQ <- 0
          for (j in 1:nrow(TP))
              CQ <- CQ+tcrossprod(TP[j,],TQ[j,])+ tcrossprod(TQ[j,],TP[j,])
-
+        
         eigCQ <- eigen(CQ)
         Hstar <- Tinv%*%eigCQ$vectors
-        Hstarunit <- apply(Hstar,2,function(x) x <- x/sqrt(sum(x^2)))
         w1 <- Hstar[,3]
         w1 <- w1/sqrt(sum(w1^2))
         n <- crossp(Hstar[,1],Hstar[,2])
@@ -68,40 +52,44 @@ wNeighbour <- function(mat,pairedLM,hmult=5) {
     
     return(append(lapply(1:npair,getSi),list(allphi = PhiIJ)))
 }
-
-retrodeform <- function(mat,pairedLM,hmult=5,alpha=0.01) {
-    #alpha <- 0.01
+#' @export
+retrodeform3d <- function(mat,pairedLM,hmult=5,alpha=0.01) {
+    hmultlocal <- hmult
     npair <- nrow(pairedLM)
     P <- mat[pairedLM[,1],]
     Q <- mat[pairedLM[,2],]
     Pdiff <- lapply(1:npair,function(x){ out <- t(t(-P)+P[x,]);return(out )})
-        Qdiff <- lapply(1:npair,function(x){ out <- t(t(-Q)+Q[x,]);return(out )})
+    Qdiff <- lapply(1:npair,function(x){ out <- t(t(-Q)+Q[x,]);return(out )})
     PQdiff <- lapply(1:npair,function(x){ out <- t(t(Q)-P[x,]);return(out )})
     QPdiff <- lapply(1:npair,function(x){ out <- t(t(-P)+Q[x,]);return(out )})
-    precode <- wNeighbour(mat,pairedLM,hmult=hmult)
-    sqrtPhiIJ <- (precode$allphi)
-    diag(sqrtPhiIJ) <- 0
-    Amat <- -8*(1+alpha)*sqrtPhiIJ
-    diagA <- 8*(1+alpha)*colSums(sqrtPhiIJ)
+    precode <- getLocalStretchNoArticulate(mat,pairedLM,hmult=hmultlocal)
+    PhiIJ <- GetPhi(P,Q,hmult)
+    diag(PhiIJ) <- 0
+
+    ## create normal equation system for y,z coordinates
+    Amat <- -8*(1+alpha)*PhiIJ
+    diagA <- 8*(1+alpha)*colSums(PhiIJ)
     diag(Amat) <- diagA
 
-    ## create Amat for case ri = si (x-dimension)
+    ## create normal equation system for x coordinates
+    ## create Amat for case ri = -si (x-dimension)
     ## first 2 terms
-    Amatx <- -8*sqrtPhiIJ
-    diag(Amatx) <- 8*colSums(sqrtPhiIJ)
+    alpha <- alpha*1
+    Amatx <- -8*PhiIJ
+    diag(Amatx) <- 8*colSums(PhiIJ)
     ## alpha terms
-    Amatx1 <- precode$allphi*4*alpha
-    newphi <- matrix(8,nrow(precode$allphi),ncol(precode$allphi))*alpha*precode$allphi
+    xPhiIJ <- GetPhi(P,Q,hmult)
+    Amatx1 <- xPhiIJ*8*alpha
+    newphi <- matrix(8,nrow(xPhiIJ),ncol(xPhiIJ))*alpha*xPhiIJ
     diag(newphi) <- 16*alpha*diag(precode$allphi)
     diag(Amatx1) <- colSums(newphi)
-    
     Amatx <- Amatx+Amatx1
-    Bmat <- sqrtPhiIJ*2
-    Bmat2 <- precode$allphi
-    #Bmat <- Bmat*t(sqrt(precode$allphi))
+
+    ##calculate constants for x y z normal equations
     QiMiPx <- QiMiQx <-QiPx <- QiQx <- NULL
     QiMiPy <- QiMiQy <- QiPy <- QiQy <- NULL
     QiMiPz <- QiMiQz <- QiPz <- QiQz <- NULL
+    
     for (i in 1:npair) {
         QiMiP <- t(precode[[i]]$Qi%*%precode[[i]]$Si%*%t(Pdiff[[i]]))
         QiMiQ <- t(precode[[i]]$Qi%*%precode[[i]]$Si%*%t(Qdiff[[i]]))
@@ -120,35 +108,58 @@ retrodeform <- function(mat,pairedLM,hmult=5,alpha=0.01) {
         QiQy <-  cbind(QiQy,(QiQ[,2]))
         QiQz <-  cbind(QiQz,(QiQ[,3]))
     }
-    bx <- 0
-    by <- NULL
-    bz <- NULL
-    Bmatx <- precode$allphi*2
+
+    ##calculate weights for constants in normal equation (y,z coords)
+    Bmat <- PhiIJ*2
+    ##calculate weights for constants in normal equation x-coords
+    Bmatx <- xPhiIJ*2
     diag(Bmatx) <- 4
+    Bmatx <- Bmatx
+    bx <- by <- bz <- 0
+    ## x dim precalculation
+    constPx <- Bmat*QiMiPx
+    constQx <- Bmat*QiMiQx
+    constQix <- alpha*Bmatx*QiQx
+    constPix <- alpha*Bmatx*QiPx
+
+    ## ydim precalculation
+    constPy <- Bmat*QiMiPy
+    constQy <- Bmat*QiMiQy
+    constQiy <- alpha*Bmat*QiQy
+    constPiy <- alpha*Bmat*QiPy
+
+    ## z-dim precalculation
+    constPz <- Bmat*QiMiPz
+    constQz <- Bmat*QiMiQz
+    constQiz <- alpha*Bmat*QiQz
+    constPiz <- alpha*Bmat*QiPz
+    
     for (i in 1:npair) {
-        tmp <- Bmat*QiMiPx
-        tmpQ <- Bmat*QiMiQx
-        tmpQi <- alpha*Bmatx*QiQx
-        tmpPi <- alpha*Bmatx*QiPx
-        bx[i] <- +sum(tmp[,i])-sum(tmp[i,])-sum(tmpQ[,i])+sum(tmpQ[i,])-sum(tmpPi[,i])-sum(tmpPi[i,])-sum(tmpQi[,i])-sum(tmpQi[i,])
+        bx[i] <- -sum(constPx[,i])+sum(constPx[i,])+sum(constQx[,i])-sum(constQx[i,])+1*(sum(constPix[,i])+sum(constPix[i,])+sum(constQix[,i])+sum(constQix[i,]))
        
-        tmp <- Bmat*QiMiPy
-        tmpQ <- Bmat*QiMiQy
-        tmpQi <- alpha*Bmat*QiQy
-        tmpPi <- alpha*Bmat*QiPy
-        by[i] <- -sum(tmp[,i])+sum(tmp[i,])-sum(tmpQ[,i])+sum(tmpQ[i,])-sum(tmpPi[,i])+sum(tmpPi[i,])-sum(tmpQi[,i])+sum(tmpQi[i,])
-        tmp <- Bmat*QiMiPz
-        tmpQ <- Bmat*QiMiQz
-        tmpQi <- alpha*Bmat*QiQz
-        tmpPi <- alpha*Bmat*QiPz
-        bz[i] <- -sum(tmp[,i])+sum(tmp[i,])-sum(tmpQ[,i])+sum(tmpQ[i,])-sum(tmpPi[,i])+sum(tmpPi[i,])-sum(tmpQi[,i])+sum(tmpQi[i,])
+        
+        by[i] <- -sum(constPy[,i])+sum(constPy[i,])-sum(constQy[,i])+sum(constQy[i,])-sum(constPiy[,i])+sum(constPiy[i,])-sum(constQiy[,i])+sum(constQiy[i,])
+
+        
+        bz[i] <- -sum(constPz[,i])+sum(constPz[i,])-sum(constQz[,i])+sum(constQz[i,])-sum(constPiz[,i])+sum(constPiz[i,])-sum(constQiz[,i])+sum(constQiz[i,])
     }
-    a <- cbind(Morpho:::armaGinv(Amatx)%*%(bx),Morpho:::armaGinv(Amat)%*%by,Morpho:::armaGinv(Amat)%*%bz)
+    a <- cbind(Morpho:::armaGinv(Amatx)%*%bx,Morpho:::armaGinv(Amat)%*%-by,Morpho:::armaGinv(Amat)%*%-bz)
     a1 <- a
     a1[,1] <- -a[,1]
     a <- rbind(a1,a)
-    check <- as.logical(rotonto(mat,a)$reflect)
-    if (check)
-        a[,1] <- -a[,1]
     return(a)
+}
+
+GetPhi <- function(P,Q,hmult) {
+    nnpd <- vcgKDtree(P,P,2)$distance[,-1]
+    nnqd <- vcgKDtree(Q,Q,2)$distance[,-1]
+    h <- hmult*mean(c(nnpd,nnqd))
+    h2 <- h^2
+    dp <- exp(-as.matrix(dist(mat[pairedLM[,1]])^2)/h2)
+    dq <- exp(-as.matrix(dist(mat[pairedLM[,2]]))^2/h2)
+    
+    arr <- bindArr(dp,dq,along=3)
+    PhiIJ <- apply(arr,1:2,min)
+    diag(PhiIJ) <- 1
+    return(PhiIJ)
 }
