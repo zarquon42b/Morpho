11d231b6d0ed3be0ff3c2a228b924d0422980357
schlager <zarquon42@gmail.com>
2014-10-17 20:18:20 +0200

    made read.lmdta more versatile

--- R/read.lmdta.r
+++ R/read.lmdta.r
@@ -15,16 +15,20 @@
 #' 
 #' @export
 read.lmdta <- function(file="x", na=9999)
-{         x <- file
-          A <- readLines(x)
-          em <- which(A=="")
-          idnames <- A[c((em[1]+1):(em[2]-1))]
-          info <- strsplit(A[3]," ")[[1]]
-          n2 <- nchar(info[2])-1
-          nspeci <- as.numeric(substr(info[2],1L,n2))
-          ndim <- as.numeric(substr(info[6],5,nchar(info[6])))
-          nlms <- as.numeric(info[3])/ndim
-          eot <- em[2]
+{
+    x <- file
+    A <- readLines(x)
+    em <- which(A=="")
+    infoline <- grep("DIM=",A,ignore.case = TRUE)
+    idlines <- infoline+min(which(A[-c(1:infoline)] != ""))
+    endid <- idlines+min(which(A[-c(1:idlines)] == ""))
+    idnames <- A[c((idlines):(endid-1))]
+    info <- strsplit(A[infoline]," ")[[1]]
+    n2 <- nchar(info[2])-1
+    nspeci <- as.numeric(substr(info[2],1L,n2))
+    ndim <- as.numeric(substr(info[6],5,nchar(info[6])))
+    nlms <- as.numeric(info[3])/ndim
+    eot <- endid
           B <- as.matrix(read.table(x,skip=eot),na.strings=as.numeric(info[5]))
           tt <- array(t(B),dim=c(ndim,nlms,nspeci))
           arr <- array(NA,dim=c(nlms,ndim,nspeci))
