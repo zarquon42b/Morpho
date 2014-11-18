b19d8e0edba78023abd39d945a426d565d2549e4
schlager <zarquon42@gmail.com>
2014-11-18 09:26:33 +0100

    added vector norming in crossp and made point2plane more efficient

--- R/crossp.r
+++ R/crossp.r
@@ -6,6 +6,7 @@ crossp <- function(x,y)
  	out[1] <- x[2]*y[3]-x[3]*y[2]
 	out[2] <- x[3]*y[1]-x[1]*y[3]
 	out[3] <- x[1]*y[2]-x[2]*y[1]
+        out <- out/norm(out,"2")
  return(out)
  }
 
--- R/point2plane.r
+++ R/point2plane.r
@@ -17,10 +17,9 @@
 #' #visualize
 #' wire3d(skull_0144_ch_fe.mesh,col="white")
 #' ##get plane normal
-#' pNorm <- crossp(boneLM[3,,1]-boneLM[9,,1],boneLM[4,,1]-boneLM[9,,1])
-#' pNorm <- pNorm/norm(pNorm,"2")
-#' ## get plane offset
-#' d <- norm(point2plane(c(0,0,0),pt=boneLM[9,,1],pNorm=pNorm),"2")
+#' normal <- crossp(boneLM[3,,1]-boneLM[9,,1],boneLM[4,,1]-boneLM[9,,1])
+#' #' ## get plane offset
+#' d <- norm(point2plane(c(0,0,0),pt=boneLM[9,,1],normal=normal),"2")
 #' spheres3d(boneLM[,,1],radius=0.5)
 #' spheres3d(boneLM[c(3,4,9),,1],radius=0.6,col=3)
 #' ##original position of Rhinion
@@ -29,69 +28,37 @@
 #' spheres3d(rpro,radius=0.9,col=6)
 #' lines3d(rbind(rpro,boneLM[10,,1]),lwd=3)
 #' ##plot plane
-#' planes3d(pNorm[1],pNorm[2],pNorm[3],d=d,col=2,alpha=0.5)
+#' planes3d(normal[1],normal[2],normal[3],d=d,col=2,alpha=0.5)
 #'
 #' ##now we project all points onto that plane:
 #' spheres3d(point2plane(boneLM[,,1],pt=boneLM[9,,1],pt1=boneLM[3,,1],pt2=boneLM[4,,1]),col=3)
 #'
 #' ## and finally project the vertices of the mesh onto the plane
-#' meshpro <- point2plane(vert2points(skull_0144_ch_fe.mesh),pt=boneLM[9,,1],pNorm=pNorm)
+#' meshpro <- point2plane(vert2points(skull_0144_ch_fe.mesh),pt=boneLM[9,,1],normal=normal)
 #' points3d(meshpro,col=2)
 #' }
 #' @rdname point2plane
 #' @export
-point2plane <- function(x, pt, pNorm=NULL, pt1=NULL, pt2=NULL) UseMethod("point2plane")
+point2plane <- function(x, pt, normal=NULL, pt1=NULL, pt2=NULL) {
+    if (is.vector(x))
+        x <- matrix(x,1,3)
 
-#' @rdname point2plane
-#' @export
-point2plane.default <- function(x, pt, pNorm=NULL, pt1=NULL, pt2=NULL) {
-    if (is.null(pNorm) && is.null(pt1) && is.null(pt2))
-        stop("either specify pNorm or pt1 and pt2")
-    if (is.null(pNorm)) {
-        v1 <- pt1-pt
-        v2 <- pt2-pt
-        pNorm <- crossp(v1,v2)
+        if (is.null(normal) && is.null(pt1) && is.null(pt2))
+        stop("either specify normal or pt1 and pt2")
+    if (is.null(normal)) {
+        e1 <- pt1-pt
+        e1 <- e1/norm(e1,"2")
+        e2 <- pt2-pt
+        normal <- crossp(e1,e2)
+        e2 <- crossp(e1,normal)
+    } else {
+        tp <- tanplan(normal)
+        e1 <- tp$z
+        e2 <- tp$y
     }
-    pNorm <- pNorm/norm(pNorm,"2")
-    xdiff <- x-pt
-    xnorm <- norm(xdiff,"2")
-    ang <- angle.calc(xdiff,pNorm)
-    if (is.nan(ang))
-        ang <- 0
-    if (ang > pi/2) {
-        pNorm <- -pNorm
-        ang <- pi-ang
-    }
-    nlen <- cos(ang)*xnorm
-    xpro <- x-pNorm*nlen
-    return(xpro)
-}
-
-#' @rdname point2plane
-#' @export
-point2plane.matrix <- function(x, pt, pNorm=NULL, pt1=NULL, pt2=NULL) {
-    if (is.null(pNorm) && is.null(pt1) && is.null(pt2))
-        stop("either specify pNorm or pt1 and pt2")
-    if (is.null(pNorm)) {
-        v1 <- pt1-pt
-        v2 <- pt2-pt
-        pNorm <- crossp(v1,v2)
-    }
-    pNorm <- pNorm/norm(pNorm,"2")
-    xdiff <- sweep(x,2,pt)
-    xnorm <- sqrt(rowSums(xdiff^2))
-    ang <- apply(xdiff,1,angle.calc,y=pNorm)
-    if (length(is.nan(ang)))
-        ang[which(is.nan(ang))] <- 0
-    sapply(ang,function(ang) {
-        if (ang > pi/2) {
-            pNorm <- -pNorm
-            ang <- pi-ang
-        } else
-            ang <- ang
-        return(ang)
-    })
-    nlen <- cos(ang)*xnorm
-    xpro <- x-nlen*matrix(pNorm,nrow(x),ncol(x),byrow = T)
-    return(xpro)
+    Ep <- cbind(e1,e2)
+    pointcloud0 <- sweep(x,2,pt)
+    orthopro <- t(Ep%*%t(Ep)%*%t(pointcloud0))
+    orthopro <- sweep(orthopro,2,-pt)
+    return(orthopro)
 }
