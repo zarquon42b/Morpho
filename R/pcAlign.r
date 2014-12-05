d4d8ef9efd76b7344ee4cf2f7ad6dbf1019f46cc
schlager <zarquon42@gmail.com>
2014-12-05 13:10:50 +0100

    pcAligned with y missing now centers a shape and aligns it by its principal axes.

--- R/pcAlign.r
+++ R/pcAlign.r
@@ -2,7 +2,7 @@
 #'
 #' align two 3D-pointclouds/meshes by their principal axes
 #' @param x matrix or mesh3d
-#' @param y matrix or mesh3d
+#' @param y matrix or mesh3d, if missing, x will be centered by its centroid and aligned by its princial axis.
 #' @param optim logical if TRUE, all possible PC-axis are tested and the rotation with the smallest RMSE between configs will be used.
 #' @param subsample integer use subsampled points to decrease computation time
 #' 
@@ -25,58 +25,64 @@ pcAlign <- function(x,y,optim=TRUE,subsample=NULL)UseMethod("pcAlign")
 #' @rdname pcAlign
 #' @export
 pcAlign.matrix <- function(x, y,optim=TRUE,subsample=NULL) {
-    if (inherits(y,"mesh3d"))
-        y <- vert2points(y)
-    pca1 <- prcomp(x)
-    pca2 <- prcomp(y)
-    x <- apply(x,2,scale,scale=F)    
-    y <- apply(y,2,scale,scale=F)
-    rotx <- pca1$rotation
-    roty <- pca2$rotation
-    chk <- det(rotx%*%roty)
-    if (chk  < 0)
-        rotx[,3] <- -rotx[,3]
+    if (!missing(y)) {
+        if (inherits(y,"mesh3d"))
+            y <- vert2points(y)
+        pca1 <- prcomp(x)
+        pca2 <- prcomp(y)
+        x <- apply(x,2,scale,scale=F)    
+        y <- apply(y,2,scale,scale=F)
+        rotx <- pca1$rotation
+        roty <- pca2$rotation
+        chk <- det(rotx%*%roty)
+        if (chk  < 0)
+            rotx[,3] <- -rotx[,3]
 
-    x <- x%*%rotx
-    y <- y%*%roty
-    rotlist <- list(
-        arot=getTrafoRotaxis(pt1=c(1,0,0),pt2=c(0,0,0),theta=pi),
-        brot= getTrafoRotaxis(pt1=c(0,1,0),pt2=c(0,0,0),theta=pi),
-        crot = getTrafoRotaxis(pt1=c(0,0,1),pt2=c(0,0,0),theta=pi))
-    tests <- as.matrix(expand.grid(c(1,0),c(1,0),c(1,0)))
-    tmpfun <- function(x,rotlist){
-        for (i in 1:3) {
-            if (x[i] == 0)
-                rotlist[[i]] <- diag(4)
+        x <- x%*%rotx
+        y <- y%*%roty
+        rotlist <- list(
+            arot=getTrafoRotaxis(pt1=c(1,0,0),pt2=c(0,0,0),theta=pi),
+            brot= getTrafoRotaxis(pt1=c(0,1,0),pt2=c(0,0,0),theta=pi),
+            crot = getTrafoRotaxis(pt1=c(0,0,1),pt2=c(0,0,0),theta=pi))
+        tests <- as.matrix(expand.grid(c(1,0),c(1,0),c(1,0)))
+        tmpfun <- function(x,rotlist){
+            for (i in 1:3) {
+                if (x[i] == 0)
+                    rotlist[[i]] <- diag(4)
+            }
+            return(rotlist)
         }
-        return(rotlist)
-    }
-    subs <- rep(FALSE,nrow(x))
-    if (!is.null(subsample)) {
-        subsample <- min(nrow(x)-1,subsample)
-        subs <- duplicated(kmeans(x,centers=subsample,iter.max =100)$cluster)
-    }
-    
-    dists <- 1e10
-    fintrafo <- diag(4)
-    for (i in 1:8) {
+        subs <- rep(FALSE,nrow(x))
+        if (!is.null(subsample)) {
+            subsample <- min(nrow(x)-1,subsample)
+            subs <- duplicated(kmeans(x,centers=subsample,iter.max =100)$cluster)
+        }
         
-        rottmp <- tmpfun(tests[i,],rotlist)
-        trafotmp <- rottmp[[1]]%*%rottmp[[2]]%*%rottmp[[3]]
-        xtmp <- applyTransform(x,trafotmp)
-        # print(system.time(disttmp <- mean(ann(xtmp,y,k=1,verbose = F,search.type = "priority")$knnIndexDist[2]^2)))
-        disttmp <- mean(vcgKDtree(y,xtmp[!subs,],k=1)$dist^2)
-        if (disttmp < dists) {
-            dists <- disttmp
-            fintrafo <- trafotmp
+        dists <- 1e10
+        fintrafo <- diag(4)
+        for (i in 1:8) {
             
+            rottmp <- tmpfun(tests[i,],rotlist)
+            trafotmp <- rottmp[[1]]%*%rottmp[[2]]%*%rottmp[[3]]
+            xtmp <- applyTransform(x,trafotmp)
+                                        # print(system.time(disttmp <- mean(ann(xtmp,y,k=1,verbose = F,search.type = "priority")$knnIndexDist[2]^2)))
+            disttmp <- mean(vcgKDtree(y,xtmp[!subs,],k=1)$dist^2)
+            if (disttmp < dists) {
+                dists <- disttmp
+                fintrafo <- trafotmp
+                
+            }
         }
+        x <- applyTransform(x,fintrafo)
+        x <- x%*%t(pca2$rotation)
+        x <- t(t(x)+pca2$center)
+        return(x)
+    } else {
+        pca1 <- prcomp(x)
+        return(pca1$x)
     }
-    x <- applyTransform(x,fintrafo)
-    x <- x%*%t(pca2$rotation)
-    x <- t(t(x)+pca2$center)
-    return(x)
-}
+        
+    }
 #' @rdname pcAlign
 #' @export
 pcAlign.mesh3d <- function(x,y,optim=TRUE,subsample=NULL) {
