b30acbfb3c71b6746905695657dfcc58abf5e7c9
Stefan Schlager <zarquon42@gmail.com>
2015-03-13 13:56:18 +0100

    added method for classifying and printing typprobClass results. also added loo cross-validation

--- R/classify.r
+++ R/classify.r
@@ -1,15 +1,16 @@
-#' classify specimen based on between-group PCA or CVA
+#' classify specimen based on between-group PCA or CVA or typprobClass
 #'
-#' classify specimen based on between-group PCA or CVA
+#' classify specimen based on between-group PCA, CVA or typprobClass
 #'
-#' @param x result of groupPCA or CVA
+#' @param x result of groupPCA, CVA or typprobClass
 #' @param cv logical: use cross-validated scores if available
 #' @return
 #' \item{class}{classification result}
 #' \item{groups}{original grouping variable}
 #'
-#' for object of CVA, also the posterior probabilities are returned.
+#' for object of CVA and typprob, also the posterior probabilities are returned.
 #' @rdname classify
+#' @seealso \code{\link{CVA},\link{groupPCA},  \link{typprobClass}}
 #' @export
 classify <- function(x,cv=TRUE) UseMethod("classify")
 
@@ -21,6 +22,8 @@ classify.bgPCA <- function(x,cv=TRUE) {
         CV <- x$CV
     else
         CV <- x$Scores
+    if (is.null(x$CV)) 
+        cv  <- FALSE
     classVec <- x$groups
     GmeanCenter <- sweep(x$groupmeans,2,x$Grandmean)
     GmeanScores <- GmeanCenter%*%x$groupPCs
@@ -28,7 +31,6 @@ classify.bgPCA <- function(x,cv=TRUE) {
         tmpdist <- (sqrt(rowSums(sweep(GmeanScores,2,CV[i,])^2)))
         classVec[i] <- names(tmpdist)[which(tmpdist == min(tmpdist))]
     }
-    #classVec <- factor(classVec)
     out <- list(class=classVec,groups=x$groups,cv=cv)
     class(out) <- "classify"
     return(out)
@@ -39,6 +41,8 @@ classify.bgPCA <- function(x,cv=TRUE) {
 classify.CVA <- function(x,cv=T) {
 
     if (!is.null(x$class) && cv) {
+        if (is.null(x$CVcv)) 
+            cv  <- FALSE
         out <- list(class=x$class,groups=x$groups,posterior=x$posterior,cv=cv)
         class(out) <- "classify"
         return(out)
@@ -60,31 +64,65 @@ classify.CVA <- function(x,cv=T) {
         }
         names(classVec) <- rownames(classprobs) <- rownames(x$CVscores)
         colnames(classprobs) <- rownames(x$groupmeans)
-        #classVec <- factor(classVec)
+                                        #classVec <- factor(classVec)
         out <- list(class=classVec,groups=x$groups,posterior=classprobs,cv=cv)
         class(out) <- "classify"
         return(out)
     }
 }
-    
-    
+
+#' @rdname classify
+#' @export
+classify.typprob <- function(x,cv=TRUE) {
+    out <- list()
+    if (!x$cv)
+        cv <- FALSE
+    if (cv) {
+        out$class <- x$groupaffinCV
+        out$groups <- x$groups
+        out$posterior <- x$probsCV
+    } else {        
+        out$class <- x$groupaffin
+        out$groups <- x$groups
+        out$posterior <- x$classprobs
+    }
+    out$cv=cv
+    class(out) <- "classify"
+    attributes(out) <- append(attributes(out),list(self=x$self))
+    return(out)
+}   
+
 #' @export       
 print.classify <- function(x,...) {
-    if (x$cv)
-        cat(" cross-validated classification results in frequencies\n")
-    else 
-    cat(" classification result in frequencies\n")
-    tab <- table(x$groups,x$class)
-    acc <- 100*sum(diag(tab))/sum(tab)
-    
-    probtab <- prop.table(tab,1)*100
-    print(tab)
-    if (x$cv)
-        cat("\n\n cross-validated classification result in %\n")
+    if (is.null(attributes(x)$self))
+        self <- TRUE
     else
-        cat("\n\n classification result in %\n")
-    print(probtab,digits=5)
-    
-    cat(paste0("\n\n overall classification accuracy: ",round(acc,digits=5)," %\n"))
+        self <- attributes(x)$self
+    if (self) {
+        if (x$cv)
+            cat(" cross-validated classification results in frequencies\n")
+        else 
+            cat(" classification result in frequencies\n")
+        tab <- table(x$groups,x$class)
+        acc <- 0
+        good <- which(as.character(x$groups) == as.character(x$class))
+        acc <- 100*length(good)/length(x$groups)
+        
+        
+        probtab <- prop.table(tab,1)*100
+        print(tab)
+        if (x$cv)
+            cat("\n\n cross-validated classification result in %\n")
+        else
+            cat("\n\n classification result in %\n")
+        print(probtab,digits=5)
+        
+        cat(paste0("\n\n overall classification accuracy: ",round(acc,digits=5)," %\n"))
+    } else {
+        numbs <- tapply(x$class,x$class,length)
+        cat("\n\n specimens have been classified as:\n\n ")
+        print(numbs)
+    }
+                                        
 }
-    
+
--- R/typprob.r
+++ R/typprob.r
@@ -6,7 +6,7 @@
 #' 
 #' @title calculate typicality probabilities
 #' @param x vector or matrix of data the probability is to be calculated.
-#' @param data Reference data set.
+#' @param data Reference data set. If missing x will be used.
 #' @param groups vector containing grouping information.
 #' @param small adjustion of Mahalanobis D^2 for small sample sizes as
 #' suggested by Wilson (1981), only takes effect when method="wilson".
@@ -23,12 +23,16 @@
 #' defined, group mean will be used.
 #' @param cova covariance matrix to calculate mahalanobis-distance: specify
 #' custom covariance matrix to calculate distance.
+#' @param cv logical: if data is missing and \code{cv=TRUE}, the resulting classification will be validated by leaving-one-out crossvalidation.
 #' @return typprob: returns a vector of probabilities.
 #' 
 #' typprobClass:
 #' \item{probs }{matrix of probabilities for each group}
-#' \item{groupaffin }{vector of groups each specimen has been assigned to.
-#' Outliers are classified "none"}
+#' \item{groupaffin }{vector of groups each specimen has been assigned to. Outliers are classified "none"}
+#' \item{probsCV }{cross-validated matrix of probabilities for each group}
+#' \item{groupaffinCV }{cross-validated vector of groups each specimen has been assigned to. Outliers are classified "none"}
+#' \item{self }{logical: if TRUE, the data has been classified by self-inference.}
+#'
 #' @author Stefan Schlager
 #' @references Albrecht G. 1992. Assessing the affinities of fossils using
 #' canonical variates and generalized distances Human Evolution 7:49-69.
@@ -48,21 +52,16 @@
 #' 
 #' data2 <- procSym(abind(gorf.dat,gorm.dat))$PCscores[,1:3]
 #' fac <- as.factor(c(rep("female",dim(gorf.dat)[3]),rep("male",dim(gorm.dat)[3])))
-#' typClass <- typprobClass(data2,data2,fac,method="w",small=TRUE)
+#' typClass <- typprobClass(data2,groups=fac,method="w",small=TRUE,cv=TRUE)
 #' ## only 59 specimen is rather small.
-#' typClass2 <- typprobClass(data2,data2,fac,method="c")## use default settings
+#' typClass2 <- typprobClass(data2,groups=fac,method="c",cv=TRUE)## use default settings
 #' 
 #' ### check results for first method:
-#' ct <- table(fac,typClass$groupaffin)
-#' ct #view classification table
-#' ### get percentage of correct classification
-#' prop.table(ct, 1)
+#' typClass
+#' 
 #' 
 #' ### check results for second method:
-#' ct1 <- table(fac,typClass2$groupaffin)
-#' ct1 #view classification table ### one specimen has been tagged an outlier.
-#' ### get percentage of correct classification
-#' prop.table(ct1, 1) 
+#' typClass2
 #' 
 #' 
 #' @rdname typprob
@@ -102,13 +101,17 @@ typprob <- function(x,data,small=FALSE, method=c("chisquare","wilson"), center=N
 
 #' @rdname typprob
 #' @export
-typprobClass <- function(x,data,groups,small=FALSE,method=c("chisquare","wilson"),outlier=0.01,sep=FALSE)
+typprobClass <- function(x,data,groups,small=FALSE,method=c("chisquare","wilson"),outlier=0.01,sep=FALSE,cv=TRUE)
 {
-    if (!is.factor(groups))
-        {
+    if (!is.factor(groups)) {
             groups <- as.factor(groups)
             warning("groups coerced to factors")
         }
+    self <- FALSE
+    if (missing(data)) {
+        data <- x
+        self <- TRUE
+    }
     probs <- NULL
     groups <- factor(groups)
     glev <- levels(groups)
@@ -130,11 +133,32 @@ typprobClass <- function(x,data,groups,small=FALSE,method=c("chisquare","wilson"
     outsider <- which(apply(probs,1,max) < outlier)
     classify[outsider] <- nlev+1
     groupaffin <- as.factor(glev[classify])
+    CV <- probsCV <- gaffinCV <- NULL
+    if (cv && self) {
+        probsCV <- probs
+        gaffinCV <- as.character(groupaffin)
+        
+        for(i in 1:length(groups)) {
+            tmp <- typprobClass(data[i,],data[-i,],groups=groups[-i],small=small,method=method,outlier = outlier, sep=sep,cv=FALSE)
+            probsCV[i,] <- tmp$probs
+            gaffinCV[i] <- as.character(tmp$groupaffin)
+        }
+    } else
+        cv <- FALSE
+    out <- (list(probs=probs,groupaffin=groupaffin,cv=cv,self=self))
+    out$probsCV <- probsCV
+    out$groupaffinCV <- factor(gaffinCV)
+    out$groups <- groups
     
-    out <- (list(probs=probs,groupaffin=groupaffin))
     class(out) <- "typprob"
     return(out)
 }
+
+#' @export
+print.typprob <- function(x,...) {
+    print(classify(x,cv=TRUE))
+}
+
 
 #' calculate the pooled within groups covariance matrix
 #'
