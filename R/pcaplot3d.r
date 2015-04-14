7becce163fcf62bd25ff121675dbab3d88256837
Stefan Schlager <zarquon42@gmail.com>
2015-04-14 15:33:53 +0200

    NEW: added option type to pcaplot3d to enable rendering landmarks as points

--- R/pcaplot3d.r
+++ R/pcaplot3d.r
@@ -13,6 +13,7 @@
 #' @param sym logical: if TRUE the symmetric component of shape is displayed.
 #' Otherwise the asymmetric one.
 #' @param legend logical: if TRUE a legend explaining the color coding of the PCs is plotted.
+#' @param type character: for \code{type="spheres"}, the landmarks will be rendered using rgl's \code{spheres3d} function and for \code{type="points"} by  \code{points3d} respectivly.
 #' @param \dots Additional parameters which will be passed to the methods.
 #' @return returns an invisible array containing the shapes associated with the Principal components selected.
 #' @seealso \code{\link{procSym}}
@@ -30,7 +31,7 @@ pcaplot3d <- function (x,...) UseMethod("pcaplot3d")
 #' @rdname pcaplot3d
 #'
 #' @export
-pcaplot3d.symproc <- function(x,pcshow=c(1,2,3),mag=3,color=4,lwd=1,sym=TRUE,legend=TRUE,...) 
+pcaplot3d.symproc <- function(x,pcshow=c(1,2,3),mag=3,color=4,lwd=1,sym=TRUE,legend=TRUE,type=c("spheres","points"),...) 
 {   
   refshape <- x$mshape
   if (sym) {
@@ -41,26 +42,26 @@ pcaplot3d.symproc <- function(x,pcshow=c(1,2,3),mag=3,color=4,lwd=1,sym=TRUE,leg
       Scores <- x$PCscore_asym
   }
   
-  .pcaplot3d(refshape, PCs, Scores, pcshow=pcshow, mag=mag,color=color, lwd=lwd,legend=legend)
+  .pcaplot3d(refshape, PCs, Scores, pcshow=pcshow, mag=mag,color=color, lwd=lwd,legend=legend,type=type)
 }
 #' @rdname pcaplot3d
 #'
 #' @export
-pcaplot3d.nosymproc <- function(x,pcshow=c(1,2,3),mag=3,color=4,lwd=1,legend=TRUE,...)
+pcaplot3d.nosymproc <- function(x,pcshow=c(1,2,3),mag=3,color=4,lwd=1,legend=TRUE,type=c("spheres","points"),...)
 {   
     refshape <- x$mshape
     PCs <- x$PCs
     Scores <- x$PCscores
-    .pcaplot3d(refshape, PCs, Scores, pcshow=pcshow, mag=mag,color=color, lwd=lwd, legend=legend)
+    .pcaplot3d(refshape, PCs, Scores, pcshow=pcshow, mag=mag,color=color, lwd=lwd, legend=legend, type=type)
 }
 
-.pcaplot3d <- function(refshape,PCs, Scores, pcshow=c(1,2,3), mag=3,color=4,lwd=1,legend=TRUE,...) {
+.pcaplot3d <- function(refshape,PCs, Scores, pcshow=c(1,2,3), mag=3,color=4,lwd=1,legend=TRUE,type=c("spheres","points"),...) {
     A <- refshape
     k <- dim(A)[1]
     m <- dim(A)[2]
     if (is.vector(PCs))
         PCs <- matrix(PCs,length(PCs),1)
-    
+    type <- match.arg(type,c("spheres","points"))
     npc <- dim(PCs)[2]
     lpc <- length(pcshow)
     rainb <- rainbow(lpc)
@@ -85,6 +86,9 @@ pcaplot3d.nosymproc <- function(x,pcshow=c(1,2,3),mag=3,color=4,lwd=1,legend=TRU
         plot(0,0, xlab="", ylab="", axes =F, cex=0,xlim=c(-1,1), ylim=c(-1,1))
         legend(-1,1, pch=20, cex=2, col=rainb, legend=paste0("PC",pcshow))
     }
-    spheres3d(refshape,  col = color,radius=sz)
+    if (type == "spheres")
+        spheres3d(refshape,  col = color,radius=sz)
+    else
+        points3d(refshape,col=color)
     invisible(outarr)
 }
