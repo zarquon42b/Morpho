213d463ecc55a6c3291471b9c4407fa12a6a490a
Stefan Schlager <zarquon42@gmail.com>
2015-04-14 12:03:23 +0200

    NEW: added option expect in getPCtol and getMeaningfulPCs

--- R/PCmeaning.r
+++ R/PCmeaning.r
@@ -2,24 +2,35 @@
 #'
 #' determine the minimum ratio for two subsequent eigenvalues to be considered different
 #' @param n sample size
+#' @param expect expectation value for chi-square distribution of df=2
 #' @return returns the minimum ratio between two subsequent subsequent eigenvalues to be considered different.
 #' @examples
 #' ## reproduce the graph from Bookstein (2014, p. 324)
+#' ## and then compare it to ratios for values to be considered
+#' ## statistically significant
 #' myseq <- seq(from=10,to = 50, by = 2)
 #' myseq <- c(myseq,seq(from=50,to=1000, by =20))
 #' ratios <- getPCtol(myseq)
-#' plot(log(myseq),ratios,cex=0,xaxt = "n")
+#' plot(log(myseq),ratios,cex=0,xaxt = "n",ylim=c(1,5.2))
 #' ticks <- c(10,20,50,100,200,300,400,500,600,700,800,900,1000)
 #' axis(1,at=log(ticks),labels=ticks)
 #' lines(log(myseq),ratios)
 #' abline(v=log(ticks), col="lightgray", lty="dotted")
-#' abline(h=seq(from=1.2,to=2.4, by = 0.1), col="lightgray", lty="dotted")
+#' abline(h=seq(from=1.2,to=5, by = 0.2), col="lightgray", lty="dotted")
+#'
+#' ## now we raise the bar and compute the ratios for values
+#' ## to be beyond the 95th percentile of
+#' ## the corresponding chi-square distribution:
+#' ratiosSig <- getPCtol(myseq,expect=qchisq(0.95,df=2))
+#' lines(log(myseq),ratiosSig,col=2)
+#'
+#' 
 #' @references
 #' Bookstein, F. L. Measuring and reasoning: numerical inference in the sciences. Cambridge University Press, 2014
 #' @seealso getMeaningfulPCs
 #' @export
-getPCtol <- function(n) {
-    x <- exp(1/n)+ sqrt(exp(2/n)-1)
+getPCtol <- function(n,expect=2) {
+    x <- exp(expect/(2*n))+ sqrt(exp((2*expect)/(2*n))-1)
     return(x^2)
 }
 #' get number of meaningful Principal components
@@ -27,6 +38,7 @@ getPCtol <- function(n) {
 #' get number of meaningful Principal components
 #' @param values eigenvalues from a PCA
 #' @param n sample size
+#' @param expect expectation value for chi-square distribution of df=2
 #' @param sdev logical: if TRUE, it is assumed that the values are square roots of eigenvalues.
 #' @return
 #' \item{tol}{threshold of ratio specific for \code{n}}
@@ -42,11 +54,11 @@ getPCtol <- function(n) {
 #' @references
 #' Bookstein, F. L. Measuring and reasoning: numerical inference in the sciences. Cambridge University Press, 2014
 #' @export
-getMeaningfulPCs <- function(values,n,sdev=FALSE) {
+getMeaningfulPCs <- function(values,n,expect=2,sdev=FALSE) {
     nv <- length(values)
     if (sdev)
         values <- values^2
-    tol <- getPCtol(n)
+    tol <- getPCtol(n,expect)
     coeff <- values[1:(nv-1)]/values[2:nv]
     
     bad <- which(coeff < tol)
