2f03875b9dc589a45c272ed71c7b22b8797dac72
Stefan Schlager <zarquon42@gmail.com>
2015-04-23 13:53:06 +0200

    bumped version

--- R/Morpho-package.R
+++ R/Morpho-package.R
@@ -8,8 +8,8 @@
 #' \tabular{ll}{
 #' Package: \tab Morpho\cr
 #' Type: \tab Package\cr
-#' Version: \tab 2.2.150416\cr
-#' Date: \tab 2015-04-16\cr
+#' Version: \tab 2.2.150423\cr
+#' Date: \tab 2015-04-23\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr
 #' }
