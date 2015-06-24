62345708c4b45952d4b602d0c0b05d78beff17c2
Stefan Schlager <zarquon42@gmail.com>
2015-06-24 12:14:21 +0200

    simplified relWarps if alpha=0.

--- R/relwarps.r
+++ R/relwarps.r
@@ -19,18 +19,20 @@
 #' @param orp logical: request orthogonal projection into tangent space.
 #' @param pcAlign logical: if TRUE, the shapes are aligned by the principal axis of the first specimen
 #' @return
-#' \item{bescores }{relative warp scores}
-#' \item{uniscores }{uniform scores}
+#' \item{bescores }{relative warp scores (PC-scores if \code{alpha = 0})}
+#' \item{uniscores }{uniform scores, NULL if  \code{alpha = 0}}
 #' \item{Var }{non-affine variation explained by each relative warp}
 #' \item{mshape }{sample's conensus shape}
 #' \item{rotated }{Procrustes superimposed data}
-#' \item{bePCs }{vector basis of nonaffine shape variation- relative warps}
+#' \item{bePCs }{vector basis of nonaffine shape variation- relative warps (plain PCs if  \code{alpha = 0})}
 #' \item{uniPCs }{vector basis of affine shape variation - uniform
-#' component}
+#' component. NULL if \code{alpha = 0}}
 #' @author Stefan Schlager
 #' @references Bookstein FL 1989. Principal Warps: Thin-plate splines and the
 #' decomposition of deformations. IEEE Transactions on pattern analysis and
-#' machine intelligence 11. Bookstein FL, 1991. Morphometric tools for landmark
+#' machine intelligence 11.
+#'
+#' Bookstein FL, 1991. Morphometric tools for landmark
 #' data. Geometry and biology. Cambridge Univ. Press, Cambridge.
 #' 
 #' Rohlf FJ, Bookstein FL 2003. Computing the Uniform Component of Shape
@@ -66,7 +68,8 @@
 #' @export
 relWarps <- function(data,scale=TRUE,CSinit=TRUE,alpha=1,tol=1e-10,orp=TRUE, pcAlign=TRUE)
 {
-    #n <- dim(data)[3]
+                                        #n <- dim(data)[3]
+    uniscores <- uniPCs <- NULL
     m <- dim(data)[2]
     k <- dim(data)[1]
     datanames <- dimnames(data)[[3]]
@@ -74,68 +77,72 @@ relWarps <- function(data,scale=TRUE,CSinit=TRUE,alpha=1,tol=1e-10,orp=TRUE, pcA
     proc <- ProcGPA(data,scale=scale,CSinit=CSinit,silent=TRUE,pcAlign=pcAlign)
     if (orp)
         proc$rotated <- orp(proc$rotated, mshape=proc$mshape)
-    
+
+    if (alpha !=0 ) {
 ### create bending energy matrix ###
-    BE <- CreateL(proc$mshape,output="Lsubk")$Lsubk
+        BE <- CreateL(proc$mshape,output="Lsubk")$Lsubk
 ### vectorize and scale superimposed data ###
-    vecs <- vecx(proc$rotated)
-    vecs <- scale(vecs, scale=FALSE)
-    dimnames(proc$rotated)[[3]] <- rownames(vecs) <- datanames
-    
+        vecs <- vecx(proc$rotated)
+        vecs <- scale(vecs, scale=FALSE)
+        dimnames(proc$rotated)[[3]] <- rownames(vecs) <- datanames
+        
 ### generate covariance matrix of superimposed data ###
-    Sc <- cov(vecs)
-    
+        Sc <- cov(vecs)
+        
 ### explore eigenstructure of BE ###	
-    eigBE <- svd(BE)
-    eigBE$d <- Re(eigBE$d)
-    eigBE$v <- Re(eigBE$v)
-    
-    zero <- which(eigBE$d<tol)
-    diaginv <- diagBE <- eigBE$d*0
-    diagBE[-zero] <- eigBE$d[-zero]^(-alpha/2)
-    diaginv[-zero] <- eigBE$d[-zero]^(alpha/2)
-    IM <- Matrix::Diagonal(x=rep(1,m))
-    
-    if (alpha !=0) {	
+        eigBE <- svd(BE)
+        eigBE$d <- Re(eigBE$d)
+        eigBE$v <- Re(eigBE$v)
+        
+        zero <- which(eigBE$d<tol)
+        diaginv <- diagBE <- eigBE$d*0
+        diagBE[-zero] <- eigBE$d[-zero]^(-alpha/2)
+        diaginv[-zero] <- eigBE$d[-zero]^(alpha/2)
+        IM <- Matrix::Diagonal(x=rep(1,m))
         suppressMessages(BE2 <- IM%x%(eigBE$v%*%Matrix::Diagonal(x=diagBE)%*%t(eigBE$v)))
-        ##  invBE2 <- IM%x%(eigBE$vectors%*%diag(diaginv)%*%t(eigBE$vectors))
-    } else {
-        BE2 <- Matrix::Diagonal(x=rep(1,k*m))
-        ##   invBE2 <- BE2
-    }
-    
+        
 ### generate covariance structure of scaled space ###
-    covcom <- suppressMessages(BE2%*%Sc%*%BE2)
-    eigCOVCOM <- svd(covcom)
-    nonz <- which(eigCOVCOM$d > tol)
-    bescores <- as.matrix(t(suppressMessages(t(eigCOVCOM$v[,nonz])%*%BE2)%*%t(vecs)))
-    rownames(bescores) <- rownames(vecs)
-    bePCs <-  suppressMessages(IM %x% eigBE$v)
-    bePCs <- as.matrix(suppressMessages(bePCs %*% Matrix::Diagonal(x=rep(diaginv,m)) %*% t(bePCs) %*%  eigCOVCOM$v))
+        covcom <- suppressMessages(BE2%*%Sc%*%BE2)
+        eigCOVCOM <- svd(covcom)
+        nonz <- which(eigCOVCOM$d > tol)
+        bescores <- as.matrix(t(suppressMessages(t(eigCOVCOM$v[,nonz])%*%BE2)%*%t(vecs)))
+        rownames(bescores) <- rownames(vecs)
+        bePCs <-  suppressMessages(IM %x% eigBE$v)
+        bePCs <- as.matrix(suppressMessages(bePCs %*% Matrix::Diagonal(x=rep(diaginv,m)) %*% t(bePCs) %*%  eigCOVCOM$v))
 
 ### calculate uniform component scores ###
-    ## U <- NULL
-    uniscores <- NULL
-    
+        ## U <- NULL
+        
+        
 ### Rohlf first method ###
-    
-    E <- eigBE$v[,-zero]
-    N <- diag(rep(1,k))-E%*%solve(crossprod(E))%*%t(E)
-    V <- vecs
-    NIk <- IM %x% N
-    
-    svdBend <- svd(V%*%NIk)
-    useBendv <- min(ncol(svdBend$v),(m+0.5*m*(m-1)-1))
-    LS <- svdBend$u%*%diag(svdBend$d)
-    useLS <- min(ncol(LS),(m+0.5*m*(m-1)-1))
-    uniscores <- LS[,1:useLS]
-    rownames(uniscores) <- datanames   
-    
+        
+        E <- eigBE$v[,-zero]
+        N <- diag(rep(1,k))-E%*%solve(crossprod(E))%*%t(E)
+        V <- vecs
+        NIk <- IM %x% N
+        
+        svdBend <- svd(V%*%NIk)
+        useBendv <- min(ncol(svdBend$v),(m+0.5*m*(m-1)-1))
+        LS <- svdBend$u%*%diag(svdBend$d)
+        useLS <- min(ncol(LS),(m+0.5*m*(m-1)-1))
+        uniscores <- LS[,1:useLS]
+        rownames(uniscores) <- datanames
+        uniPCs <- svdBend$v[,1:useBendv]
+        Var <- createVarTable(eigCOVCOM$d[nonz],square = FALSE)
+    } else {
+        pca <- prcomp(vecx(proc$rotated))
+        bad <- which(pca$sdev^2 < tol)
+        bePCs <- pca$rotation[,-bad]
+        bescores <- pca$x[,-bad]
+        Var <- createVarTable(pca$sdev)
+    }
+        
 ### create Variance table according to eigenvalues ###
-   
-    Var <- createVarTable(eigCOVCOM$d[nonz],square = FALSE)
     
-    return(list(bescores=bescores,uniscores=uniscores,Var=Var,mshape=proc$mshape,rotated=proc$rotated,bePCs=bePCs,uniPCs=svdBend$v[,1:useBendv]))
+    
+    
+    
+    return(list(bescores=bescores,uniscores=uniscores,Var=Var,mshape=proc$mshape,rotated=proc$rotated,bePCs=bePCs,uniPCs=uniPCs))
     
 }
 
