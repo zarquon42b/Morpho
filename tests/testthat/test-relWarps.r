212d0603b1a6d01687a644514dcee987c0e91611
Stefan Schlager <zarquon42@gmail.com>
2015-06-25 11:37:04 +0200

    fixing travis

--- tests/testthat/test-relWarps.r
+++ tests/testthat/test-relWarps.r
@@ -1,6 +1,7 @@
 context("relative warps")
 test_that("relwarps behaves", {
               data(boneData)
-              rW.baselince=readRDS("testdata/rW.rds")
-              expect_equal(lapply(relWarps(boneLM),abs),lapply(rW.baseline,abs),tol=1e-7)
+              rW.baseline=readRDS("testdata/rW.rds")
+              rwtest <- relWarps(boneLM)
+              expect_equal(lapply(rwtest,abs),lapply(rW.baseline,abs),tol=1e-7)
 })
