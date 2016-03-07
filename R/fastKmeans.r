67a4e62745c15ada25b7ba2466b9b8ede7c0be39
Stefan Schlager <zarquon42@gmail.com>
2016-03-07 12:43:40 +0100

    added fastKmeans

---
+++ R/fastKmeans.r
@@ -0,0 +1,44 @@
+#' fast kmeans clustering for 2D or 3D point clouds
+#'
+#' fast kmeans clustering for 2D or 3D point clouds
+#' @param x matrix containing coordinates or mesh3d
+#' @param k number of clusters
+#' @param iter.max maximum number of iterations
+#' @param tol convergence threshold if the mean distance between centers is below it, the algorithm stops
+#' @param threads integer number of threads to use
+#' @examples
+#' require(Rvcg);require(rgl)
+#' data(humface)
+#' clust <- fastKmeans(humface,k=1000,threads=2)
+#' \dontrun{
+#' require(rgl)
+#' spheres3d(clust$centers)
+#' }
+#' ## now look at the vertices closest to the centers
+#' wire3d(humface)
+#' spheres3d(vert2points(humface)[clust$selected,],col=2)
+#' @export
+fastKmeans <- function(x,k,iter.max=10,tol=1e-5,threads=parallel::detectCores()) {
+    if (inherits(x,"mesh3d"))
+        x <- vert2points(x)
+    k <- abs(k)
+    if (k >= nrow(x))
+        return(list(centers=x,selected=1:nrow(x)))
+    if (!ncol(x) %in% 2:3)
+        stop("x can only have 2 or 3 columns")
+    centerinit <- sample(1:nrow(x))[1:k]
+    centers <- x[centerinit,]
+    cnt <- 1
+    centerchk <- 1e12
+    while (cnt < iter.max && centerchk > tol) {
+        centerold <- centers
+        clost <- vcgKDtree(centers,x,k=1,threads = threads)$index
+        centers <- .Call("fastSubsetMeans",x,clost-1L)
+        centerchk <- mean(vcgKDtree(centers,centerold,k=1,threads = threads)$distance)
+        clost_center <- sort(unique(vcgKDtree(x,centers,k=1,threads = threads)$index))
+        cnt <- cnt+1
+    }
+    out <- list(selected=clost_center,centers=centers)
+    return(out)
+}
+    
---
+++ src/fastSubsetMeans.cpp
@@ -0,0 +1,27 @@
+#include "RcppArmadillo.h"
+using namespace Rcpp;
+using namespace arma;
+
+RcppExport SEXP fastSubsetMeans(SEXP x_, SEXP inds_) {
+   try {
+     mat x = as<mat>(x_);
+  uvec inds = as<uvec>(inds_);
+  int maxlev = inds.max();
+  mat center(maxlev,x.n_cols);
+  for (int i =0; i < maxlev;i++) {
+    
+    uvec tmpinds = find(inds ==i);
+    mat tmpmat = x.rows(tmpinds);
+    rowvec tmpresult(x.n_cols);tmpresult.fill(0);
+    for (int j = 0; j < tmpinds.size();j++)
+      tmpresult += tmpmat.row(j);
+    tmpresult /= tmpinds.size();
+    center.row(i) = tmpresult;
+  }
+  return wrap(center);
+   }  catch (std::exception& e) {
+    ::Rf_error( e.what());
+  } catch (...) {
+    ::Rf_error("unknown exception");
+  }
+}
