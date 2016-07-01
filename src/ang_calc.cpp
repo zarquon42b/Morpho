254e511f01653661d82a9c373d5f802b46d6fde1
Stefan Schlager <zarquon42@gmail.com>
2016-07-01 14:19:46 +0200

    added sortCurve and equidistantCurve

--- src/ang_calc.cpp
+++ src/ang_calc.cpp
@@ -19,4 +19,21 @@ RcppExport SEXP ang_calcC(SEXP x_, SEXP y_) {
 }
 
    
-  
+RcppExport SEXP ang_calcM(SEXP x_, SEXP y_) {
+  try{
+    NumericMatrix x(x_);
+    NumericVector angle(x.nrow());
+    NumericMatrix y(y_);
+    
+    for (int i = 0; i < x.nrow(); i++) {
+      angle(i) = angcalcRcpp(x(i,_),y(i,_));
+    }
+
+    return wrap(angle);
+  } catch (std::exception& e) {
+    ::Rf_error( e.what());
+  } catch (...) {
+    ::Rf_error("unknown exception");
+  }
+}
+
