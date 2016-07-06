23a9d7c84bf7ef5eff0ed982efdb38c983f3d432
Stefan Schlager <zarquon42@gmail.com>
2016-07-06 09:56:07 +0200

    addded support for one-sided configs in fixLMmirror

--- R/fixLMmirror.r
+++ R/fixLMmirror.r
@@ -7,7 +7,7 @@
 #' paired LM. E.g. the left column contains the lefthand landmarks, while the
 #' right side contains the corresponding right hand landmarks.
 #' @note in case both landmarks of a bilateral pair are missing a message will be issued. As well if there are missing landmarks on the midsaggital plane are detected.
-#' @details the configurations are mirrored and the relabled version is matched onto the original using a thin-plate spline deformation. The missing landmark is now estimated using its bilateral counterpart.
+#' @details the configurations are mirrored and the relabled version is matched onto the original using a thin-plate spline deformation. The missing landmark is now estimated using its bilateral counterpart. If one side is completely missing, the landmarks will be mirrored and aligned by the unilateral landmarks.
 #' @return a matrix or array with fixed missing bilateral landmarks.
 #' @examples
 #' data(boneData)
@@ -21,6 +21,14 @@
 #' fixed <- fixLMmirror(exampmat, pairedLM=pairedLM)
 #' \dontrun{
 #' deformGrid3d(fixed, boneLM[,,1],ngrid=0)
+#' ## result is a bit off due to actual asymmetry
+#' }
+#' ## example with one side completely missing
+#' oneside <- boneLM[,,1]
+#' oneside[pairedLM[,1],] <- NA
+#' onesidefixed <- fixLMmirror(oneside,pairedLM)
+#' \dontrun{
+#' deformGrid3d(onesidefixed, boneLM[,,1],ngrid=0)
 #' ## result is a bit off due to actual asymmetry
 #' }
 #' @export
@@ -40,14 +48,16 @@ fixLMmirror.array <- function(x,pairedLM) {
 #' @rdname fixLMmirror
 #' @export
 fixLMmirror.matrix <- function(x,pairedLM) {
-    data <- x
+    mydata <- x
     m <- dim(x)[2]
     count <- 0
     k <- nrow(x)
     checklist <- NA
     unilatNA <- NULL
+    unilat <- 1:nrow(x)
+    unilat <- unilat[which(!unilat %in% c(pairedLM))]
     for (j in 1:k) {
-        if (TRUE %in% is.na(data[j,])) {
+        if (TRUE %in% is.na(mydata[j,])) {
             count <- count+1
             checklist[count] <- j
         }
@@ -62,7 +72,7 @@ fixLMmirror.matrix <- function(x,pairedLM) {
         checkPaired <- is.na(x[pairedLM[i,],])
         if (TRUE %in% checkPaired) {
             affected <- append(affected,i)
-            checkCol <- (apply(checkPaired,1,prod))
+            checkCol <- as.logical(apply(checkPaired,1,prod))
             affectCol <- append(affectCol,which(!as.logical(checkCol)))
             goodPaired <- append(goodPaired,pairedLM[i,which(!as.logical(checkCol))])
         }
@@ -76,13 +86,24 @@ fixLMmirror.matrix <- function(x,pairedLM) {
         warning("missing landmarks are not bilateral")
         unilatNA <- append(unilatNA,checklist[which(! checklist %in% pairedLM)])
     }
-        
+    if (length(unilatNA))
+        unilat <- unilat[-c(unilatNA)]
     xmir <- x %*% diag(c(-1,1,1))[1:m, 1:m]#mirror landmarks
     xmir[c(pairedLM),] <- xmir[c(pairedLM[,2:1]),]##relabel landmarks
     xref <- xmir[-c(unilatNA,pairedLM[affected,]),]
     xtar <- x[-c(unilatNA,pairedLM[affected,]),]
-    xout <- tps3d(xmir,xref,xtar,threads=1)
-    xout[goodPaired,] <- x[goodPaired,]
+    if (prod(sort(goodPaired) == sort(pairedLM[,1])) || prod(sort(goodPaired) == sort(pairedLM[,2]))) {
+        message("one side completely missing: mirroring on midplane")
+        trans <- computeTransform(x[unilat,],xmir[unilat,])
+        xrot <- applyTransform(xmir,trans)
+        xout <- x
+        xout[pairedLM[,which(checkCol)],] <- xrot[pairedLM[,which(checkCol)],]
+        
+    } else {
+        xout <- tps3d(xmir,xref,xtar,threads=1)
+        xout[goodPaired,] <- x[goodPaired,]
+    }
+
     return(xout)
 
 }
