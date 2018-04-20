76830f362c058761aad701ddffe73c4a6861dd65
Stefan Schlager <zarquon42@gmail.com>
2018-04-20 11:10:11 +0200

    CRAN release

--- R/Morpho-package.R
+++ R/Morpho-package.R
@@ -8,8 +8,8 @@
 #' \tabular{ll}{
 #' Package: \tab Morpho\cr
 #' Type: \tab Package\cr
-#' Version: \tab 2.5.1\cr
-#' Date: \tab 2017-04-19\cr
+#' Version: \tab 2.6\cr
+#' Date: \tab 2018-04-19\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr
 #' }
