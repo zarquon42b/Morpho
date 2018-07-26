cb1672e34d57be8f374a5220b56f93e2c788a949
Stefan Schlager <zarquon42@gmail.com>
2018-07-26 15:34:03 +0200

    checkLM: added option meshlist

--- R/check.lm.r
+++ R/check.lm.r
@@ -26,6 +26,7 @@
 #' be shaded.
 #' @param point how to render landmarks. "s"=spheres, "p"=points.
 #' @param add logical: add to existing rgl window.
+#' @param meshlist list holding meshes in the same order as \code{dat.array} (Overrides \code{path}).
 #' @param Rdata logical: if the meshes are previously stored as Rdata-files by
 #' calling save(), these are simply loaded and rendered. Otherwise it is
 #' assumed that the meshes are stored in standard file formats such as PLY, STL
@@ -66,117 +67,125 @@
 #' }
 #' 
 #' @export
-checkLM <- function(dat.array, path=NULL, prefix="", suffix=".ply", col="white", pt.size=NULL, alpha=1, begin=1, render=c("w","s"), point=c("s","p"), add=FALSE, Rdata=FALSE, atlas=NULL, text.lm=FALSE)
-    {
-        k <- NULL
-        marked <- NULL
-        j <- 1
-        if (!Rdata)
-            load <- file2mesh
-        outid <- NULL
-        point <- point[1]
-        ## set point/sphere sizes
-        radius <- pt.size
-        if (is.null(radius)) {
-            if (point == "s")
-                radius <- (cSize(dat.array[,,1])/sqrt(nrow(dat.array[,,1])))*(1/30)
-            else
-                radius <- 10
-        }
-        size <- radius
-        render <- render[1]
-        arr <- FALSE
-        point <- point[1]
-        if (point == "s") {
-            rendpoint <- spheres3d
-        } else if (point == "p") {
-            rendpoint <- points3d
-        } else {
-            stop("argument \"point\" must be \"s\" for spheres or \"p\" for points")
-        }
-        dimDat <- dim(dat.array)
-        if (length(dimDat) == 3) {
-            n <- dim(dat.array)[3]
-            name <- dimnames(dat.array)[[3]]
-            arr <- TRUE
-        } else if (is.list(dat.array)) {
-            n <- length(dat.array)
-            name <- names(dat.array)
-        } else {
-            stop("data must be 3-dimensional array or a list")
-        }
-        i <- begin
-        if (render=="w") {
-            back <- front <- "lines"
-            rend <- wire3d
-        } else {
-            back <- front <- "filled"
-        }
-        if (!add || rgl.cur()==0)
-            open3d()
-        if (!is.null(atlas)) {
-            k <- dim(atlas$landmarks)[1]
-            #k1 <- dim(atlas$patch)[1]
-        }
-        meshnames <-  paste(path,prefix,name,suffix,sep="")
-        while (i <= n) {
-            rgl.bringtotop()
-            tmp.name <- meshnames[i]
-            if (arr)
-                landmarks <- dat.array[,,i]
-            else
-                landmarks <- dat.array[[i]]
-            if (is.null(atlas)) { 
-                outid <- rendpoint(landmarks,radius=radius, size=size)
-                if (text.lm)
-                    outid <- c(outid, text3d(landmarks, texts=paste(1:dim(landmarks)[1], sep=""), cex=1, adj=c(1,1.5)))
-                             
-                if (!is.null(path)) {
-                    if (!Rdata) {
-                        tmpmesh <- file2mesh(tmp.name,readcol=TRUE)
-                    } else {
-                        input <- load(tmp.name)
-                        tmp.name <- gsub(path,"",tmp.name)
-                        tmpmesh <- get(input)
-                    }
-                    
-                    outid <- c(outid,shade3d(tmpmesh,col=col,alpha=alpha,back=back,front=front))
-                    rm(tmpmesh)
-                    if (Rdata)
-                        rm(list=input)
-                    gc()
+checkLM <- function(dat.array, path=NULL, prefix="", suffix=".ply", col="white", pt.size=NULL, alpha=1, begin=1, render=c("w","s"), point=c("s","p"), add=FALSE,meshlist=NULL, Rdata=FALSE, atlas=NULL, text.lm=FALSE)
+{
+    k <- NULL
+    marked <- NULL
+    j <- 1
+    if (!Rdata)
+        load <- file2mesh
+    outid <- NULL
+    point <- point[1]
+    ## set point/sphere sizes
+    radius <- pt.size
+    if (is.null(radius)) {
+        if (point == "s")
+            radius <- (cSize(dat.array[,,1])/sqrt(nrow(dat.array[,,1])))*(1/30)
+        else
+            radius <- 10
+    }
+    size <- radius
+    render <- render[1]
+    arr <- FALSE
+    point <- point[1]
+    if (point == "s") {
+        rendpoint <- spheres3d
+    } else if (point == "p") {
+        rendpoint <- points3d
+    } else {
+        stop("argument \"point\" must be \"s\" for spheres or \"p\" for points")
+    }
+    dimDat <- dim(dat.array)
+    if (length(dimDat) == 3) {
+        n <- dim(dat.array)[3]
+        name <- dimnames(dat.array)[[3]]
+        arr <- TRUE
+    } else if (is.list(dat.array)) {
+        n <- length(dat.array)
+        name <- names(dat.array)
+    } else {
+        stop("data must be 3-dimensional array or a list")
+    }
+    i <- begin
+    if (render=="w") {
+        back <- front <- "lines"
+        rend <- wire3d
+    } else {
+        back <- front <- "filled"
+    }
+    if (!add || rgl.cur()==0)
+        open3d()
+    if (!is.null(atlas)) {
+        k <- dim(atlas$landmarks)[1]
+                                        #k1 <- dim(atlas$patch)[1]
+    }
+    meshnames <-  paste(path,prefix,name,suffix,sep="")
+    while (i <= n) {
+        rgl.bringtotop()
+        tmp.name <- meshnames[i]
+        if (arr)
+            landmarks <- dat.array[,,i]
+        else
+            landmarks <- dat.array[[i]]
+        if (is.null(atlas)) { 
+            outid <- rendpoint(landmarks,radius=radius, size=size)
+            if (text.lm)
+                outid <- c(outid, text3d(landmarks, texts=paste(1:dim(landmarks)[1], sep=""), cex=1, adj=c(1,1.5)))
+            
+            if (!is.null(meshlist)) {
+                tmpmesh <- meshlist[[i]]
+            } else if (!is.null(path)) {
+                if (!Rdata) {
+                    tmpmesh <- file2mesh(tmp.name,readcol=TRUE)
+                } else {
+                    input <- load(tmp.name)
+                    tmp.name <- gsub(path,"",tmp.name)
+                    tmpmesh <- get(input)
                 }
-            } else {
-                atlas.tmp <- atlas
-                atlas.tmp$mesh <- NULL
-                atlas.tmp$landmarks <- landmarks[1:k,]
-                atlas.tmp$patch <- landmarks[-c(1:k),]
+            }
+            if (!is.null(meshlist) || !is.null(path)) {
                 
-                if (!is.null(path)) {
-                    if (!Rdata) {
-                        atlas.tmp$mesh <- file2mesh(tmp.name)
-                    } else {
-                        input <- load(tmp.name)
-                        tmp.name <- gsub(path,"",tmp.name)
-                        atlas.tmp$mesh <- get(input)
-                    }
-                }
-                outid <- plotAtlas(atlas.tmp, add=TRUE, alpha=alpha, pt.size=radius, render=render, point=point, meshcol=col, legend=FALSE)
+                outid <- c(outid,shade3d(tmpmesh,col=col,alpha=alpha,back=back,front=front))
+                rm(tmpmesh)
+                if (Rdata)
+                    rm(list=input)
+                gc()
                 
             }
-                
-                
-                answer <- readline(paste("viewing #",i,"(return=next | p=previous | m=mark current | s=stop viewing)\n"))
-                if (answer == "m") {
-                    marked[j] <- i
-                    j <- j+1
-                } else if (answer == "s") {
-                    i <- n+1
-                } else if (answer == "p") {
-                    i <- i-1
-                } else 
-                    i <- i+1
-                rgl.pop(id=outid)
+        } else {
+            atlas.tmp <- atlas
+            atlas.tmp$mesh <- NULL
+            atlas.tmp$landmarks <- landmarks[1:k,]
+            atlas.tmp$patch <- landmarks[-c(1:k),]
+
+            if (!is.null(meshlist)) {
+                atlas.tmp$mesh <- meshlist[[i]]
+            } 
+            if (!is.null(path) && is.null(meshlist)) {
+                if (!Rdata) {
+                    atlas.tmp$mesh <- file2mesh(tmp.name)
+                } else {
+                    input <- load(tmp.name)
+                    tmp.name <- gsub(path,"",tmp.name)
+                    atlas.tmp$mesh <- get(input)
+                }
             }
-            invisible(marked)
+            outid <- plotAtlas(atlas.tmp, add=TRUE, alpha=alpha, pt.size=radius, render=render, point=point, meshcol=col, legend=FALSE)
+            
         }
+        
+        
+        answer <- readline(paste("viewing #",i,"(return=next | p=previous | m=mark current | s=stop viewing)\n"))
+        if (answer == "m") {
+            marked[j] <- i
+            j <- j+1
+        } else if (answer == "s") {
+            i <- n+1
+        } else if (answer == "p") {
+            i <- i-1
+        } else 
+            i <- i+1
+        rgl.pop(id=outid)
+    }
+    invisible(marked)
+}
