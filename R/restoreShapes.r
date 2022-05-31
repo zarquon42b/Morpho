d6b69bf2c295d4573152793e7fc0ec7a1295af24
Stefan Schlager <zarquon42@gmail.com>
2022-05-31 12:06:48 +0200

    restoreShapes: fixed example

--- R/restoreShapes.r
+++ R/restoreShapes.r
@@ -33,7 +33,7 @@
 #'
 #' ## Now restore some sizeshape data
 #' procSize <- procSym(gorf.dat,sizeshape=TRUE)
-#' est1 <- restoreShapes(range(gpa$PCscores[,1]),gpa$PCs[,1],gpa$mshape,sizeshape=TRUE,origsize=TRUE,meanlogCS=procSize$meanlogCS)
+#' est1 <- restoreShapes(range(procSize$PCscores[,1]),procSize$PCs[,1],procSize$mshape,sizeshape=TRUE,origsize=TRUE,meanlogCS=procSize$meanlogCS)
 #' }
 #' 
 #' @seealso \code{\link{getPCscores}}
