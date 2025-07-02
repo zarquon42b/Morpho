3d1528c964679c83340328e822eaa88c1eeef852
Mario Schädel <42519751+mcranium@users.noreply.github.com>
2025-07-02 11:39:30 +0200

    Shrinking prevention for icpmat.r
    Implemented two ways to prevent two types of undesired shrinking, see explanation in https://github.com/zarquon42b/Morpho/issues/41

--- R/icpmat.r
+++ R/icpmat.r
@@ -11,6 +11,8 @@
 #' @param weights vector of length \code{nrow(x)} containing weights for each row in \code{x}
 #' @param centerweight logical: if weights are defined and centerweigths=TRUE, the matrix will be centered according to these weights instead of the barycenter.
 #' @param threads integer: number of threads to use.
+#' @param minscalef double: for types "similarity" and "affine", minimum scale factor per iteration, prevents excessive shrinking in first iteration.
+#' @param mincumscalef double: for types "similarity" and "affine", minimum cumulative scale factor, prevents shrinking below threshold (e.g. 0.7), values above 1 will enforce size increase, should only be used if both landmark configurations are in the same scale.
 #' @return returns the rotated landmarks
 #' @examples
 #' data(nose)
@@ -51,43 +53,70 @@
 #' }
 #' @importFrom Rvcg vcgKDtree vcgSearchKDtree vcgCreateKDtree
 #' @export
-icpmat <- function(x,y,iterations,mindist=1e15,subsample=NULL,type=c("rigid","similarity","affine"),weights=NULL,threads=1,centerweight=FALSE) {
-    m <- ncol(x)
-    if (m == 2) {
-        x <- cbind(x,0)
-        y <- cbind(y,0)
+icpmat = function (x,y,iterations,mindist=1e+15,subsample=NULL,type=c("rigid","similarity","affine"),weights = NULL,threads=1,centerweight=FALSE,minscalef=0.9,mincumscalef=NULL) 
+{
+  m <- ncol(x)
+  if (m == 2) {
+    x <- cbind(x,0)
+    y <- cbind(y,0)
+  }
+  if (!is.null(weights)) 
+    if (length(weights) != nrow(x)) 
+      stop("weights must be of same length as nrow(x)")
+  type <- match.arg(type, c("rigid","similarity","affine"))
+  if (!is.null(subsample)) {
+    subsample <- min(nrow(x) - 1, subsample)
+    subs <- fastKmeans(x, k = subsample, iter.max = 100, 
+                       threads = threads)$selected
+    xtmp <- x[subs, ]
+    if (!is.null(weights)) 
+      weights <- weights[subs]
+  }
+  else {
+    xtmp <- x
+  }
+  yKD <- vcgCreateKDtree(y)
+  cumscalef <- 1
+  for (i in 1:iterations) {
+    clost <- vcgSearchKDtree(yKD, xtmp, 1, threads = threads)
+    good <- which(clost$distance < mindist)
+    tmpweights <- weights
+    if (!is.null(weights)) 
+      tmpweights <- weights[good]
+    trafo <- computeTransform(y[clost$index[good], ], xtmp[good, 
+    ], type = type, weights = tmpweights, centerweight = centerweight)
+    if(type!="rigid"){
+      # calculate scale factor from transformation matrix
+      scalef <- sqrt(sum(trafo[1:3,1]^2))
+      if (!is.null(mincumscalef) & scalef<minscalef){
+        trafo <- computeTransform(y[clost$index[good], ], xtmp[good, 
+        ], type = "rigid", weights = tmpweights, centerweight = centerweight)
+        scalef <- 1
+      }
+      cumscalef <- cumscalef * scalef
+      if(!is.null(mincumscalef)){
+        if(cumscalef<mincumscalef){
+          type <- "rigid"
+          warning("Cumulative scale factor belows mincumscalf, switching to rigid type")
+          #scale up by inverse of cumscalef
+          trafo <- matrix(0, 4, 4) 
+          trafo[1,1] <- 1/cumscalef
+          trafo[2,2] <- 1/cumscalef
+          trafo[3,3] <- 1/cumscalef
+          cumscalef <- 1 # reset cumscalef
+        }
+      }
     }
-    if (!is.null(weights))
-        if (length(weights) != nrow(x))
-            stop("weights must be of same length as nrow(x)")
-    type <- match.arg(type,c("rigid","similarity","affine"))
-    if (!is.null(subsample)) {
-        subsample <- min(nrow(x)-1,subsample)
-        subs <- fastKmeans(x,k=subsample,iter.max = 100,threads=threads)$selected
-        xtmp <- x[subs,]
-        if (!is.null(weights))
-            weights <- weights[subs]
-    } else {
-        xtmp <- x
-    }
-    yKD <- vcgCreateKDtree(y)
-    for (i in 1:iterations) {
-        clost <- vcgSearchKDtree(yKD,xtmp,1,threads=threads)
-        good <- which(clost$distance < mindist)
-        tmpweights <- weights
-        if (!is.null(weights))
-            tmpweights <- weights[good]
-        trafo <- computeTransform(y[clost$index[good],],xtmp[good,],type=type,weights = tmpweights,centerweight = centerweight)
-        xtmp <- applyTransform(xtmp[,],trafo)
-    }
-    if (!is.null(subsample)) {
-        fintrafo <- computeTransform(xtmp[,],x[subs,],type = type)
-        xtmp <- applyTransform(x,fintrafo)
-    }
-    if (m == 2)
-        xtmp <- xtmp[,1:2]
-    return(xtmp)
-        
+    xtmp <- applyTransform(xtmp[, ], trafo)
+  }
+  if (!is.null(subsample)) {
+    fintrafo <- computeTransform(xtmp[, ], x[subs, ], type = type)
+    xtmp <- applyTransform(x, fintrafo)
+  }
+  if (m == 2) 
+    xtmp <- xtmp[,1:2]
+  return(xtmp)
+  
 }
     
 
